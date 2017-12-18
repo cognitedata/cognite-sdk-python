f54d21c942adfac032b2a6214e8b4ac98d1c6d26
erlendcognite <erlend.vollset@cognite.com>
2017-12-18 17:55:26 +0100

    Update package name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='cognite',
+setup(name='cognite-sdk-sdk',
       version='0.3',
       description='Cognite Python SDK',
       url='https://github.com/cognitedata/cognite-sdk-python',
-      download_url='https://github.com/cognitedata/cognite-sdk-python/cognite/archive/0.3.tar.gz',
+      download_url='https://github.com/cognitedata/cognite-sdk-python/cognite-sdk/archive/0.3.tar.gz',
       author='Erlend Vollset',
       author_email='erlend.vollset@cognite.com',
-      packages=['cognite'],
+      packages=['cognite-sdk'],
       install_requires=[
           'requests',
           'pandas'
