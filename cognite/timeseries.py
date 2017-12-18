a37907f4fb6a20294147e3850aa52596783cc55e
erlendcognite <erlend.vollset@cognite.com>
2017-12-18 20:13:40 +0100

    Add new methods getLatest, getMultiTagDatapoints, getDatapoints

--- cognite/timeseries.py
+++ cognite/timeseries.py
@@ -4,26 +4,74 @@ import json
 import pandas as pd
 import requests
 
-def get_datapoints_frame(tagIds, aggregates, granularity, start=None, end=None, limit=10000, api_key=None, project=None):
+def get_datapoints(tagId, aggregates=None, granularity=None, start=None, end=None, limit=10000, api_key=None,
+                   project=None):
+    api_key, project = config.get_config_variables(api_key, project)
+    tagId = tagId.replace('/', '%2F')
+    url = 'https://api.cognitedata.com/api/0.3/projects/{}/timeseries/data/{}'.format(project, tagId)
+    params = {
+        'aggregates': aggregates,
+        'granularity': granularity,
+        'limit': limit,
+        'start': start,
+        'end': end,
+    }
+    headers = {
+        'api-key': api_key,
+        'accept': 'application/json'
+    }
+    r = requests.get(url, params=params, headers=headers)
+    return r.json()
+
+def get_latest(tagId, api_key=None, project=None):
+    api_key, project = config.get_config_variables(api_key, project)
+    tagId = tagId.replace('/', '%2F')
+    url = 'https://api.cognitedata.com/api/0.3/projects/{}/timeseries/latest/{}'.format(project, tagId)
+    headers = {
+        'api-key': api_key,
+        'accept': 'application/json'
+    }
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+def get_multi_tag_datapoints(tagIds, aggregates=None, granularity=None, start=None, end=None, limit=10000, api_key=None,
+                             project=None):
+    api_key, project = config.get_config_variables(api_key, project)
+    url = 'https://api.cognitedata.com/api/0.3/projects/{}/timeseries/dataquery'.format(project)
+    body = {
+        'items': [{'tagId': '{}'.format(tagId)} for tagId in tagIds],
+        'aggregates': aggregates,
+        'granularity': granularity,
+        'start': start,
+        'end': end,
+        'limit': limit
+    }
+    headers = {
+        'api-key': api_key,
+        'content-type': 'application/json',
+        'accept': 'application/json'
+    }
+    r = requests.post(url=url, data=json.dumps(body), headers=headers)
+    return r.json()
+
+def get_datapoints_frame(tagIds, aggregates, granularity, start=None, end=None, limit=10000, api_key=None,
+                         project=None):
     api_key, project = config.get_config_variables(api_key, project)
     url = 'https://api.cognitedata.com/api/0.3/projects/{}/timeseries/dataframe'.format(project)
     body = {
         'items': [{'tagId': '{}'.format(tagId)} for tagId in tagIds],
         'aggregates': aggregates,
         'granularity': granularity,
+        'start': start,
+        'end': end,
         'limit': limit
     }
-    if start:
-        body['start'] = start
-    if end:
-        body['end'] = end
-
     headers = {
         'api-key': api_key,
         'content-type': 'application/json',
         'accept': 'text/csv'
     }
-
     r = requests.post(url=url, data=json.dumps(body), headers=headers)
+    if r.status_code != 200:
+        return r.json()
     return pd.read_csv(io.StringIO(r.content.decode('utf-8')))
-
