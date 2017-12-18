6bdff188b2d901fd0e88c277fe5e8ed0fd4af310
erlendcognite <erlend.vollset@cognite.com>
2017-12-18 17:47:57 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cognite',
-      version='0.1',
+      version='0.2',
       description='Cognite Python SDK',
       url='https://github.com/cognitedata/cognite-sdk-python',
       download_url='https://github.com/cognitedata/cognite-sdk-python/cognite/archive/0.1.tar.gz',
