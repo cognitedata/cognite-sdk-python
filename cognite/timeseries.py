b12dbc62f8d48401921a6c909259e1d45320578a
erlendcognite <erlend.vollset@cognite.com>
2017-12-20 16:43:00 +0100

    Add helpers module and add paging for get_datapoints_frame() method

--- cognite/timeseries.py
+++ cognite/timeseries.py
@@ -1,10 +1,11 @@
 import cognite.config as config
+from cognite._helpers import _granularity_to_ms
 import io
 import json
 import pandas as pd
 import requests
 
-from cognite.data_objects import DatapointsObject, LatestDatapointObject
+from cognite._data_objects import DatapointsObject, LatestDatapointObject
 
 def get_datapoints(tagId, aggregates=None, granularity=None, start=None, end=None, limit=10000, api_key=None,
                    project=None):
@@ -24,9 +25,8 @@ def get_datapoints(tagId, aggregates=None, granularity=None, start=None, end=Non
     }
     r = requests.get(url, params=params, headers=headers)
     if r.status_code != 200:
-        return r.json()
-    else:
-        return DatapointsObject(r.json())
+        return r.text()
+    return DatapointsObject(r.json())
 
 def get_latest(tagId, api_key=None, project=None):
     api_key, project = config.get_config_variables(api_key, project)
@@ -38,9 +38,8 @@ def get_latest(tagId, api_key=None, project=None):
     }
     r = requests.get(url, headers=headers)
     if r.status_code != 200:
-        return r.json()
-    else:
-        return LatestDatapointObject(r.json())
+        return r.text()
+    return LatestDatapointObject(r.json())
 
 def get_multi_tag_datapoints(tagIds, aggregates=None, granularity=None, start=None, end=None, limit=10000, api_key=None,
                              project=None):
@@ -61,13 +60,13 @@ def get_multi_tag_datapoints(tagIds, aggregates=None, granularity=None, start=No
     }
     r = requests.post(url=url, data=json.dumps(body), headers=headers)
     if r.status_code != 200:
-        return r.json()
-    else:
-        return [DatapointsObject({'data': {'items': [dp]}}) for dp in r.json()['data']['items']]
+        return r.text()
+    return [DatapointsObject({'data': {'items': [dp]}}) for dp in r.json()['data']['items']]
 
-def get_datapoints_frame(tagIds, aggregates, granularity, start=None, end=None, limit=10000, api_key=None,
+def get_datapoints_frame(tagIds, aggregates, granularity, start=None, end=None, api_key=None,
                          project=None):
     api_key, project = config.get_config_variables(api_key, project)
+    limit = 10000
     url = 'https://api.cognitedata.com/api/0.3/projects/{}/timeseries/dataframe'.format(project)
     body = {
         'items': [{'tagId': '{}'.format(tagId)} if type(tagId) == str else {'tagId': '{}'.format(tagId['tagId']), 'aggregates': tagId['aggregates']} for tagId in tagIds],
@@ -77,7 +76,6 @@ def get_datapoints_frame(tagIds, aggregates, granularity, start=None, end=None, 
         'end': end,
         'limit': limit
     }
-    print(body)
     headers = {
         'api-key': api_key,
         'content-type': 'application/json',
@@ -85,5 +83,16 @@ def get_datapoints_frame(tagIds, aggregates, granularity, start=None, end=None, 
     }
     r = requests.post(url=url, data=json.dumps(body), headers=headers)
     if r.status_code != 200:
-        return r.json()
-    return pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+        print(r.text())
+        return None
+    dataframes = [pd.read_csv(io.StringIO(r.content.decode(r.encoding if r.encoding else r.apparent_encoding)))]
+    while dataframes[-1].shape[0] == limit:
+        print(len(dataframes))
+        start = int(dataframes[-1].iloc[-1,0]) + _granularity_to_ms(granularity)
+        body['start'] = start
+        r = requests.post(url=url, data=json.dumps(body), headers=headers)
+        if r.status_code != 200:
+            return r.text()
+        dataframes.append(pd.read_csv(io.StringIO(r.content.decode(r.encoding if r.encoding else r.apparent_encoding))))
+    return pd.concat(dataframes).reset_index(drop=True)
+
