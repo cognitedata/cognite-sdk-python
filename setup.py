9c62c8fdcc328ab1b041bc23d7dd2242523a587d
erlendcognite <erlend.vollset@cognite.com>
2018-01-05 14:17:46 +0100

    Move version variable out of setup method

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
+version = '0.3.5'
+
 setup(name='cognite-sdk',
-      version='0.3.5',
+      version=version,
       description='Cognite API SDK for Python',
       url='https://github.com/cognitedata/cognite-sdk-python',
-      download_url='https://github.com/cognitedata/cognite-sdk-python/cognite/archive/0.3.5.tar.gz',
+      download_url='https://github.com/cognitedata/cognite-sdk-python/cognite/archive/{}.tar.gz'.format(version),
       author='Erlend Vollset',
       author_email='erlend.vollset@cognite.com',
       packages=['cognite'],
