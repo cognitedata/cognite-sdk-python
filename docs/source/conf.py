378199bec5da6884d6df6447acdd5883bbe4c776
erlendcognite <erlend.vollset@cognite.com>
2018-01-08 19:16:09 +0100

    Change static to _static in docs and add .nojekyll

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,10 +90,10 @@ html_theme = 'alabaster'
 #
 # html_theme_options = {}
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
+# Add any paths that contain custom _static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin _static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
