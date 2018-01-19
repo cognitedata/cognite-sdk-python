f401bf5818c66faec04be0a3722c338d26dee76e
erlendcognite <erlend.vollset@cognite.com>
2018-01-19 13:04:18 +0100

    Update docstrings in assets module

--- cognite/assets.py
+++ cognite/assets.py
@@ -4,6 +4,7 @@
 This module mirrors the Assets API.
 """
 import cognite.config as config
+import cognite._constants as _constants
 import cognite._utils as _utils
 from cognite.data_objects import AssetSearchObject
 
@@ -12,7 +13,7 @@ def search_assets(description, api_key=None, project=None):
     '''Returns assets matching provided description.
 
     Args:
-        description (str): Search query.
+        description (str):      Search query.
 
         api_key (str):          Your api-key.
 
@@ -35,16 +36,25 @@ def search_assets(description, api_key=None, project=None):
 
 
 # Author: TK
-def get_assets(asset_id=None, depth=None, api_key=None, project=None):
+def get_assets(tag_id=None, depth=None, limit=_constants.LIMIT, api_key=None, project=None):
     '''Returns assets with provided assetId.
 
-    TODO:
-        * TK: Enter description, args, and returns
+    Args:
+        tag_id (str):         The tag ID of the top asset to get.
+
+        depth (int):            Get subassets this many levels below the top asset.
+
+        api_key (str):          Your api-key.
+
+        project (str):          Project name.
+    Returns:
+        AssetSearchObject
     '''
     api_key, project = config.get_config_variables(api_key, project)
-    url = config.get_base_url() + '/projects/{}/assets/{}'.format(project, asset_id)
+    url = config.get_base_url() + '/projects/{}/assets/{}'.format(project, tag_id)
     params = {
         'depth': depth,
+        'limit': limit
     }
     headers = {
         'api-key': api_key,
