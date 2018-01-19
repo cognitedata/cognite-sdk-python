d281862d191e2e39e9de547ffab75c7090dd168b
erlendcognite <erlend.vollset@cognite.com>
2018-01-19 13:04:01 +0100

    Change release convention instructions, move version number to cognite.__init__, and fix download url in setup.py

--- cognite/__init__.py
+++ cognite/__init__.py
@@ -4,4 +4,16 @@ This package has been created to ensure excellent excellent user experience for 
 Data Platform (CDP).
 '''
 
+# RELEASE NAMING CONVENTIONS
+#
+# Format:
+#
+# MAJOR.MINOR[.MICRO]
+# The major and minor version numbers should mirror the Cognite API. Micro releases are dedicated to bug fixes,
+# improvements, and additions.
+#
+
+
 __all__ = ['assets', 'config', 'similarity_search', 'tagmatching', 'timeseries', 'raw']
+__version__ = '0.4.6'
+
