f4eb6138c02c3203ae039b1174ffaa3625185162
erlendcognite <erlend.vollset@cognite.com>
2018-01-19 13:04:01 +0100

    Change release convention instructions, move version number to cognite.__init__, and fix download url in setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,11 @@
 from setuptools import setup
-
-# RELEASE NAMING CONVENTIONS
-#
-# Format: MAJOR.MINOR[.MICRO][PRE-RELEASE IDENTIFIER]
-# Example: 0.4.1a1
-#
-# Major: Major revision number for the software like 2 or 3 for Python
-# Minor: Groups moderate changes to the software like bug fixes or minor improvements
-# Micro: Releases dedicated to bug fixes
-#
-# Valid pre-release identifiers are: a (alpha), b (beta), rc (release candidate)
-#
-# alpha releases: early pre-releases. A lot of changes can occur between alphas and the final release,
-# like feature additions or refactorings. But they are minor changes and the software should stay pretty unchanged
-# by the time the first beta is reached.
-#
-# beta releases: at this stage, no new features are added and developers are tracking remaning bugs.
-#
-# release candidate releases: a release candidate is an ultimate release before the final release. Unless something
-# bad happens, nothing is changed.
-
-
-version = '0.4.7'
+import cognite
 
 setup(name='cognite-sdk',
-      version=version,
+      version=cognite.__version__,
       description='Cognite API SDK for Python',
       url='http://cognite-sdk-python.readthedocs.io/',
-      download_url='https://github.com/cognitedata/cognite-sdk-python/cognite/archive/{}.tar.gz'.format(version),
+      download_url='https://github.com/cognitedata/cognite-sdk-python/archive/{}.tar.gz'.format(cognite.__version__),
       author='Erlend Vollset',
       author_email='erlend.vollset@cognite.com',
       packages=['cognite'],
