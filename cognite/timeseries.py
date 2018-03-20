ae50fb9ee07395962b0161652f892ca7376b4759
erlendcognite <erlend.vollset@cognite.com>
2018-03-20 14:10:11 +0100

    Implement split and parallelization of get_datapoints requests

--- cognite/timeseries.py
+++ cognite/timeseries.py
@@ -4,7 +4,11 @@
 This module mirrors the Timeseries API. It allows you to fetch data from the api and output it in various formats.
 """
 import io
+import warnings
+import os
 from datetime import datetime
+from functools import partial
+from multiprocessing import Pool
 
 import pandas as pd
 
@@ -37,7 +41,9 @@ def get_datapoints(tag_id, aggregates=None, granularity=None, start=None, end=No
 
     Keyword Arguments:
         protobuf (bool):        Download the data using the binary protobuf format. Only applicable when getting raw data.
-                                Defaults to False.
+                                Defaults to True.
+
+        processes (int):        Number of download processes to run in parallell. Defaults to number returned by cpu_count().
 
         api_key (str):          Your api-key.
 
@@ -47,7 +53,85 @@ def get_datapoints(tag_id, aggregates=None, granularity=None, start=None, end=No
         DatapointsObject: A data object containing the requested data with several getter methods with different
         output formats.
     '''
+
+
+
     api_key, project = config.get_config_variables(kwargs.get('api_key'), kwargs.get('project'))
+    if end is None:
+        end = _utils.get_last_datapoint_ts(tag_id, api_key=api_key, project=project)
+    diff = end - start
+    num_of_processes = kwargs.get('processes', os.cpu_count())
+    steps = min(num_of_processes, max(1, int(diff / _utils.granularity_to_ms(granularity))))
+    step_size = _utils.round_to_nearest(int(diff / steps), base=_utils.granularity_to_ms(granularity))
+
+    partial_get_dps = partial(
+        _get_datapoints_helper_wrapper,
+        tag_id=tag_id,
+        aggregates=aggregates,
+        granularity=granularity,
+        protobuf=True,
+        api_key=api_key,
+        project=project
+    )
+
+    step_starts = [start + (i * step_size) for i in range(steps)]
+    args = [{'start': start, 'end': start + step_size, 'display_progress': False} for start in step_starts]
+
+    p = Pool(steps)
+
+    datapoints = p.map(partial_get_dps, args)
+
+    concat_dps = []
+    [concat_dps.extend(el) for el in datapoints]
+    return DatapointsObject({'data': {'items': [{'tagId': tag_id, 'datapoints': concat_dps}]}})
+
+def _get_datapoints_helper_wrapper(args, tag_id, aggregates, granularity, protobuf, api_key, project):
+    return _get_datapoints_helper(
+        tag_id,
+        aggregates,
+        granularity,
+        args['start'],
+        args['end'],
+        protobuf=protobuf,
+        api_key=api_key,
+        project=project,
+        display_progress=args['display_progress']
+    )
+
+def _get_datapoints_helper(tag_id, aggregates=None, granularity=None, start=None, end=None, **kwargs):
+    '''Returns a list of datapoints for the given query.
+
+    This method will automate paging for the given time period.
+
+    Args:
+        tag_id (str):           The tag_id to retrieve data for.
+
+        aggregates (list):      The list of aggregate functions you wish to apply to the data. Valid aggregate functions
+                                are: 'average/avg, max, min, count, sum, interpolation/int, stepinterpolation/step'.
+
+        granularity (str):      The granularity of the aggregate values. Valid entries are : 'day/d, hour/h, minute/m,
+                                second/s', or a multiple of these indicated by a number as a prefix e.g. '12hour'.
+
+        start (Union[str, int, datetime]):    Get datapoints after this time. Format is N[timeunit]-ago where timeunit is w,d,h,m,s.
+                                    E.g. '2d-ago' will get everything that is up to 2 days old. Can also send time in ms since
+                                    epoch or a datetime object which will be converted to ms since epoch UTC.
+
+        end (Union[str, int, datetime]):      Get datapoints up to this time. Same format as for start.
+
+    Keyword Arguments:
+        protobuf (bool):        Download the data using the binary protobuf format. Only applicable when getting raw data.
+                                Defaults to True.
+
+        display_progress (bool):   Whether or not to display progress indicator.
+
+        api_key (str):          Your api-key.
+
+        project (str):          Project name.
+
+    Returns:
+        list of datapoints: A list containing datapoint dicts.
+    '''
+    api_key, project = kwargs.get('api_key'), kwargs.get('project')
     tag_id = tag_id.replace('/', '%2F')
     url = config.get_base_url() + '/projects/{}/timeseries/data/{}'.format(project, tag_id)
 
@@ -56,7 +140,7 @@ def get_datapoints(tag_id, aggregates=None, granularity=None, start=None, end=No
     if isinstance(end, datetime):
         end = _utils.datetime_to_ms(end)
 
-    use_protobuf = kwargs.get('protobuf', False)
+    use_protobuf = kwargs.get('protobuf', True) and aggregates is None
     limit = _constants.LIMIT if aggregates is None else _constants.LIMIT_AGG
 
     params = {
@@ -66,10 +150,12 @@ def get_datapoints(tag_id, aggregates=None, granularity=None, start=None, end=No
         'start': start,
         'end': end,
     }
+
     headers = {
         'api-key': api_key,
         'accept': 'application/protobuf' if use_protobuf else 'application/json'
     }
+
     prog_ind = _utils.ProgressIndicator([tag_id], start, end, api_key, project)
     datapoints = []
     while not datapoints or len(datapoints[-1]) == limit:
@@ -79,21 +165,23 @@ def get_datapoints(tag_id, aggregates=None, granularity=None, start=None, end=No
             ts_data.ParseFromString(res.content)
             res = [{'timestamp': p.timestamp, 'value': p.value} for p in ts_data.numericData.points]
         else:
-            res = res['data']['items'][0]['datapoints']
+            res = res.json()['data']['items'][0]['datapoints']
 
-        if not res and not datapoints:
-            prog_ind.terminate()
-            return DatapointsObject({'data': {'items': [{'tagId': tag_id, 'datapoints': []}]}})
+        if not res:
+            warning = "An interval with no data has been requested ({}, {})."\
+                .format(params['start'], params['end'])
+            warnings.warn(warning)
+            break
 
         datapoints.append(res)
         latest_timestamp = int(datapoints[-1][-1]['timestamp'])
         prog_ind.update_progress(latest_timestamp)
-        params['start'] = latest_timestamp + (_utils.granularity_to_ms(granularity) if granularity else 0)
+        params['start'] = latest_timestamp + (_utils.granularity_to_ms(granularity) if granularity else 1)
 
     prog_ind.terminate()
     dps = []
     [dps.extend(el) for el in datapoints]
-    return DatapointsObject({'data': {'items': [{'tagId': tag_id, 'datapoints': dps}]}})
+    return dps
 
 
 def get_latest(tag_id, **kwargs):
