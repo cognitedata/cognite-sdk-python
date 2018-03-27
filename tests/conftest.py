135186ee1098bb4bf069ab55a3fa5a2962aaee34
erlendcognite <erlend.vollset@cognite.com>
2018-03-27 16:34:26 +0200

    Add global fixture configure_test_session and MockReturnValue class

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+from unittest import mock
+
+import pytest
+from requests.structures import CaseInsensitiveDict
+
+from cognite.config import configure_session
+
+MOCK_API_KEY = 'SOME_KEY'
+MOCK_PROJECT = 'SOME_PROJECT'
+
+
+@pytest.fixture
+def configure_test_session():
+    configure_session(MOCK_API_KEY, MOCK_PROJECT)
+    yield
+    configure_session('', '')  # teardown
+
+
+class MockReturnValue(mock.Mock):
+    """Helper class for building mock request responses.
+
+    Should be assigned to mock.return_value
+    """
+
+    def __init__(self, status=200, content="CONTENT", json_data=None, raise_for_status=None,
+                 headers=CaseInsensitiveDict()
+                 ):
+        mock.Mock.__init__(self)
+        if "X-Request-Id" not in headers:
+            headers["X-Request-Id"] = "1234567890"
+
+        # mock raise_for_status call w/optional error
+        self.raise_for_status = mock.Mock()
+        if raise_for_status:
+            self.raise_for_status.side_effect = raise_for_status
+
+        # set status code and content
+        self.status_code = status
+
+        # requests.models.Response.ok mock
+        self.ok = status < 400
+        self.content = content
+        self.headers = headers
+
+        # add json data if provided
+        if json_data:
+            self.json = mock.Mock(
+                return_value=json_data
+            )
