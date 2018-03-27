9ca80c2e92ffd0b51f1fdaa875232901364c014a
erlendcognite <erlend.vollset@cognite.com>
2018-03-27 16:33:19 +0200

    Move api_key and project into kwargs in tagmatching method

--- cognite/tagmatching.py
+++ cognite/tagmatching.py
@@ -8,7 +8,7 @@ import cognite.config as config
 from cognite.data_objects import TagMatchingObject
 
 
-def tag_matching(tag_ids, fuzzy_threshold=0, platform=None, api_key=None, project=None):
+def tag_matching(tag_ids, fuzzy_threshold=0, platform=None, **kwargs):
     '''Returns a TagMatchingObject containing a list of matched tags for the given query.
 
     This method takes an arbitrary string as argument and performs fuzzy matching with a user defined threshold
@@ -22,6 +22,7 @@ def tag_matching(tag_ids, fuzzy_threshold=0, platform=None, api_key=None, projec
 
         platform (str):         The platform to search on.
 
+    Keyword Args:
         api_key (str):          Your api-key.
 
         project (str):          Project name.
@@ -30,7 +31,7 @@ def tag_matching(tag_ids, fuzzy_threshold=0, platform=None, api_key=None, projec
         TagMatchingObject: A data object containing the requested data with several getter methods with different
         output formats.
     '''
-    api_key, project = config.get_config_variables(api_key, project)
+    api_key, project = config.get_config_variables(kwargs.get('api_key'), kwargs.get('project'))
     url = config.get_base_url() + '/projects/{}/tagmatching'.format(project)
     body = {
         'tagIds': tag_ids,
