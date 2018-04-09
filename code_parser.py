dbf981399842ed79641f1e21a7fc2dae2d5a24f1
Erlend Vollset <31001374+erlendcognite@users.noreply.github.com>
2018-04-09 19:43:01 +0200

    py2.7 compatibility: Add script removing typehints from source code upon build in Jenkins. (#38)
    * Add code parser to remove typehints from source code.
    
    * Run code parser from jenkinsfile
    
    * Update pipfile
    
    * Run code parser in virtual environment.

---
+++ code_parser.py
@@ -0,0 +1,60 @@
+'''This module is used to remove typehints from source code to ensure python 2.7 compatibility.'''
+import argparse
+import ast
+import os
+import re
+import sys
+
+import astunparse
+
+
+class TypeHintRemover(ast.NodeTransformer):
+    def visit_FunctionDef(self, node):
+        # remove the return type defintion
+        node.returns = None
+        # remove all argument annotations
+        if node.args.args:
+            for arg in node.args.args:
+                arg.annotation = None
+        return node
+
+    def visit_Import(self, node):
+        node.names = [n for n in node.names if n.name != 'typing']
+        return node if node.names else None
+
+    def visit_ImportFrom(self, node):
+        return node if node.module != 'typing' else None
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-th', '--remove-type-hints', action='store_true')
+    parser.add_argument('--suppress-warning', action='store_true')
+
+    args = parser.parse_args()
+
+    dirs = [os.path.abspath(os.path.join(os.path.dirname(__file__), 'cognite', file)) for file in
+            os.listdir('./cognite') if
+            re.match('.+\.py$', file)]
+
+    if not args.suppress_warning:
+        if input("This will alter the source code of your project and should not be run without knowing what you're "
+                 "doing. Enter 'PEACOCK' to continue: ") != 'PEACOCK':
+            sys.exit(0)
+
+    for dir in dirs:
+        print(dir)
+
+        # parse the source code into an AST
+        with open(dir, 'r') as f:
+            parsed_source = ast.parse(f.read())
+
+        if args.remove_type_hints:
+            print("*****Removing type hints")
+            # remove all type annotations, function return type definitions
+            # and import statements from 'typing'
+            transformed = TypeHintRemover().visit(parsed_source)
+
+        with open(dir, 'w') as f:
+            f.write(astunparse.unparse(transformed))
+        print()
