293333e24009b9f299044169a6249aa49edee037
erlendcognite <erlend.vollset@cognite.com>
2018-04-19 17:22:30 +0200

    Remove everything related to 'tagId' in cloud_storage v05.

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,10 +13,8 @@ TEST_PROJECT = 'mltest'
 @pytest.fixture(scope='session', autouse=True)
 def configure_test_session():
     configure_session(TEST_API_KEY, TEST_PROJECT)
-    print("CONFTEST START")
     yield
     configure_session('', '')  # teardown
-    print("CONFTEST END")
 
 
 class MockReturnValue(mock.Mock):
