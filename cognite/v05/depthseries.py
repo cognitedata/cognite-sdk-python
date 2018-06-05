e91bbf8884e77be454ec5c5a8e8e8475bd66be6d
Kenneth Skaar <kenneth.skaar@cognite.com>
2018-06-05 12:25:37 +0200

    Fix minor bug

--- cognite/v05/depthseries.py
+++ cognite/v05/depthseries.py
@@ -149,7 +149,7 @@ def get_depthseries(prefix=None, description=None, include_metadata=False, asset
     while next_cursor and kwargs.get('autopaging'):
         params['cursor'] = next_cursor
         res = _utils.get_request(url=url, headers=headers, params=params, cookies=config.get_cookies())
-        timeseries.extend([ts for ts in res.json()['data']['items'] if not ts['name'].endswith(_generateIndexName(""))])
+        timeseries.extend([ts for ts in res.json()['data']['items'] ])
         next_cursor = res.json()['data'].get('nextCursor')
 
     return TimeSeriesResponse(
