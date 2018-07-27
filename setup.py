5f7d6a4fba35fd8d1397130a168f4d0218276c1a
erlendcognite <erlend.vollset@cognite.com>
2018-07-27 13:38:38 +0200

    Add check to make sure 'tests' is in packages before removing it. Also bump to 0.8.13

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-from setuptools import setup, find_packages
-
 import cognite
+from setuptools import find_packages, setup
 
 packages = find_packages()
-packages.remove('tests')
+if "tests" in packages:
+    packages.remove("tests")
 
-setup(name='cognite-sdk',
-      version=cognite.__version__,
-      description='Cognite API SDK for Python',
-      url='http://cognite-sdk-python.readthedocs.io/',
-      download_url='https://github.com/cognitedata/cognite-sdk-python/archive/{}.tar.gz'.format(cognite.__version__),
-      author='Erlend Vollset',
-      author_email='erlend.vollset@cognite.com',
-      packages=packages,
-      install_requires=[
-          'requests',
-          'pandas',
-          'protobuf',
-          'cognite-logger'
-      ],
-      zip_safe=False,
-      include_package_data=True)
+setup(
+    name="cognite-sdk",
+    version=cognite.__version__,
+    description="Cognite API SDK for Python",
+    url="http://cognite-sdk-python.readthedocs.io/",
+    download_url="https://github.com/cognitedata/cognite-sdk-python/archive/{}.tar.gz".format(cognite.__version__),
+    author="Erlend Vollset",
+    author_email="erlend.vollset@cognite.com",
+    packages=packages,
+    install_requires=["requests", "pandas", "protobuf", "cognite-logger"],
+    zip_safe=False,
+    include_package_data=True,
+)
