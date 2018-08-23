eafeb9f1144a1bef94da0782b0daf3a301d3c6e2
Stian Lågstad <stianlagstad@gmail.com>
2018-08-23 12:03:08 +0100

    Bump version to trigger release

--- cognite/__init__.py
+++ cognite/__init__.py
@@ -20,4 +20,4 @@ Data Platform (CDP).
 from cognite.data_transfer_service import DataTransferService
 
 __all__ = ["v04", "v05", "preprocessing", "config", "data_transfer_service"]
-__version__ = "0.9.2"
+__version__ = "0.9.3"
