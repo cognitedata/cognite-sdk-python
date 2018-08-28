375381e95d44633b2f6ff70b60472b1083738ed8
Stian Lågstad <stianlagstad@gmail.com>
2018-08-28 15:08:09 +0200

    Specify the major.minor version in setup.py also

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author="Erlend Vollset",
     author_email="erlend.vollset@cognite.com",
     packages=packages,
-    install_requires=["requests", "pandas", "protobuf", "cognite-logger"],
+    install_requires=["requests", "pandas", "protobuf", "cognite-logger>=0.3"],
     zip_safe=False,
     include_package_data=True,
 )
