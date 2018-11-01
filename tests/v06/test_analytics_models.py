308372abd31eeed010efed0f172b2df95b525d4c
Erlend Vollset <31001374+erlendcognite@users.noreply.github.com>
2018-11-01 15:17:03 +0100

    Update models client and fix issues in data transfer service (#138)
    * Add runtime version parameter for uploading sp and fix incorrect schema field names
    
    * Update docstrings
    
    * Move runtimeVersion parameter to correct method
    
    * Fix doc issues and bump version to 0.11.9
    
    * Create readthedocs config specifying to build with python 3.6 and no longer remove type-hints
    
    * Fix bug in v05 timeseries module
    
    * Add support for schedules in models client
    
    * Bump version to 0.11.9
    
    * Rename some methods in models client
    
    * Add typehints and update docstrings
    
    * Update typehints
    
    * Rename upload_source_package -> create_source_package
    
    * Add method for deleting version
    
    * Fix issue when serializing DataSpec with FilesDataSpec
    
    * Update data_transfer_service json dump load test to include files data spec
    
    * Remove default runtime_version
    
    * Update mocker spies for methods with changed names
    
    * Update incorrect patch
    
    * Write some integration tests to increase coverage

---
+++ tests/v06/test_analytics_models.py
@@ -0,0 +1,32 @@
+from random import randint
+
+import pytest
+from cognite.v06.analytics import models
+
+
+@pytest.fixture
+def created_model():
+    model_name = f"test-model-{randint(0, 1e5)}"
+    model = models.create_model(name=model_name)
+    yield model
+    models.delete_model(model["id"])
+
+
+@pytest.fixture
+def created_source_package():
+    sp_name = f"test-sp-{randint(0, 1e5)}"
+    sp = models.create_source_package(
+        name=sp_name, package_name="whatever", available_operations=["TRAIN", "PREDICT"], runtime_version="0.1"
+    )
+    yield sp
+    models.delete_source_package(source_package_id=sp["id"])
+
+
+class TestModels:
+    def test_get_model(self, created_model):
+        model = models.get_model(created_model["id"])
+        assert model["name"] == created_model["name"]
+
+    def test_get_source_package(self, created_source_package):
+        sp = models.get_source_package(created_source_package["id"])
+        assert sp["id"] == created_source_package["id"]
