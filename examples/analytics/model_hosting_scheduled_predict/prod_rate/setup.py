6b1e776561074f9ffc5eac270f98d8efb7aa2131
Nils Barlaug <nils.barlaug@cognite.com>
2018-11-15 22:48:08 +0100

    Add example notebook creating schedule in Model Hosting

---
+++ examples/analytics/model_hosting_scheduled_predict/prod_rate/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+# It's important to specify all dependencies on external packages,
+# so that these can be installed.
+# There's no need to specify cognite-sdk (it's installed by default)
+REQUIRED_PACKAGES = ["scikit-learn==0.19.1", "numpy>=1.8.2", "scipy>=0.13.3"]
+
+setup(
+    name="prod-rate",
+    version="0.1",
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    description="A random forrest regressor used to find production rate for abc equipment",
+    url="https://relevant.webpage",
+    maintainer="Tutorial",
+    maintainer_email="Tutorial"
+)
