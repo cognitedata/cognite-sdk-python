f2a98393cc920e8a10bd8d7a94c7b9c0e73c6325
erlendcognite <erlend.vollset@cognite.com>
2018-11-27 16:42:22 +0100

    Simplify type_hint_remover

--- type_hint_remover.py
+++ type_hint_remover.py
@@ -1,32 +1,24 @@
-"""This module is used to remove typehints from source code to ensure python 2.7 and 3.5 compatibility."""
+"""This module is used to remove typehints from source code to ensure python 3.5 compatibility."""
 import os
 import re
 
 import strip_hints
 
+SKIP_DIRECTORIES = ["auxiliary", "__pycache__"]
+PYTHON_FILE_PATTERN = r"^.+\.py$"
+
+
+def strip_hints_and_overwrite(file_path):
+    print("*****Removing type hints in {}".format(file_path))
+    transformed = strip_hints.strip_file_to_string(file_path, to_empty=True, only_assigns_and_defs=True)
+    with open(file_path, "w") as f:
+        f.write(transformed)
+
+
 if __name__ == "__main__":
-    files = [
-        os.path.abspath(os.path.join(os.path.dirname(__file__), "cognite", file))
-        for file in os.listdir("./cognite")
-        if re.match(".+\.py$", file)
-    ]
-
-    version_dirs = [
-        os.path.abspath(os.path.join(os.path.dirname(__file__), "cognite", file))
-        for file in os.listdir("./cognite")
-        if re.match("^v\d\d$", file)
-    ]
-    for dir in version_dirs:
-        version_files = [
-            os.path.abspath(os.path.join(dir, file)) for file in os.listdir(dir) if re.match(".+\.py$", file)
-        ]
-        files.extend(version_files)
-
-    for file_path in files:
-        print("*****Removing type hints in {}".format(file_path))
-
-        transformed = strip_hints.strip_file_to_string(file_path, to_empty=True, only_assigns_and_defs=True)
-
-        with open(file_path, "w") as f:
-            f.write(transformed)
-        print()
+    for root, _, files in os.walk("cognite"):
+        if not any(dir_name in root for dir_name in SKIP_DIRECTORIES):
+            for file in files:
+                if re.match(PYTHON_FILE_PATTERN, file):
+                    file_path = "{}/{}".format(root, file)
+                    strip_hints_and_overwrite(file_path)
