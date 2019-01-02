2c416975630f4b8d553d7e80b9e7df3c97fc89ca
erlendcognite <erlend.vollset@cognite.com>
2019-01-02 19:32:11 +0100

    Remove docstring from top-level __init__.py

--- cognite/__init__.py
+++ cognite/__init__.py
@@ -1,9 +1,3 @@
-"""Cognite API SDK
-
-This package has been created to ensure excellent excellent user experience Python programmers using the Cognite
-Data Platform (CDP).
-"""
-
 from cognite.client.cognite_client import CogniteClient
 from cognite.client.exceptions import APIError
 
