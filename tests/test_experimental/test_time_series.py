2c211acfa0b3b4d9c5d3f3f7e7bd69aa8fe81e9e
Dmytro Shynkevych <dm.shynk@gmail.com>
2019-01-08 09:46:15 -0500

    Added delete_time_series_by_id

--- tests/test_experimental/test_time_series.py
+++ tests/test_experimental/test_time_series.py
@@ -3,11 +3,13 @@ from random import randint
 import pytest
 
 from cognite import CogniteClient
+from cognite.client.stable.time_series import TimeSeries
 from cognite.client.experimental.time_series import TimeSeriesClient, TimeSeriesResponse
 from tests.conftest import TEST_TS_1_NAME
 
-time_series = CogniteClient().experimental.time_series
-
+_client = CogniteClient()
+time_series = _client.experimental.time_series
+stable_time_series = _client.time_series
 
 TS_NAME = None
 
@@ -19,6 +21,16 @@ def ts_name():
 
 
 class TestTimeseries:
+    @pytest.fixture(scope="class")
+    def created_ts_id(self):
+        tso = TimeSeries(TS_NAME)
+        res = stable_time_series.post_time_series([tso])
+        yield time_series.search_for_time_series().to_json()[0]["id"]
+
+    def test_delete_time_series_by_id(self, created_ts_id):
+        res = time_series.delete_time_series_by_id([created_ts_id])
+        assert res is None
+
     @pytest.fixture(scope="class")
     def get_time_series_by_id_response_obj(self, time_series_in_cdp):
         yield time_series.get_time_series_by_id(id=time_series_in_cdp[0])
