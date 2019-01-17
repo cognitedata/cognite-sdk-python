7934ca169e4d991296d584c047bd284d41e81e32
erlendcognite <erlend.vollset@cognite.com>
2019-01-17 17:10:14 +0100

    Cast env config to int where appropriate
    Also move resolution of env vars into client __init__()

--- tests/test_client/test_cognite_client.py
+++ tests/test_client/test_cognite_client.py
@@ -22,6 +22,21 @@ def default_client_config():
     yield DEFAULT_BASE_URL, DEFAULT_NUM_OF_RETRIES, DEFAULT_NUM_OF_WORKERS, DEFAULT_TIMEOUT
 
 
+@pytest.fixture
+def environment_client_config():
+    base_url = "blabla"
+    num_of_retries = "1"
+    num_of_workers = "1"
+    timeout = "10"
+
+    os.environ["COGNITE_BASE_URL"] = base_url
+    os.environ["COGNITE_NUM_RETRIES"] = num_of_retries
+    os.environ["COGNITE_NUM_WORKERS"] = num_of_workers
+    os.environ["COGNITE_TIMEOUT"] = timeout
+
+    yield base_url, num_of_retries, num_of_workers, timeout
+
+
 class TestCogniteClient:
     def test_get(self, client):
         res = client.get("/login/status")
@@ -45,10 +60,10 @@ class TestCogniteClient:
         assert client._project == "mltest"
 
     def assert_config_is_correct(self, client, base_url, num_of_retries, num_of_workers, timeout):
-        assert client._base_url == base_url
-        assert client._num_of_retries == num_of_retries
-        assert client._num_of_workers == num_of_workers
-        assert client._timeout == timeout
+        assert str(client._base_url) == base_url
+        assert str(client._num_of_retries) == num_of_retries
+        assert str(client._num_of_workers) == num_of_workers
+        assert str(client._timeout) == timeout
 
     def test_default_config(self, client, default_client_config):
         self.assert_config_is_correct(client, *default_client_config)
@@ -67,3 +82,7 @@ class TestCogniteClient:
             timeout=timeout,
         )
         self.assert_config_is_correct(client, base_url, num_of_retries, num_of_workers, timeout)
+
+    def test_environment_config(self, environment_client_config):
+        client = CogniteClient(project="something")
+        self.assert_config_is_correct(client, *environment_client_config)
