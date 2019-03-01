1e29516bdf9b9b532bb0a0b4f7d42b7a9c68c73b
erlendcognite <erlend.vollset@cognite.com>
2019-03-01 20:08:50 +0100

    Decode before json.loads()

--- tests/utils.py
+++ tests/utils.py
@@ -5,5 +5,5 @@ import json
 def get_call_args_data_from_mock(mock, index, decompress_gzip=False):
     data = mock.call_args_list[index][1]["data"]
     if decompress_gzip:
-        data = gzip.decompress(data)
+        data = gzip.decompress(data).decode()
     return json.loads(data)
