3e52e994ffc386eac92d0bca343f65d081cce1f9
erlendcognite <erlend.vollset@cognite.com>
2019-04-14 13:30:17 +0200

    Add decorator for profiling

--- tests/tests_unit/test_api/test_datapoints.py
+++ tests/tests_unit/test_api/test_datapoints.py
@@ -1,5 +1,4 @@
 import json
-import time
 from datetime import datetime
 from random import random
 
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,5 @@
+import cProfile
+import functools
 import gzip
 import json
 
@@ -6,3 +8,15 @@ BASE_URL = "https://api.cognitedata.com"
 
 def jsgz_load(s):
     return json.loads(gzip.decompress(s).decode())
+
+
+def profile(method):
+    @functools.wraps(method)
+    def wrapper(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        method(*args, **kwargs)
+        pr.disable()
+        pr.print_stats(sort="cumtime")
+
+    return wrapper
