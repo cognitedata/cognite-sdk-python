71e6be4d8e576ee017e5f72e249e3bfd4fd5cca5
erlendcognite <erlend.vollset@cognite.com>
2019-05-28 10:40:02 +0200

    Update readme and a docstring

--- cognite/client/_cognite_client.py
+++ cognite/client/_cognite_client.py
@@ -31,9 +31,9 @@ class CogniteClient:
     Args:
         api_key (str): API key
         project (str): Project. Defaults to project of given API key.
-        client_name (str): A name for the client. Used to identify number of unique applications/scripts.
+        client_name (str): A user-defined name for the client. Used to identify number of unique applications/scripts
+            running on top of CDF.
         base_url (str): Base url to send requests to. Defaults to "https://api.cognitedata.com"
-
         max_workers (int): Max number of workers to spawn when parallelizing data fetching. Defaults to 10.
         headers (Dict): Additional headers to add to all requests.
         timeout (int): Timeout on requests sent to the api. Defaults to 30 seconds.
