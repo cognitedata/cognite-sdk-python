30e7b8c7dcadb2e818ec67478bdcce262886fd9f
Even Wiik Thomassen <even.thomassen@cognite.com>
2019-07-10 11:19:53 +0200

    Fixed copyright notice to say Cognite AS.
    Signed-off-by: Even Wiik Thomassen <even.thomassen@cognite.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "cognite-sdk"
-copyright = "2019, Cognite"
+copyright = "2019, Cognite AS"
 author = "Erlend Vollset"
 
 # The version info for the project you're documenting, acts as replacement for
