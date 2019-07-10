6d48c4d67f0d27cc5fbfde49f0103ba81abe7641
erlendcognite <erlend.vollset@cognite.com>
2019-07-10 17:17:02 +0200

    Fix AssetList unit tests

--- tests/tests_unit/test_data_classes/test_assets.py
+++ tests/tests_unit/test_data_classes/test_assets.py
@@ -56,21 +56,21 @@ class TestAsset:
 class TestAssetList:
     def test_get_events(self):
         c.events.list = mock.MagicMock()
-        a = Asset(id=1, cognite_client=c)
+        a = AssetList(resources=[Asset(id=1)], cognite_client=c)
         a.events()
-        assert c.events.list.call_args == call(asset_ids=[1])
+        assert c.events.list.call_args == call(asset_ids=[1], limit=-1)
         assert c.events.list.call_count == 1
 
     def test_get_time_series(self):
         c.time_series.list = mock.MagicMock()
-        a = Asset(id=1, cognite_client=c)
+        a = AssetList(resources=[Asset(id=1)], cognite_client=c)
         a.time_series()
-        assert c.time_series.list.call_args == call(asset_ids=[1])
+        assert c.time_series.list.call_args == call(asset_ids=[1], limit=-1)
         assert c.time_series.list.call_count == 1
 
     def test_get_files(self):
         c.files.list = mock.MagicMock()
-        a = Asset(id=1, cognite_client=c)
+        a = AssetList(resources=[Asset(id=1)], cognite_client=c)
         a.files()
-        assert c.files.list.call_args == call(asset_ids=[1])
+        assert c.files.list.call_args == call(asset_ids=[1], limit=-1)
         assert c.files.list.call_count == 1
