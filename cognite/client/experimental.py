60fff2103e745b789ebeb3433078530775dcc449
erlendcognite <erlend.vollset@cognite.com>
2019-07-26 12:11:59 +0200

    Display unknown fields in error message returned from API

--- cognite/client/experimental.py
+++ cognite/client/experimental.py
@@ -6,5 +6,5 @@ from cognite.client._cognite_client import CogniteClient as Client
 class CogniteClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.sequences = SequencesAPI(self._config, api_version=self.API_VERSION, cognite_client=self)
+        self.sequences = SequencesAPI(self._config, api_version=self._API_VERSION, cognite_client=self)
         self.model_hosting = ModelHostingAPI(self._config, api_version="0.6", cognite_client=self)
--- tests/tests_unit/test_exceptions.py
+++ tests/tests_unit/test_exceptions.py
@@ -1,4 +1,6 @@
+from cognite.client import CogniteClient
 from cognite.client.exceptions import CogniteAPIError
+from tests.utils import set_env_var
 
 
 class TestAPIError:
@@ -18,3 +20,19 @@ class TestAPIError:
         assert [{"externalId": "abc"}] == e.duplicated
 
         assert "bla" in e.__str__()
+
+    def test_unknown_fields_in_api_error(self, rsps):
+        with set_env_var("COGNITE_DISABLE_PYPI_VERSION_CHECK", "1"):
+            c = CogniteClient()
+        print(c.assets._get_base_url_with_base_path() + "/any")
+        rsps.add(
+            rsps.GET,
+            c.assets._get_base_url_with_base_path() + "/any",
+            status=400,
+            json={"error": {"message": "bla", "extra": {"haha": "blabla"}, "other": "yup"}},
+        )
+        try:
+            c.get(url="/api/v1/projects/{}/any".format(c.config.project))
+            assert False, "Call did not raise exception"
+        except CogniteAPIError as e:
+            assert {"extra": {"haha": "blabla"}, "other": "yup"} == e.extra
