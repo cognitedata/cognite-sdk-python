badb096c25071e9bf54a9f56c65d8a8ff5946776
Sander Land <sander.land@cognite.com>
2019-08-23 09:58:49 +0200

    openapi gen

--- cognite/client/data_classes/relationships.py
+++ cognite/client/data_classes/relationships.py
@@ -4,29 +4,50 @@ from typing import *
 from cognite.client.data_classes._base import *
 
 
-# GenClass: relationship
+# GenClass: relationshipResponse
 class Relationship(CogniteResource):
+    """No description.
+
+    Args:
+        source (Dict[str, Any]): If resource is `threeD` or `threeDRevision` the `resourceId` is a set of internal ids concatenated by a colons. Otherwise, the resourceId follows the formatting rules as described in `resourceId`.
+
+If resource id of type `threeD`, the externalId must follow the pattern `<nodeId>:<modelId>:<revisionId>`. If resource id of type `threeDRevision`, the externalId must follow the pattern `<revisionId>:<modelId>`. The values `<nodeId>`, `<modelId>` and `<revisionId>` are the corresponding internal ids to identify the referenced resource uniquely.
+        target (Dict[str, Any]): If resource is `threeD` or `threeDRevision` the `resourceId` is a set of internal ids concatenated by a colons. Otherwise, the resourceId follows the formatting rules as described in `resourceId`.
+
+If resource id of type `threeD`, the externalId must follow the pattern `<nodeId>:<modelId>:<revisionId>`. If resource id of type `threeDRevision`, the externalId must follow the pattern `<revisionId>:<modelId>`. The values `<nodeId>`, `<modelId>` and `<revisionId>` are the corresponding internal ids to identify the referenced resource uniquely.
+        start_time (float): Unix timestamp representing when a relationship among the resources was established (physically).
+        end_time (float): Unix timestamp representing when a relationship among the resources stopped to exist (physically).
+        confidence (float): Confidence value of the existence of this relationship. Humans should enter 1.0 usually, generated relationships should provide a realistic score on the likelihood of the existence of the relationship.
+Generated relationships should never have the a confidence score of 1.0.
+        data_set (str): String describing the source system storing or generating the relationship.
+        external_id (str): Disallowing leading and trailing whitespaces. Case sensitive. The external Id must be unique within the project.
+        relationship_type (str): No description.
+        created_time (float): No description.
+        last_updated_time (float): No description.
+        cognite_client (CogniteClient): The client to associate with this object.
+    """
+
     def __init__(
         self,
-        external_id: str = None,
-        source: str = None,
-        target: int = None,
-        start_time: int = None,
-        end_time: int = None,
+        source: Dict[str, Any] = None,
+        target: Dict[str, Any] = None,
+        start_time: float = None,
+        end_time: float = None,
         confidence: float = None,
         data_set: str = None,
+        external_id: str = None,
         relationship_type: str = None,
-        created_time: int = None,
-        last_updated_time: int = None,
+        created_time: float = None,
+        last_updated_time: float = None,
         cognite_client=None,
     ):
-        self.external_id = external_id
         self.source = source
         self.target = target
         self.start_time = start_time
         self.end_time = end_time
         self.confidence = confidence
         self.data_set = data_set
+        self.external_id = external_id
         self.relationship_type = relationship_type
         self.created_time = created_time
         self.last_updated_time = last_updated_time
