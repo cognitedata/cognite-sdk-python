39a473de76d6d390dbc63783a8ae5b54b35a9952
Henrik Eiding <henrik.eiding@cognite.com>
2019-11-01 12:57:51 +0100

    Revert "Reactivate skipped tests."
    This reverts commit 049c76693387295153fba7ce4a6874aa5c1135dd.

--- tests/tests_integration/test_api/test_model_hosting/test_models.py
+++ tests/tests_integration/test_api/test_model_hosting/test_models.py
@@ -49,11 +49,13 @@ class TestModels:
         )
         yield rsps
 
+    @pytest.mark.skip(reason="wait for model hosting v1")
     def test_get_model(self, created_model):
         model = MODELS_API.get_model(created_model.name)
         assert model.name == created_model.name
         assert model.active_version_id is None
 
+    @pytest.mark.skip(reason="wait for model hosting v1")
     def test_list_models(self, created_model):
         res = MODELS_API.list_models()
         assert len(res) > 0
@@ -63,6 +65,7 @@ class TestModels:
         for model in res:
             assert isinstance(model, Model)
 
+    @pytest.mark.skip(reason="wait for model hosting v1")
     def test_update_model(self, created_model):
         res = MODELS_API.update_model(name=created_model.name, description="bla")
         assert isinstance(res, Model)
@@ -78,6 +81,7 @@ class TestModels:
         with pytest.raises(PredictionError, match="User error"):
             MODELS_API.online_predict(model_name="model1")
 
+    @pytest.mark.skip(reason="wait for model hosting v1")
     def test_deprecate_model(self, created_model):
         res = MODELS_API.deprecate_model(name=created_model.name)
         assert isinstance(res, Model)
