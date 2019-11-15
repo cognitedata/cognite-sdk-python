1aa1889ac580b949b93a30c36b3adbaa1c68cd83
Henrik Mathias Eiding <51706349+HMEiding@users.noreply.github.com>
2019-11-15 09:14:44 +0100

    [DT-513] Breaking changes Model Hosting v1 (#596)
    * Update models, versions and schedules.
    
    * Fix typehint.
    
    * Update tests.
    
    * Fix bugs.
    
    * Client now needs to do post requests without body.
    
    * More test updates (temporarily skipping some until nostromo v1 deployment)
    
    * Make alpha-release
    
    * Revert "Make alpha-release"
    
    This reverts commit 559a6e9a38ed21bb2f554d3c43d2033aad05e440.
    
    * Remove data- and items- wrappers.
    
    * put deprecate -> post deprecate for sourcepackage.
    
    * Reactivate skipped tests.
    
    * Revert "Reactivate skipped tests."
    
    This reverts commit 049c76693387295153fba7ce4a6874aa5c1135dd.
    
    * Skip sourcepackage-tests until v1 deployment.
    
    * Reactivate skipped model hosting tests.
    
    * Replace active_version_id -> active_version_name.
    
    * Create model_hosting.versions API.
    
    * Update cognite.rst with model_hosting.versions API.
    
    * Update version no. and CHANGELOG.md
    
    * Update CHANGELOG.md and docstring after review.
    
    * Reactivate disabled tests.
    
    * Seperate versions from models in data_classes.
    
    * Update docs/source/cognite.rst
    
    Co-Authored-By: Erlend vollset <erlend.vollset@cognite.com>

--- cognite/client/_api/model_hosting/__init__.py
+++ cognite/client/_api/model_hosting/__init__.py
@@ -1,6 +1,7 @@
 from cognite.client._api.model_hosting.models import ModelsAPI
 from cognite.client._api.model_hosting.schedules import SchedulesAPI
 from cognite.client._api.model_hosting.source_packages import SourcePackagesAPI
+from cognite.client._api.model_hosting.versions import ModelVersionsAPI
 from cognite.client._api_client import APIClient
 
 
@@ -8,5 +9,6 @@ class ModelHostingAPI(APIClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.models = ModelsAPI(*args, **kwargs)
+        self.versions = ModelVersionsAPI(*args, **kwargs)
         self.source_packages = SourcePackagesAPI(*args, **kwargs)
         self.schedules = SchedulesAPI(*args, **kwargs)
