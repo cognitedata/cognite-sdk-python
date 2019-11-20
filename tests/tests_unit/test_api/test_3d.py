c8a9d0d3c1f892903de496d7eb034cf8b9531fd4
Sander Land <sander.land@cognite.com>
2019-11-20 09:32:36 +0100

    fix 3d list

--- tests/tests_unit/test_api/test_3d.py
+++ tests/tests_unit/test_api/test_3d.py
@@ -43,6 +43,14 @@ class Test3DModels:
         assert isinstance(res, ThreeDModelList)
         assert mock_3d_model_response.calls[0].response.json()["items"] == res.dump(camel_case=True)
 
+    def test_list_published_default(self, mock_3d_model_response):
+        res = THREE_D_API.models.list(limit=100)
+        assert isinstance(res, ThreeDModelList)
+        assert mock_3d_model_response.calls[0].response.json()["items"] == res.dump(camel_case=True)
+        assert {"limit": 100, "published": None, "cursor": None} == jsgz_load(
+            mock_3d_model_response.calls[0].request.body
+        )
+
     def test_update_with_update_object(self, mock_3d_model_response):
         update = ThreeDModelUpdate(id=1).name.set("bla")
         res = THREE_D_API.models.update(update)
