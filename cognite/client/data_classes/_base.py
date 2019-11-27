02347f8728fe088482fb3ea64974f0f3b85a7098
sanderland <48946947+sanderland@users.noreply.github.com>
2019-11-27 16:34:57 +0100

    dont camel case notebook output / asset subtree filtering / 1.4.2 (#618)

--- cognite/client/data_classes/_base.py
+++ cognite/client/data_classes/_base.py
@@ -104,20 +104,21 @@ class CogniteResource:
             return instance
         raise TypeError("Resource must be json str or Dict, not {}".format(type(resource)))
 
-    def to_pandas(self, expand: List[str] = ("metadata",), ignore: List[str] = None):
+    def to_pandas(self, expand: List[str] = ("metadata",), ignore: List[str] = None, camel_case: bool = True):
         """Convert the instance into a pandas DataFrame.
 
         Args:
             expand (List[str]): List of row keys to expand, only works if the value is a Dict.
                 Will expand metadata by default.
             ignore (List[str]): List of row keys to not include when converting to a data frame.
+            camel_case (bool): Convert column names to camel case (e.g. `externalId` instead of `external_id`)
 
         Returns:
             pandas.DataFrame: The dataframe.
         """
         ignore = [] if ignore is None else ignore
         pd = utils._auxiliary.local_import("pandas")
-        dumped = self.dump(camel_case=True)
+        dumped = self.dump(camel_case=camel_case)
 
         for element in ignore:
             del dumped[element]
@@ -134,7 +135,7 @@ class CogniteResource:
         return df
 
     def _repr_html_(self):
-        return self.to_pandas()._repr_html_()
+        return self.to_pandas(camel_case=False)._repr_html_()
 
 
 class CognitePropertyClassUtil:
