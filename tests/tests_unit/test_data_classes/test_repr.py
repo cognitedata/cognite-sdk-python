821071df210073b1a77393f6027aed30a3ce520b
Sander Land <sander.land@cognite.com>
2019-12-03 10:50:56 +0100

    repr html instead of kwargs and tests

---
+++ tests/tests_unit/test_data_classes/test_repr.py
@@ -0,0 +1,22 @@
+from unittest import mock
+from unittest.mock import call
+
+import pytest
+
+from cognite.client.data_classes import *
+from cognite.client.experimental import CogniteClient
+
+c = CogniteClient()
+
+
+class TestRepr:
+    def test_repr_html(self):
+        for cls in [Asset, Datapoints, Sequence, FileMetadata, Row, Table, ThreeDModel]:
+            assert len(cls()._repr_html_()) > 0
+
+    def test_repr_html_datapoint(self):
+        assert len(Datapoint(timestamp=0, value=0)._repr_html_()) > 0
+
+    def test_repr_html_list(self):
+        for cls in [AssetList, DatapointsList, RowList, TableList]:
+            assert len(cls([cls._RESOURCE()])._repr_html_()) > 0
