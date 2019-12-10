fb6b8923e0a5a6871065fb50a62c34da25231017
Sander Land <sander.land@cognite.com>
2019-12-10 14:01:11 +0100

    additional features for synthetic time series

--- tests/tests_unit/test_api/test_synthetic_time_series.py
+++ tests/tests_unit/test_api/test_synthetic_time_series.py
@@ -88,3 +88,16 @@ class TestSyntheticQuery:
         assert isinstance(dps_res, Datapoints)
         assert 0 == len(dps_res)
         assert 1 == len(mock_get_datapoints_empty.calls)
+
+    @pytest.mark.dsl
+    def test_expression_builder(self):
+        from sympy import symbols
+
+        assert "ts{externalId:'x'}" == DPS_CLIENT._build_expression(symbols("a"), {"a": "x"})
+        assert "ts{externalId:'x',aggregate:'average',granularity:'1m'}" == DPS_CLIENT._build_expression(
+            symbols("a"), {"a": "x"}, aggregate="average", granularity="1m"
+        )
+        assert "(ts{externalId:'x'}+ts{externalId:'y'}+ts{externalId:'z'})" == DPS_CLIENT._build_expression(
+            symbols("a") + symbols("b") + symbols("c"), {"a": "x", "b": "y", "c": "z"}
+        )
+        assert "(1/ts{externalId:'a'})" == DPS_CLIENT._build_expression(1 / symbols("a"), {"a": "a"})
