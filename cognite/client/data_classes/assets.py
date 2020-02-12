a039e24d1ef7808e6f0919dc9ec6568beae4b6a6
Fredrik Anfinsen <fredrik.anfinsen@cognite.com>
2020-02-12 12:31:57 +0100

    feat: return parentExternalId for assets (#650)
    * feat: return parentExternalId for assets
    
    * fix/disable generator tests
    
    * docs: warning about asset link resolvement
    
    * feat: added missing aggregate properties
    
    * test: fix snapshot of generated asset class
    
    * test: update snapshot for assets generated from OpenAPI spec
    
    Co-authored-by: sanderland <48946947+sanderland@users.noreply.github.com>

--- cognite/client/data_classes/assets.py
+++ cognite/client/data_classes/assets.py
@@ -11,13 +11,19 @@ class AggregateResultItem(dict):
 
     Args:
         child_count (int): Number of direct descendants for the asset
+        depth (int): Asset path depth (number of levels below root node).
+        path (List[Dict[str, Any]]): IDs of assets on the path to the asset.
     """
 
-    def __init__(self, child_count: int = None, **kwargs):
+    def __init__(self, child_count: int = None, depth: int = None, path: List[Dict[str, Any]] = None, **kwargs):
         self.child_count = child_count
+        self.depth = depth
+        self.path = path
         self.update(kwargs)
 
     child_count = CognitePropertyClassUtil.declare_property("childCount")
+    depth = CognitePropertyClassUtil.declare_property("depth")
+    path = CognitePropertyClassUtil.declare_property("path")
 
     # GenStop
 
@@ -38,7 +44,7 @@ class Asset(CogniteResource):
         last_updated_time (int): The number of milliseconds since 00:00:00 Thursday, 1 January 1970, Coordinated Universal Time (UTC), minus leap seconds.
         root_id (int): A server-generated ID for the object.
         aggregates (Union[Dict[str, Any], AggregateResultItem]): Aggregated metrics of the asset
-        parent_external_id (str): The external ID of the parent. This will be resolved to an internal ID and stored as `parentId`.
+        parent_external_id (str): The external ID of the parent. The property is omitted if the asset doesn't have a parent or if the parent doesn't have externalId.
         cognite_client (CogniteClient): The client to associate with this object.
     """
 
