caf58e90c53b6c60b30662042bdfa25471e1a711
sanderland <48946947+sanderland@users.noreply.github.com>
2020-03-27 13:56:15 +0100

    fix openapi utils (#667)
    * fix openapi utils

--- openapi/utils.py
+++ openapi/utils.py
@@ -40,6 +40,17 @@ def get_type_hint(item):
             return types[0]
         else:
             return "Union[{}]".format(", ".join(types))
+    elif "allOf" in item and "type" in item["allOf"][0]:
+        types = []
+        for current in item["allOf"]:
+            types.append(get_type_hint(current))
+        types = list(set(types))
+        if len(types) == 1:
+            return types[0]
+        else:
+            return "Union[{}]".format(", ".join(types))
+    else:
+        raise "Unrecognized type in '{}'".format(item)
 
     if type == "array":
         return "List[{}]".format(get_type_hint(item["items"]))
