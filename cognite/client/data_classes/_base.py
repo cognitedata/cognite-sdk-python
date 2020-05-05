176c2e0ae9cadf53704131bac7657da8626e58e1
sanderland <48946947+sanderland@users.noreply.github.com>
2020-05-05 13:22:09 +0200

    nullable int (#678)

--- cognite/client/data_classes/_base.py
+++ cognite/client/data_classes/_base.py
@@ -234,7 +234,7 @@ class CogniteResourceList(UserList):
         """
         pd = utils._auxiliary.local_import("pandas")
         df = pd.DataFrame(self.dump(camel_case=camel_case))
-        nullable_int_fields = ["startTime", "endTime", "assetId", "parentId"]
+        nullable_int_fields = ["startTime", "endTime", "assetId", "parentId", "datasetId"]
         if not camel_case:
             nullable_int_fields = [utils._auxiliary.to_snake_case(f) for f in nullable_int_fields]
         try:
