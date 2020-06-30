89bd9df115e070e6c3ca8ff063738702e67cbe71
Michael Susæg <michael.susag@cognite.com>
2020-06-30 15:50:56 +0200

    Handle asset mappings to delete in a better way

--- cognite/client/_api/three_d.py
+++ cognite/client/_api/three_d.py
@@ -547,11 +547,11 @@ class ThreeDAssetMappingAPI(APIClient):
         path = utils._auxiliary.interpolate_and_url_encode(self._RESOURCE_PATH, model_id, revision_id)
         utils._auxiliary.assert_type(asset_mapping, "asset_mapping", [list, ThreeDAssetMapping])
         if isinstance(asset_mapping, ThreeDAssetMapping):
-            asset_mapping = [asset_mapping]
+            asset_mapping = [ThreeDAssetMapping(asset_mapping.node_id, asset_mapping.asset_id)]
         chunks = utils._auxiliary.split_into_chunks(
             [a.dump(camel_case=True) for a in asset_mapping], self._DELETE_LIMIT
         )
-        tasks = [{"url_path": path + "/delete", "json": {"items": { "nodeId": chunk.nodeId, "assetId": chunk.assetId }}} for chunk in chunks]
+        tasks = [{"url_path": path + "/delete", "json": {"items": chunk}} for chunk in chunks]
         summary = utils._concurrency.execute_tasks_concurrently(self._post, tasks, self._config.max_workers)
         summary.raise_compound_exception_if_failed_tasks(
             task_unwrap_fn=lambda task: task["json"]["items"],
