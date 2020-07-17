4f68f52938db070cfb88b1fcb3b92d9203b25b40
Fredrik Anfinsen <fredrik.anfinsen@gmail.com>
2020-07-17 09:56:26 +0200

    test: clean up tests for openapi generator

--- openapi/tests/input_output/input.py
+++ openapi/tests/input_output/input.py
@@ -36,7 +36,7 @@ class TimestampRange:
     pass
 
 
-class AssetLabelFilter:
+class LabelFilter:
     pass
 
 
@@ -46,20 +46,8 @@ class AggregateResultItem(dict):
     # GenStop
 
 
-# GenClass: Asset
-class Asset(CogniteResource):
-    # GenStop
-    def to_pandas(self):
-        pass
-
-
 # GenUpdateClass: AssetChange
 class AssetUpdate(CogniteUpdate):
     pass
     # GenStop
 
-
-# GenClass: AssetFilter.filter
-class AssetFilter(CogniteFilter):
-    pass
-    # GenStop
