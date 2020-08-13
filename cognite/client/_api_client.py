1e487e584b441152a4ae4aa25b53a26cbb57788f
Andreas Våvang Solbrå <andreas.solbra@cognite.com>
2020-08-13 14:21:53 +0200

    add playground to retryable endpoints (#719)
    * add playground and to regex and test for relationships endpoint
    * Bump version
    * Merge branch 'master' into include-playground-on-retryable-endpoints
    * bump version again
    * fix date

--- cognite/client/_api_client.py
+++ cognite/client/_api_client.py
@@ -207,7 +207,7 @@ class APIClient:
 
     def _is_retryable(self, method, path):
         valid_methods = ["GET", "POST", "PUT", "DELETE", "PATCH"]
-        match = re.match("(?:http|https)://[a-z\d.:]+(?:/api/v1/projects/[^/]+)?(/.+)", path)
+        match = re.match("(?:http|https)://[a-z\d.:]+(?:/api/(?:v1|playground)/projects/[^/]+)?(/.+)", path)
 
         if not match:
             raise ValueError("Path {} is not valid. Cannot resolve whether or not it is retryable".format(path))
