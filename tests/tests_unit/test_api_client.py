0963a76016d86ea46dc8d16b81b4d60b4da7f8aa
Andreas Våvang Solbrå <andreas.solbra@cognite.com>
2020-08-13 14:21:53 +0200

    add playground to retryable endpoints (#719)
    * add playground and to regex and test for relationships endpoint
    * Bump version
    * Merge branch 'master' into include-playground-on-retryable-endpoints
    * bump version again
    * fix date

--- tests/tests_unit/test_api_client.py
+++ tests/tests_unit/test_api_client.py
@@ -965,6 +965,7 @@ class TestHelpers:
             ("POST", "https://localhost:8000/api/v1/projects/blabla/files/list", True),
             ("PUT", "https://api.cognitedata.com/bla", True),
             ("POST", "https://greenfield.cognitedata.com/api/v1/projects/blabla/assets", False),
+            ("POST", "https://greenfield.cognitedata.com/api/playground/projects/blabla/relationships/list", True),
             ("PUT", "https://localhost:8000.com/api/v1/projects/blabla/assets", True),
             ("PATCH", "https://localhost:8000.com/api/v1/projects/blabla/patchy", True),
         ],
