f9741cbec17630d75c13244657cf36e1cf71acb6
Sahil Yadav <yadav_sahil@outlook.com>
2020-09-14 14:49:03 +0200

    Remove relationships from experimental

--- cognite/client/experimental.py
+++ cognite/client/experimental.py
@@ -1,6 +1,5 @@
 from cognite.client._api.datapoints import DatapointsAPI
 from cognite.client._api.model_hosting import ModelHostingAPI
-from cognite.client._api.relationships import RelationshipsAPI
 from cognite.client._cognite_client import CogniteClient as Client
 
 
@@ -13,4 +12,3 @@ class CogniteClient(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.model_hosting = ModelHostingAPI(self._config, api_version="playground", cognite_client=self)
-        self.relationships = RelationshipsAPI(self._config, api_version="playground", cognite_client=self)
