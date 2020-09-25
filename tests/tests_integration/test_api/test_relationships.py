cefeef377ae72153aa3313fff7a09553bc0d5354
Sahil Yadav <yadav_sahil@outlook.com>
2020-09-25 11:10:01 +0200

    Fix integration tests

--- tests/tests_integration/test_api/test_relationships.py
+++ tests/tests_integration/test_api/test_relationships.py
@@ -50,48 +50,49 @@ def new_label():
 def create_multiple_relationships(new_label):
     ext_id = new_label.external_id
     relationships_ext_id = [uuid.uuid4().hex[0:20] for i in range(5)]
+    random_ext_id = [uuid.uuid4().hex[0:20] for i in range(5)]
     relationshipList = [
         Relationship(
             external_id=relationships_ext_id[0],
             source_type="asset",
-            source_external_id="source_ext_id1",
+            source_external_id=random_ext_id[0],
             target_type="event",
-            target_external_id="target_ext_id1",
+            target_external_id=random_ext_id[1],
             labels=[Label(ext_id)],
         ),
         Relationship(
             external_id=relationships_ext_id[1],
             source_type="timeSeries",
-            source_external_id="source_ext_id2",
+            source_external_id=random_ext_id[3],
             target_type="asset",
-            target_external_id="target_ext_id2",
+            target_external_id=random_ext_id[2],
         ),
         Relationship(
             external_id=relationships_ext_id[2],
             source_type="event",
-            source_external_id="source_ext_id3",
+            source_external_id=random_ext_id[2],
             target_type="timeseries",
-            target_external_id="target_ext_id3",
+            target_external_id=random_ext_id[3],
             labels=[Label(ext_id)],
         ),
         Relationship(
             external_id=relationships_ext_id[3],
             source_type="sequence",
-            source_external_id="source_ext_id3",
+            source_external_id=random_ext_id[3],
             target_type="sequence",
-            target_external_id="target_ext_id4",
+            target_external_id=random_ext_id[4],
             labels=[Label(ext_id)],
         ),
         Relationship(
             external_id=relationships_ext_id[4],
             source_type="file",
-            source_external_id="source_ext_id5",
+            source_external_id=random_ext_id[4],
             target_type="file",
-            target_external_id="target_ext_id5",
+            target_external_id=random_ext_id[0],
         ),
     ]
     relationships = API_REL.create(relationshipList)
-    yield relationships, ext_id
+    yield relationships, ext_id, random_ext_id[3]
     API_REL.delete(external_id=[ext_ids["external_id"] for ext_ids in relationships.dump()])
 
 
@@ -102,7 +103,7 @@ class TestRelationshipsAPI:
         assert res.dump()["confidence"] == 0.9
 
     def test_create_multiple(self, create_multiple_relationships):
-        relationships, ext_id = create_multiple_relationships
+        relationships, ext_id, source_ext_id = create_multiple_relationships
         assert isinstance(relationships, RelationshipList)
 
     def test_retrieve_unknown(self, new_relationship):
@@ -111,12 +112,13 @@ class TestRelationshipsAPI:
         assert API_REL.retrieve(external_id="this does not exist") is None
 
     def test_list_filter(self, create_multiple_relationships):
-        res = API_REL.list(source_external_ids=["source_ext_id3"])
+        relationships, ext_id, source_ext_id = create_multiple_relationships
+        res = API_REL.list(source_external_ids=[source_ext_id])
         assert len(res) == 2
         assert isinstance(res, RelationshipList)
 
     def test_list_label_filter(self, create_multiple_relationships):
-        relationships, ext_id = create_multiple_relationships
+        relationships, ext_id, source_ext_id = create_multiple_relationships
         res = API_REL.list(labels=LabelFilter(contains_all=[ext_id]))
         assert len(res) == 3
         assert isinstance(res, RelationshipList)
