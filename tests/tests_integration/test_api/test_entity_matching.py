f45ff529a68cee623933963066cfc86239bb8275
Ching-yu Lin <60384727+chingyulin@users.noreply.github.com>
2021-05-07 09:27:24 +0200

    correct exception when model not found (#823)
    * correct exception when model not found
    
    * lint

--- tests/tests_integration/test_api/test_entity_matching.py
+++ tests/tests_integration/test_api/test_entity_matching.py
@@ -10,7 +10,7 @@ from cognite.client.data_classes import (
     EntityMatchingModelUpdate,
 )
 from cognite.client.data_classes.contextualization import ContextualizationJobList
-from cognite.client.exceptions import CogniteAPIError, ModelFailedException
+from cognite.client.exceptions import CogniteNotFoundError, ModelFailedException
 
 COGNITE_CLIENT = CogniteClient()
 EMAPI = COGNITE_CLIENT.entity_matching
@@ -21,7 +21,7 @@ def fitted_model():
     extid = "abc" + str(random.randint(1, 1000000000))
     try:
         EMAPI.delete(external_id=extid)
-    except CogniteAPIError as e:
+    except CogniteNotFoundError as e:
         pass  # expected
     entities_from = [{"id": 1, "name": "xx-yy"}]
     entities_to = [{"id": 2, "bloop": "yy"}, {"id": 3, "bloop": "zz"}]
