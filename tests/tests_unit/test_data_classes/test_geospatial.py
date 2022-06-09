dfccff8849f8dc870383ae675091d4707e10ef1b
tuanng-cognite <tuan.nguyen@cognite.com>
2022-06-09 10:25:56 +0200

    geospatial: fix feature properies serialization (#931)

---
+++ tests/tests_unit/test_data_classes/test_geospatial.py
@@ -0,0 +1,59 @@
+from cognite.client.data_classes.geospatial import Feature, FeatureList
+
+
+class TestFeature:
+    def test_load_feature(self):
+        feature = Feature._load(
+            {
+                "firstName": "name",
+                "temperature_Hot": 12.0,
+                "levelH2O": 12.0,
+                "externalId": "f1",
+                "createdTime": 1654612200225,
+                "lastUpdatedTime": 1654612200225,
+            }
+        )
+        assert feature.firstName == "name"
+        assert feature.temperature_Hot == 12.0
+        assert feature.levelH2O == 12.0
+        assert feature.external_id == "f1"
+        assert feature.created_time == 1654612200225
+        assert feature.last_updated_time == 1654612200225
+
+    def test_dump_feature(self):
+        feature = Feature(external_id="f1", firstName="name", temperature_Hot=12.0, levelH2O=12.0)
+        dumped = feature.dump(camel_case=True)
+        assert dumped == {"externalId": "f1", "firstName": "name", "temperature_Hot": 12.0, "levelH2O": 12.0}
+        dumped = feature.dump(camel_case=False)
+        assert dumped == {"external_id": "f1", "firstName": "name", "temperature_Hot": 12.0, "levelH2O": 12.0}
+
+
+class TestFeatureList:
+    def test_load_feature_list(self):
+        features = FeatureList._load(
+            [
+                {
+                    "firstName": "name",
+                    "temperature_Hot": 12.0,
+                    "levelH2O": 12.0,
+                    "externalId": "f1",
+                    "createdTime": 1654612200225,
+                    "lastUpdatedTime": 1654612200225,
+                }
+            ]
+        )
+        assert len(features) == 1
+        feature = features[0]
+        assert feature.firstName == "name"
+        assert feature.temperature_Hot == 12.0
+        assert feature.levelH2O == 12.0
+        assert feature.external_id == "f1"
+        assert feature.created_time == 1654612200225
+        assert feature.last_updated_time == 1654612200225
+
+    def test_dump_feature_list(self):
+        feature_list = FeatureList([Feature(external_id="f1", firstName="name", temperature_Hot=12.0, levelH2O=12.0)])
+        dumped = feature_list.dump(camel_case=True)
+        assert dumped == [{"externalId": "f1", "firstName": "name", "temperature_Hot": 12.0, "levelH2O": 12.0}]
+        dumped = feature_list.dump(camel_case=False)
+        assert dumped == [{"external_id": "f1", "firstName": "name", "temperature_Hot": 12.0, "levelH2O": 12.0}]
