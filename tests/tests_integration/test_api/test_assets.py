b6d2a8fc1a61b2da0bc2afe83dbc763f2e42b7d9
Arne Sund <arne.sund@cognite.com>
2022-06-10 14:36:39 +0200

    Fix linting

--- tests/tests_integration/test_api/test_assets.py
+++ tests/tests_integration/test_api/test_assets.py
@@ -13,7 +13,7 @@ from cognite.client.data_classes import (
     GeoLocation,
     GeoLocationFilter,
     Geometry,
-    GeometryFilter
+    GeometryFilter,
 )
 from cognite.client.exceptions import CogniteNotFoundError
 from tests.utils import set_request_limit
@@ -220,11 +220,10 @@ class TestAssetsAPI:
             a = cognite_client.assets.create(Asset(name="any", geo_location=geo_location))
             result_assets = cognite_client.assets.list(
                 geo_location=GeoLocationFilter(
-                    relation="WITHIN", shape=GeometryFilter(
-                        type="Polygon", coordinates=[[
-                            [32.0, 83.0], [-86.0, -23.0], [119.0, -25.0], [32.0, 83.0]
-                        ]]
-                    )
+                    relation="WITHIN",
+                    shape=GeometryFilter(
+                        type="Polygon", coordinates=[[[32.0, 83.0], [-86.0, -23.0], [119.0, -25.0], [32.0, 83.0]]]
+                    ),
                 ),
                 limit=1,
             )
