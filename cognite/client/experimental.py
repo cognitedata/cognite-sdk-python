491684b37ceb58e83edbb2569b2380db0a253106
erlendvollset <erlendvollset@gmail.com>
2022-07-13 16:56:04 +0200

    Remove cognite.client.experimental module

--- cognite/client/experimental.py
+++
@@ -1,8 +0,0 @@
-from cognite.client.beta import CogniteClient as Client
-from cognite.client.utils._experimental_warning import experimental_api
-
-
-@experimental_api
-class CogniteClient(Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
