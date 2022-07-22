02f63b446e19aa653baa5f2146a723293ac473ad
anders-albert <anders0albert@gmail.com>
2022-07-22 14:42:18 +0200

    tests: added caching of token for integration tests

--- tests/tests_integration/conftest.py
+++ tests/tests_integration/conftest.py
@@ -1,19 +1,40 @@
+import atexit
 import os
+import pathlib
 import random
 
 import pytest
-from msal import PublicClientApplication
+from msal import PublicClientApplication, SerializableTokenCache
 
 from cognite.client import CogniteClient
+
+CACHE_FILENAME = "cache.bin"
+CACHE_PATH = pathlib.Path(__file__).resolve().parent.parent.parent / "cache.bin"
+
+
+def create_cache():
+    cache = SerializableTokenCache()
+    if CACHE_PATH.exists():
+        cache.deserialize(open(CACHE_PATH, "r").read())
+    atexit.register(lambda: open(CACHE_PATH, "w").write(cache.serialize()) if cache.has_state_changed else None)
+    return cache
 
 
 def make_cognite_client_with_interactive_flow() -> CogniteClient:
     authority_url = os.environ["COGNITE_AUTHORITY_URL"]
     client_id = os.environ["COGNITE_CLIENT_ID"]
     scopes = os.environ.get("COGNITE_TOKEN_SCOPES", "").split(",")
-    app = PublicClientApplication(client_id=client_id, authority=authority_url)
-    redirect_port = random.randint(53000, 60000)  # random port so we can run the test suite in parallel
-    creds = app.acquire_token_interactive(scopes=scopes, port=redirect_port)
+    app = PublicClientApplication(client_id=client_id, authority=authority_url, token_cache=create_cache())
+    redirect_port = random.randint(53000, 60000)
+    # random port so we can run the test suite in parallel
+    creds = (
+        app.acquire_token_silent(scopes, account=accounts[0])
+        if (accounts := app.get_accounts())
+        else app.acquire_token_interactive(
+            scopes=scopes,
+            port=redirect_port,
+        )
+    )
     return CogniteClient(token=creds["access_token"])
 
 
