68141fe4c1d16dff33b723d2443c63603b7c029e
Håkon V. Treider <haakonvt@gmail.com>
2022-07-25 16:28:19 +0200

    Split out dpsApi constants

---
+++ cognite/client/_api/datapoint_constants.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from typing import Dict, Iterable, List, Optional, TypedDict, Union
+
+import numpy as np
+import numpy.typing as npt
+
+# Datapoints API-limits:
+DPS_LIMIT_AGG = 10_000
+DPS_LIMIT = 100_000
+POST_DPS_OBJECTS_LIMIT = 10_000
+FETCH_TS_LIMIT = 100
+RETRIEVE_LATEST_LIMIT = 100
+
+
+class CustomDatapoints(TypedDict, total=False):
+    # No field required
+    start: Union[int, str, datetime, None]
+    end: Union[int, str, datetime, None]
+    aggregates: Optional[List[str]]
+    granularity: Optional[str]
+    limit: Optional[int]
+    include_outside_points: bool
+
+
+class DatapointsQueryId(CustomDatapoints):
+    id: int  # required field
+
+
+class DatapointsQueryExternalId(CustomDatapoints):
+    external_id: str  # required field
+
+
+class DatapointsFromAPI(TypedDict):
+    id: int
+    externalId: Optional[str]
+    isString: bool
+    isStep: bool
+    datapoints: List[Dict[str, Union[int, float, str]]]
+
+
+DatapointsIdTypes = Union[int, DatapointsQueryId, Iterable[Union[int, DatapointsQueryId]]]
+DatapointsExternalIdTypes = Union[str, DatapointsQueryExternalId, Iterable[Union[str, DatapointsQueryExternalId]]]
+
+NumpyInt64Array = npt.NDArray[np.int64]
+NumpyFloat64Array = npt.NDArray[np.float64]
+NumpyObjArray = npt.NDArray[np.object_]
