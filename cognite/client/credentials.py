f4015c26bbfacc0a6cc300682744bff5a58ecc52
erlendvollset <erlendvollset@gmail.com>
2022-08-13 13:46:02 +0200

    Add new credential provider module

---
+++ cognite/client/credentials.py
@@ -0,0 +1,103 @@
+import threading
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Callable, List, Optional, Tuple, Union
+
+from oauthlib.oauth2 import BackendApplicationClient, OAuth2Error
+from requests_oauthlib import OAuth2Session
+
+from cognite.client.exceptions import CogniteAuthError
+
+
+class CredentialProvider(ABC):
+    @abstractmethod
+    def authorization_header(self) -> Tuple[str, str]:
+        ...
+
+
+class APIKey(CredentialProvider):
+    def __init__(self, api_key: str) -> None:
+        self.__api_key = api_key
+
+    def authorization_header(self) -> Tuple[str, str]:
+        return "api-key", self.__api_key
+
+
+class Token(CredentialProvider):
+    def __init__(self, token: Union[str, Callable[[], str]]) -> None:
+        if isinstance(token, str):
+            self.__token_factory = lambda: token
+        else:
+            self.__token_factory = token
+
+    def authorization_header(self) -> Tuple[str, str]:
+        return "Authorization", f"Bearer {self.__token_factory()}"
+
+
+class OAuthClientCredentials(CredentialProvider):
+    # This ensures we don't return a token which expires immediately, but within minimum 3 seconds.
+    __TOKEN_EXPIRY_LEEWAY_SECONDS = 3
+
+    def __init__(self, token_url: str, client_id: str, client_secret: str, scopes: List[str]):
+        self.__token_url = token_url
+        self.__client_id = client_id
+        self.__client_secret = client_secret
+        self.__scopes = scopes
+        self.__oauth = OAuth2Session(client=BackendApplicationClient(client_id=self.__client_id))
+
+        self.__token_refresh_lock = threading.Lock()
+        self.__access_token: Optional[str] = None
+        self.__access_token_expires_at: Optional[float] = None
+
+    @property
+    def token_url(self) -> str:
+        return self.__token_url
+
+    @property
+    def client_id(self) -> str:
+        return self.__client_id
+
+    @property
+    def client_secret(self) -> str:
+        return self.__client_secret
+
+    @property
+    def scopes(self) -> List[str]:
+        return self.__scopes
+
+    def _refresh_access_token(self) -> None:
+        from cognite.client.config import global_config
+
+        try:
+            token_result = self.__oauth.fetch_token(
+                token_url=self.__token_url,
+                client_id=self.__client_id,
+                client_secret=self.__client_secret,
+                scope=self.__scopes,
+                verify=not global_config.disable_ssl,
+            )
+            self.__access_token = token_result.get("access_token")
+            self.__access_token_expires_at = token_result.get("expires_at")
+        except OAuth2Error as oauth_error:
+            raise CogniteAuthError(
+                "Error generating access token: {0}, {1}, {2}".format(
+                    oauth_error.error, oauth_error.status_code, oauth_error.description
+                )
+            ) from oauth_error
+
+    @classmethod
+    def __should_refresh_token(cls, token: Optional[str], expires_at: Optional[float]) -> bool:
+        no_token = token is None
+        token_is_expired = (
+            expires_at is not None and datetime.now().timestamp() > expires_at - cls.__TOKEN_EXPIRY_LEEWAY_SECONDS
+        )
+        return no_token or token_is_expired
+
+    def authorization_header(self) -> Tuple[str, str]:
+        # We lock here to ensure we don't issue a herd of refresh requests in concurrent scenarios.
+        # This means we will block all requests for the duration of the token refresh.
+        # TODO: Consider instead having a background thread periodically refresh the token to avoid this blocking.
+        with self.__token_refresh_lock:
+            if self.__should_refresh_token(self.__access_token, self.__access_token_expires_at):
+                self._refresh_access_token()
+        return "Authorization", f"Bearer {self.__access_token}"
