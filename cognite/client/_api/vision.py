cf43bdf8c1104f74acab72eb37b70f3fb901fec5
Milad Mobarhan <milad.mobarhan@cognite.com>
2022-09-07 11:32:26 +0200

    Fix: remove deprecated methods

--- cognite/client/_api/vision.py
+++ cognite/client/_api/vision.py
@@ -1,76 +1,16 @@
-from typing import List, Optional, Union
+from typing import Any, List, Optional, Union
 
 from cognite.client._api._context_client import ContextAPI
 from cognite.client.data_classes.contextualization import ContextualizationJob
-from cognite.client.data_classes.vision import (
-    CreatedDetectAssetsInFilesJob,
-    DetectAssetsInFilesJob,
-    EitherFileId,
-    Feature,
-    FeatureParameters,
-    InternalId,
-    VisionExtractJob,
-)
+from cognite.client.data_classes.vision import Feature, FeatureParameters, InternalId, VisionExtractJob
 from cognite.client.utils._auxiliary import assert_type
-from cognite.client.utils._case_conversion import resource_to_camel_case
 
 
 class VisionAPI(ContextAPI):
     _RESOURCE_PATH = "/context/vision"
-    _TAG_DETECTION_PATH = f"{_RESOURCE_PATH}/tagdetection"
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-
-    def detect_assets_in_files(
-        self,
-        files: List[EitherFileId],
-        use_cache: Optional[bool] = None,
-        partial_match: Optional[bool] = None,
-        asset_subtree_ids: Optional[List[InternalId]] = None,
-    ) -> CreatedDetectAssetsInFilesJob:
-        """Initiate a job to detect external ID or name of assets in given file references
-
-        Args:
-            files (List[Union[int, str]]): references to files to work on
-            use_cache (bool): uses cached result if the file has previously been analyzed.
-            partial_match (bool): Allow partial (fuzzy) matching of detected external IDs in the file
-            asset_subtree_ids (List[int]): Search for external ID or name of assets that are in a subtree rooted at one of the assetSubtreeIds
-
-        Returns:
-            CreatedDetectAssetsInFilesJob: job information
-        """
-        request = self._prepare_detect_assets_in_files_request(files, use_cache, partial_match, asset_subtree_ids)
-        response = self._post(url_path=self._TAG_DETECTION_PATH, json=request)
-        return CreatedDetectAssetsInFilesJob._load(response.json(), cognite_client=self._cognite_client)
-
-    @classmethod
-    def _prepare_detect_assets_in_files_request(
-        cls,
-        files: List[EitherFileId],
-        use_cache: Optional[bool] = None,
-        partial_match: Optional[bool] = None,
-        asset_subtree_ids: Optional[List[InternalId]] = None,
-    ) -> dict:
-        request = {
-            "items": files,
-            "use_cache": use_cache,
-            "partial_match": partial_match,
-            "asset_subtree_ids": asset_subtree_ids,
-        }
-        request = resource_to_camel_case(request)
-        return request
-
-    def retrieve_detected_assets_in_files_job(self, job_id: InternalId) -> DetectAssetsInFilesJob:
-        """Retrieve detected external ID or name of assets with bounding boxes in images or single-page pdf files.
-
-        Args:
-            job_id (int): Contextualization job ID.
-
-        Returns:
-            DetectAssetsInFilesJob: job information with a list of succeeded and failed asset detection for files
-        """
-        return self._retrieve(id=job_id, resource_path=self._TAG_DETECTION_PATH, cls=DetectAssetsInFilesJob)
 
     def extract(
         self,
--- cognite/client/data_classes/vision.py
+++ cognite/client/data_classes/vision.py
@@ -1,7 +1,6 @@
-import json
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict, List, Optional, Type, Union, cast, get_type_hints
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Type, Union, cast, get_type_hints
 
 from typing_extensions import get_args
 
@@ -12,7 +11,7 @@ from cognite.client.data_classes.annotation_types.primitives import VisionResour
 from cognite.client.data_classes.annotations import AnnotationList
 from cognite.client.data_classes.contextualization import JobStatus
 from cognite.client.exceptions import CogniteException
-from cognite.client.utils._case_conversion import resource_to_camel_case, resource_to_snake_case
+from cognite.client.utils._case_conversion import resource_to_snake_case
 
 FeatureClass = Union[Type[TextRegion], Type[AssetLink], Type[ObjectDetection]]
 ExternalId = str
@@ -27,6 +26,12 @@ class ExternalFileId:
 @dataclass
 class InternalFileId:
     file_id: InternalId
+
+
+if TYPE_CHECKING:
+
+    from cognite.client import CogniteClient
+
 
 
 class Feature(str, Enum):
@@ -70,8 +75,6 @@ VISION_ANNOTATION_TYPE_MAP: Dict[str, str] = {
     "personal_protective_equipment_predictions": "images.ObjectDetection",
 }
 
-EitherFileId = Union[InternalFileId, ExternalFileId]
-
 
 @dataclass
 class ThresholdParameter:
@@ -113,11 +116,6 @@ class FeatureParameters(VisionResource):
     personal_protective_equipment_detection_parameters: Optional[PersonalProtectiveEquipmentDetectionParameters] = None
 
 
-@dataclass
-class AllOfFileId(InternalFileId):
-    file_external_id: Optional[ExternalId] = None
-
-
 class VisionJob(ContextualizationJob):
     def update_status(self) -> str:
         # Override update_status since we need to handle the vision-specific
@@ -133,214 +131,6 @@ class VisionJob(ContextualizationJob):
         self._result = {k: v for k, v in data.items() if k not in self._COMMON_FIELDS}
         assert self.status is not None
         return self.status
-
-
-class CreatedDetectAssetsInFilesJob(CogniteResource):
-    def __init__(
-        self,
-        status: JobStatus,
-        created_time: int,
-        status_time: int,
-        job_id: int,
-        items: Optional[List[AllOfFileId]] = None,
-        use_cache: Optional[bool] = None,
-        partial_match: Optional[bool] = None,
-        asset_subtree_ids: Optional[List[InternalId]] = None,
-        start_time: Optional[int] = None,
-    ) -> None:
-        self.status = status
-        self.created_time = created_time
-        self.status_time = status_time
-        self.job_id = job_id
-        self.use_cache = use_cache
-        self.partial_match = partial_match
-        self.asset_subtree_ids = asset_subtree_ids
-        self.start_time = start_time
-        self.items = items
-
-    def dump(self, camel_case: bool = False) -> Dict[str, Any]:
-        if camel_case:
-            return resource_to_camel_case(self)
-        else:
-            return resource_to_snake_case(self)
-
-    @classmethod
-    def _load(cls, resource: Union[Dict, str], cognite_client=None):
-        if isinstance(resource, str):
-            return cls._load(json.loads(resource))
-        elif isinstance(resource, Dict):
-            k = resource_to_snake_case(resource)
-            instance = cls(
-                status=k["status"],
-                created_time=k["created_time"],
-                status_time=k["status_time"],
-                job_id=k["job_id"],
-                use_cache=k.get("use_cache"),
-                partial_match=k.get("partial_match"),
-                asset_subtree_ids=k.get("asset_subtree_ids"),
-            )
-            items = k.get("items")
-            if items is not None:
-                instance.items = [
-                    AllOfFileId(file_id=item["file_id"], file_external_id=item.get("file_external_id"))
-                    for item in k["items"]
-                ]
-            return instance
-
-        raise TypeError(f"Resource must be json str or Dict, not {type(resource)}")
-
-
-@dataclass
-class FailedAssetDetectionInFiles:
-    error_message: str
-    items: List[AllOfFileId]
-
-    @classmethod
-    def _load(cls, resource: Optional[Union[Dict, str]] = None):
-        if resource is None:
-            return None
-        elif isinstance(resource, str):
-            return cls._load(json.loads(resource))
-        elif isinstance(resource, Dict):
-            k = resource_to_snake_case(resource)
-            return cls(
-                error_message=k["error_message"],
-                items=[
-                    AllOfFileId(file_id=v["file_id"], file_external_id=v.get("file_external_id")) for v in k["items"]
-                ],
-            )
-        raise TypeError(f"Resource must be json str or Dict, not {type(resource)}")
-
-
-@dataclass
-class VisionVertex:
-    x: float
-    y: float
-
-
-@dataclass
-class VisionRegion:
-    shape: str
-    vertices: List[VisionVertex]
-
-    @classmethod
-    def _load(cls, resource: Optional[Union[Dict, str]] = None):
-        if resource is None:
-            return None
-        elif isinstance(resource, str):
-            return cls._load(json.loads(resource))
-        elif isinstance(resource, Dict):
-            k = resource_to_snake_case(resource)
-            return cls(
-                shape=k["shape"],
-                vertices=[VisionVertex(x=v["x"], y=v["y"]) for v in k["vertices"]],
-            )
-        raise TypeError(f"Resource must be json str or Dict, not {type(resource)}")
-
-
-@dataclass
-class VisionTagDetectionAnnotation:
-    text: str
-    asset_ids: List[InternalId]
-    confidence: Optional[float] = None
-    region: Optional[VisionRegion] = None
-
-    @classmethod
-    def _load(cls, resource: Optional[Union[Dict, str]] = None):
-        if resource is None:
-            return None
-        elif isinstance(resource, str):
-            return cls._load(json.loads(resource))
-        elif isinstance(resource, Dict):
-            k = resource_to_snake_case(resource)
-            return cls(
-                text=k["text"],
-                asset_ids=k["asset_ids"],
-                confidence=k.get("confidence"),
-                region=VisionRegion._load(k.get("region")),
-            )
-        raise TypeError(f"Resource must be json str or Dict, not {type(resource)}")
-
-
-@dataclass
-class SuccessfulAssetDetectionInFiles(AllOfFileId):
-    width: Optional[int] = None
-    height: Optional[int] = None
-    annotations: Optional[List[VisionTagDetectionAnnotation]] = None
-
-    @classmethod
-    def _load(cls, resource: Union[Dict, str]):
-        if isinstance(resource, str):
-            return cls._load(json.loads(resource))
-        elif isinstance(resource, Dict):
-            k = resource_to_snake_case(resource)
-            instance = cls(
-                file_id=k["file_id"],
-                file_external_id=k.get("file_external_id"),
-                width=k.get("width"),
-                height=k.get("height"),
-            )
-            annotations = k.get("annotations")
-            if annotations is not None:
-                instance.annotations = [VisionTagDetectionAnnotation._load(v) for v in annotations]
-            return instance
-        raise TypeError(f"Resource must be json str or Dict, not {type(resource)}")
-
-
-class DetectAssetsInFilesJob(CogniteResource):
-    def __init__(
-        self,
-        status: JobStatus,
-        created_time: int,
-        status_time: int,
-        job_id: int,
-        use_cache: Optional[bool] = None,
-        partial_match: Optional[bool] = None,
-        asset_subtree_ids: Optional[List[InternalId]] = None,
-        start_time: Optional[int] = None,
-        items: Optional[List[SuccessfulAssetDetectionInFiles]] = None,
-        failed_items: Optional[List[FailedAssetDetectionInFiles]] = None,
-    ) -> None:
-        self.status = status
-        self.created_time = created_time
-        self.status_time = status_time
-        self.job_id = job_id
-        self.use_cache = use_cache
-        self.partial_match = partial_match
-        self.asset_subtree_ids = asset_subtree_ids
-        self.start_time = start_time
-        self.items = items
-        self.failed_items = failed_items
-
-    def dump(self, camel_case: bool = False) -> Dict[str, Any]:
-        if camel_case:
-            return resource_to_camel_case(self)
-        else:
-            return resource_to_snake_case(self)
-
-    @classmethod
-    def _load(cls, resource: Union[Dict, str], cognite_client=None):
-        if isinstance(resource, str):
-            return cls._load(json.loads(resource))
-        elif isinstance(resource, Dict):
-            k = resource_to_snake_case(resource)
-            instance = cls(
-                status=k["status"],
-                created_time=k["created_time"],
-                status_time=k["status_time"],
-                job_id=k["job_id"],
-                use_cache=k.get("use_cache"),
-                partial_match=k.get("partial_match"),
-                asset_subtree_ids=k.get("asset_subtree_ids"),
-            )
-            failed_items = k.get("failed_items")
-            if failed_items is not None:
-                instance.failed_items = [FailedAssetDetectionInFiles._load(v) for v in failed_items]
-            successful_items = k.get("items")
-            if successful_items is not None:
-                instance.items = [SuccessfulAssetDetectionInFiles._load(v) for v in successful_items]
-            return instance
-        raise TypeError(f"Resource must be json str or Dict, not {type(resource)}")
 
 
 class VisionExtractItem(CogniteResource):
