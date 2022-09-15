f79eef951485d0996e02946692f0f897126e1d38
Håkon V. Treider <haakonvt@gmail.com>
2022-09-15 03:20:48 +0200

    Start on integ.test for queries, Test_SingleTSQueryValidator

--- tests/tests_unit/test_api/test_datapoints_tasks.py
+++ tests/tests_unit/test_api/test_datapoints_tasks.py
@@ -1,6 +1,16 @@
-class Test_SingleTSQueryValidator:
+import math
 
+import pytest
+
+from cognite.client._api.datapoint_tasks import _SingleTSQueryValidator
+from cognite.client.data_classes import DatapointsQuery
+
+
+class Test_SingleTSQueryValidator:
     @pytest.mark.parametrize("limit, exp_limit", [(-1, None), (0, 0), (1, 1), (math.inf, None), (None, None)])
-    def test_query_valid_limits(limit, exp_limit):
+    def test_query_valid_limits(self, limit, exp_limit):
         ts_query = _SingleTSQueryValidator(DatapointsQuery(id=0, limit=limit)).validate_and_create_single_queries()
-        assert exp_limit == ts_query.limit
+        assert len(ts_query) == 1
+        assert ts_query[0].limit == exp_limit
+
+    # TODO: WIP
