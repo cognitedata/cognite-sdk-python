1ee74df2f4706ba14caed90a7597d97d3c59313f
Håkon V. Treider <haakonvt@gmail.com>
2022-11-20 17:33:01 +0100

    Add updated dps proto files that includes nextCursor

--- cognite/client/_proto/data_point_list_response_pb2.pyi
+++ cognite/client/_proto/data_point_list_response_pb2.pyi
@@ -7,12 +7,13 @@ from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Map
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class DataPointListItem(_message.Message):
-    __slots__ = ["aggregateDatapoints", "externalId", "id", "isStep", "isString", "numericDatapoints", "stringDatapoints", "unit"]
+    __slots__ = ["aggregateDatapoints", "externalId", "id", "isStep", "isString", "nextCursor", "numericDatapoints", "stringDatapoints", "unit"]
     AGGREGATEDATAPOINTS_FIELD_NUMBER: _ClassVar[int]
     EXTERNALID_FIELD_NUMBER: _ClassVar[int]
     ID_FIELD_NUMBER: _ClassVar[int]
     ISSTEP_FIELD_NUMBER: _ClassVar[int]
     ISSTRING_FIELD_NUMBER: _ClassVar[int]
+    NEXTCURSOR_FIELD_NUMBER: _ClassVar[int]
     NUMERICDATAPOINTS_FIELD_NUMBER: _ClassVar[int]
     STRINGDATAPOINTS_FIELD_NUMBER: _ClassVar[int]
     UNIT_FIELD_NUMBER: _ClassVar[int]
@@ -21,10 +22,11 @@ class DataPointListItem(_message.Message):
     id: int
     isStep: bool
     isString: bool
+    nextCursor: str
     numericDatapoints: _data_points_pb2.NumericDatapoints
     stringDatapoints: _data_points_pb2.StringDatapoints
     unit: str
-    def __init__(self, id: _Optional[int] = ..., externalId: _Optional[str] = ..., isString: bool = ..., isStep: bool = ..., unit: _Optional[str] = ..., numericDatapoints: _Optional[_Union[_data_points_pb2.NumericDatapoints, _Mapping]] = ..., stringDatapoints: _Optional[_Union[_data_points_pb2.StringDatapoints, _Mapping]] = ..., aggregateDatapoints: _Optional[_Union[_data_points_pb2.AggregateDatapoints, _Mapping]] = ...) -> None: ...
+    def __init__(self, id: _Optional[int] = ..., externalId: _Optional[str] = ..., isString: bool = ..., isStep: bool = ..., unit: _Optional[str] = ..., nextCursor: _Optional[str] = ..., numericDatapoints: _Optional[_Union[_data_points_pb2.NumericDatapoints, _Mapping]] = ..., stringDatapoints: _Optional[_Union[_data_points_pb2.StringDatapoints, _Mapping]] = ..., aggregateDatapoints: _Optional[_Union[_data_points_pb2.AggregateDatapoints, _Mapping]] = ...) -> None: ...
 
 class DataPointListResponse(_message.Message):
     __slots__ = ["items"]
