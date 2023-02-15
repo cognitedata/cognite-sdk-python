394d87c92b93549136bd9210d3ab4aaa34b26f40
Håkon V. Treider <haakonvt@gmail.com>
2023-02-15 01:23:24 +0100

    remove token refresh cred.class, refactor pri thread pool exec.

--- cognite/client/utils/_pyodide_helpers.py
+++ cognite/client/utils/_pyodide_helpers.py
@@ -1,26 +1,15 @@
 from __future__ import annotations
 
-import contextlib
-import functools
-import inspect
-import json
 import os
 import warnings
-from base64 import urlsafe_b64decode
-from concurrent.futures import CancelledError
-from pathlib import Path
-from typing import TYPE_CHECKING, Any, Callable, Dict, Iterator, Optional, Tuple
-
-from msal import PublicClientApplication
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Tuple
 
 import cognite.client as cc
 from cognite.client._http_client import _RetryTracker
 from cognite.client.config import ClientConfig
-from cognite.client.credentials import CredentialProvider, OAuthInteractive
-from cognite.client.utils._concurrency import T_Result, TaskExecutor, TaskFuture
+from cognite.client.credentials import CredentialProvider
 
 if TYPE_CHECKING:
-    from types import TracebackType
 
     from requests import Session
 
@@ -29,11 +18,11 @@ if TYPE_CHECKING:
 
 def patch_sdk_for_pyodide() -> None:
     # -------------------
-    # Patch Pyodide stuff
+    # Patch Pyodide related issues
     # - Patch 'requests' as it does not work in pyodide (socket not implemented):
-    import pyodide_http  # type: ignore [import]
+    from pyodide_http import patch_all
 
-    pyodide_http.patch_all()
+    patch_all()
 
     # -----------------
     # Patch Cognite SDK
@@ -47,20 +36,15 @@ def patch_sdk_for_pyodide() -> None:
     cc._http_client.HTTPClient._old__init__ = cc._http_client.HTTPClient.__init__  # type: ignore [attr-defined]
     cc._http_client.HTTPClient.__init__ = http_client__init__  # type: ignore [assignment]
 
-    # - If we are running inside of a JupyterLite Notebook spawned from Cognite Data Fusion, we set
-    #   the default config to FusionNotebookConfig(). This allows the user to:
-    #   >>> from cognite.client import CogniteClient
-    #   >>> client = CogniteClient()
-    if os.getenv("COGNITE_FUSION_NOTEBOOK") is not None:
-        cc.config.global_config.default_client_config = FusionNotebookConfig()
-
     # - Inject these magic classes into the correct modules so that the user may import them normally:
     cc.config.FusionNotebookConfig = FusionNotebookConfig  # type: ignore [attr-defined]
-    # TODO: Add once token refresh is working:
-    # cc.credentials.OAuthInteractiveFusionNotebook = OAuthInteractiveFusionNotebook  # type: ignore [attr-defined]
 
-    # - Set all usage of ThreadPoolExecutor to use a dummy, serial-implementation:
+    # - Set all usage of thread pool executors to use dummy/serial-implementations:
     cc.utils._concurrency.ConcurrencySettings.executor_type = "mainthread"
+    cc.utils._concurrency.ConcurrencySettings.priority_executor_type = "mainthread"
+
+    # - AssetsAPI.create_hierarchy is not implemented yet (custom threading solution)
+    cc._api.assets._AssetPoster = NotImplementedAssetPoster  # type: ignore [assignment, misc]
 
     # - Auto-ignore protobuf warning for the user (as they can't fix this):
     warnings.filterwarnings(
@@ -68,12 +52,13 @@ def patch_sdk_for_pyodide() -> None:
         category=UserWarning,
         message="Your installation of 'protobuf' is missing compiled C binaries",
     )
-    # - AssetsAPI.create_hierarchy is not implemented yet (custom threading solution)
-    cc._api.assets._AssetPoster = NotImplementedAssetPoster  # type: ignore [assignment, misc]
 
-    # - DatapointsAPI.retrieve/retrieve_arrays/retrieve_dataframe:
-    cc._api.datapoints.PriorityThreadPoolExecutor = SerialPriorityThreadPoolExecutor  # type: ignore [assignment, misc]
-    cc._api.datapoints.as_completed = serial_as_completed  # type: ignore [assignment]
+    # - If we are running inside of a JupyterLite Notebook spawned from Cognite Data Fusion, we set
+    #   the default config to FusionNotebookConfig(). This allows the user to:
+    #   >>> from cognite.client import CogniteClient
+    #   >>> client = CogniteClient()
+    if os.getenv("COGNITE_FUSION_NOTEBOOK") is not None:
+        cc.config.global_config.default_client_config = FusionNotebookConfig()
 
 
 def http_client__init__(
@@ -94,102 +79,23 @@ class NotImplementedAssetPoster:
         raise NotImplementedError("AssetsAPI.create_hierarchy is not pyodide/web-browser compatible yet! Stay tuned!")
 
 
-class DummyFuture(TaskFuture):
-    def __init__(self, fn: Callable[..., T_Result], *args: Any, **kwargs: Any):
-        self._task = functools.partial(fn, *args, **kwargs)
-        self._result: Optional[T_Result] = None
-        self._is_cancelled = False
-
-    def result(self) -> T_Result:
-        if self._is_cancelled:
-            raise CancelledError
-        if self._result is None:
-            self._result = self._task()
-        return self._result
-
-    def cancel(self) -> None:
-        self._is_cancelled = True
-
-
-def serial_as_completed(dct: Dict[DummyFuture, Any]) -> Iterator[DummyFuture]:
-    return iter(dct.copy())  # Will raise StopIteration when done (we want this):
-
-
-class SerialPriorityThreadPoolExecutor(TaskExecutor):
-    def __init__(self, max_workers: int = None) -> None:
-        if max_workers != 1:
-            raise RuntimeError("Max_workers must be == -e^(i*pi), or 1 if you wondered")
-
-    def submit(self, fn: Callable[..., T_Result], *args: Any, **kwargs: Any) -> DummyFuture:
-        if "priority" in inspect.signature(fn).parameters:
-            raise TypeError(f"Given function {fn} cannot accept reserved parameter name `priority`")
-        kwargs.pop("priority", None)
-        return DummyFuture(fn, *args, **kwargs)
-
-    def shutdown(self, wait: bool = False) -> None:
-        return None
-
-    def __enter__(self) -> SerialPriorityThreadPoolExecutor:
-        return self
-
-    def __exit__(
-        self,
-        type: Optional[type[BaseException]],
-        value: Optional[BaseException],
-        traceback: Optional[TracebackType],
-    ) -> None:
-        self.shutdown()
-
-
 class EnvVarToken(CredentialProvider):
     """Credential provider that always reads token from an environment variable just-in-time,
     allowing refreshing the value by another entity.
     Args:
-        key (str | None): The name of the env.var. to read from. Default: 'COGNITE_TOKEN'
+        key (str): The name of the env.var. to read from. Default: 'COGNITE_TOKEN'
     Raises:
         KeyError: If the env.var. is not set.
     """
 
-    def __init__(self, key: str = None) -> None:
-        self.key = "COGNITE_TOKEN" if key is None else key
+    def __init__(self, key: str = "COGNITE_TOKEN") -> None:
+        self.key = key
 
     def __token_factory(self) -> str:
         return os.environ[self.key]
 
     def authorization_header(self) -> Tuple[str, str]:
         return "Authorization", f"Bearer {self.__token_factory()}"
-
-
-class OAuthInteractiveFusionNotebook(OAuthInteractive):
-    # TODO: PublicClientApplication is broken in pyodide because of requests?
-    def __init__(self, redirect_port: int = 53000) -> None:
-        token, base_url = os.environ["COGNITE_TOKEN"], os.environ["COGNITE_BASE_URL"]
-        payload = self._load_payload_from_jwt(token)
-        client_id = payload["appid"]
-        authority_url = f"https://login.microsoftonline.com/{payload['tid']}"
-
-        # Note: Death to name mangling:
-        self._OAuthCredentialProviderWithTokenRefresh__access_token = token
-        self._OAuthCredentialProviderWithTokenRefresh__token_refresh_lock = contextlib.nullcontext()
-        self._OAuthCredentialProviderWithTokenRefresh__access_token_expires_at = payload["exp"]
-
-        self._OAuthInteractive__authority_url = authority_url
-        self._OAuthInteractive__client_id = client_id
-        self._OAuthInteractive__scopes = [f"{base_url}/.default"]
-        self._OAuthInteractive__redirect_port = redirect_port
-        self._OAuthInteractive__app = PublicClientApplication(client_id=client_id, authority=authority_url)
-
-    @staticmethod
-    def _load_payload_from_jwt(token: str) -> Dict[str, Any]:
-        """A JWT has the structure: `<header>.<payload>.<signature>`. This method returns payload as a dict"""
-        _, payload, _ = token.split(".")
-        if remainder := len(payload_bytes := payload.encode()) % 4:
-            payload_bytes += b"=" * (4 - remainder)
-        return json.loads(urlsafe_b64decode(payload_bytes))
-
-    @staticmethod
-    def _create_serializable_token_cache(cache_path: Path) -> None:
-        return None
 
 
 class FusionNotebookConfig(ClientConfig):
@@ -210,9 +116,7 @@ class FusionNotebookConfig(ClientConfig):
             file_transfer_timeout=file_transfer_timeout,
             debug=debug,
             project=os.environ["COGNITE_PROJECT"],
-            # TODO: Fix 'PublicClientApplication' for token refresh:
-            # credentials=OAuthInteractiveFusionNotebook(),  # Magic! 🪄
-            credentials=EnvVarToken(),  # Less magical, but still!
+            credentials=EnvVarToken(),  # Magic!
             base_url=os.environ["COGNITE_BASE_URL"],
             max_workers=1,
         )
