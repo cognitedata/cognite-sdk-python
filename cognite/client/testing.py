ed0560f5c90aa9bb5a0e87c74ce59229e3e959d1
Håkon V. Treider <haakonvt@gmail.com>
2023-02-18 01:38:34 +0100

    Fix test on spec or spec_set apis

--- cognite/client/testing.py
+++ cognite/client/testing.py
@@ -81,9 +81,6 @@ class CogniteClientMock(MagicMock):
         self.assets = MagicMock(spec_set=AssetsAPI)
         self.data_sets = MagicMock(spec_set=DataSetsAPI)
 
-        self.datapoints = MagicMock(spec=DatapointsAPI)  # TODO: In v6, remove and move to time_series.data
-        self.datapoints.synthetic = MagicMock(spec_set=SyntheticDatapointsAPI)
-
         self.diagrams = MagicMock(spec_set=DiagramsAPI)
         self.entity_matching = MagicMock(spec_set=EntityMatchingAPI)
         self.events = MagicMock(spec_set=EventsAPI)
@@ -134,7 +131,9 @@ class CogniteClientMock(MagicMock):
         self.three_d.revisions = MagicMock(spec_set=ThreeDRevisionsAPI)
 
         self.time_series = MagicMock(spec=TimeSeriesAPI)
-        self.time_series.data = self.datapoints
+        self.time_series.data = MagicMock(spec=DatapointsAPI)
+        self.time_series.data.synthetic = MagicMock(spec_set=SyntheticDatapointsAPI)
+        self.datapoints = self.time_series.data  # TODO: In v6, remove and move to time_series.data
 
         self.transformations = MagicMock(spec=TransformationsAPI)
         self.transformations.jobs = MagicMock(spec_set=TransformationJobsAPI)
--- tests/tests_unit/test_testing.py
+++ tests/tests_unit/test_testing.py
@@ -11,21 +11,26 @@ def test_ensure_all_apis_are_available_on_cognite_mock():
     mocked_apis = all_mock_children(CogniteClientMock())
     available = {v.__class__ for v in mocked_apis.values()}
     expected = set(all_subclasses(APIClient))
-    # New APIs that have not been added to CogniteClientMock?
+    # Any new APIs that have not been added to CogniteClientMock?
     assert not expected.difference(available)
-    # Removed APIs still available on CogniteClientMock?
+    # Any removed APIs that are still available on CogniteClientMock?
     assert not available.difference(expected)
 
 
 @pytest.mark.parametrize("api", list(all_mock_children(CogniteClientMock()).values()))
-def test_ensure_all_apis_are_specced_cognite_mock(api):
+def test_ensure_all_apis_are_specced_on_cognite_mock(api):
     # All APIs raise when trying to access a non-existing attribute:
     with pytest.raises(AttributeError):
         api.does_not_exist
-    # ...but only APIs that do not contain other APIs have spec_set=True:
+
+    # ...but only APIs that do not contain other APIs have spec_set=True.
     if api._spec_set is True:
+        assert not api._mock_children
         with pytest.raises(AttributeError):
             api.does_not_exist = 42
+    else:
+        assert api._mock_children
+        api.does_not_exist = 42
 
 
 def test_cognite_client_accepts_arguments_during_and_after_mock():
