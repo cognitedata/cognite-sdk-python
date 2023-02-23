50c91e9fbe8d09e136cea36503b60f1b5323f255
Roman Chesnokov <66942939+chessnocow@users.noreply.github.com>
2023-02-23 16:38:29 +0100

    Update docs about TransformationBlockedInfo class.  (#1161)
    Co-authored-by: Erlend vollset <erlendvollset@gmail.com>

--- cognite/client/data_classes/transformations/common.py
+++ cognite/client/data_classes/transformations/common.py
@@ -273,6 +273,13 @@ class NonceCredentials:
 
 
 class TransformationBlockedInfo:
+    """Information about the reason why and when a transformation is blocked.
+
+    Args:
+        reason (str): Reason why the transformation is blocked.
+        created_time (Optional[int]): Timestamp when the transformation was blocked.
+    """
+
     def __init__(self, reason: str = None, created_time: Optional[int] = None):
         self.reason = reason
         self.created_time = created_time
