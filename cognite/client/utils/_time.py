da048f3ffb76cb4ca25fcbbaac745113322d4d6e
Anders Albert <60234212+doctrino@users.noreply.github.com>
2023-04-27 07:03:22 +0200

    Update cognite/client/utils/_time.py
    Co-authored-by: Håkon V. Treider <haakonvt@gmail.com>

--- cognite/client/utils/_time.py
+++ cognite/client/utils/_time.py
@@ -520,7 +520,7 @@ def pandas_date_range_tz(start: datetime, end: datetime, freq: str, inclusive: s
     Assumes that start and end have the same timezone.
     """
     pd = local_import("pandas")
-    # There is a bug in date_range which makes it fail to handle ambiguous timesstamps when you use time zone aware
+    # There is a bug in date_range which makes it fail to handle ambiguous timestamps when you use time zone aware
     # datetimes. This is a workaround by passing the time zone as an argument to the function.
     # In addition, pandas struggle with ZoneInfo objects, so  we convert it to a string, so pandas can use its own
     # tzdata implementation.
