f879dbd030d174828c4efe53c0aadb60ab02c944
Nimesha Wjewardana <109508509+nimeshawij@users.noreply.github.com>
2023-05-12 08:04:16 +0530

    reformatted cognite/client/_api/transformations/schema.py

--- cognite/client/_api/transformations/schema.py
+++ cognite/client/_api/transformations/schema.py
@@ -37,10 +37,14 @@ class TransformationSchemaAPI(APIClient):
         """
 
         url_path = utils._auxiliary.interpolate_and_url_encode(self._RESOURCE_PATH + "/{}", str(destination.type))
+        filter = destination.dump(True)
+        filter.pop("type")
         other_params = {"conflictMode": conflict_mode} if conflict_mode else None
+
         return self._list(
             list_cls=TransformationSchemaColumnList,
             resource_cls=TransformationSchemaColumn,
             method="GET",
             resource_path=url_path,
+            filter=other_params,
         )
