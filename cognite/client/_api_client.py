1ce5bfe0b050a77387d55115db43e1ccc87eb6fb
anders-albert <anders0albert@gmail.com>
2023-05-31 13:22:50 +0200

    fix: Do not pass filter unlessed used

--- cognite/client/_api_client.py
+++ cognite/client/_api_client.py
@@ -398,9 +398,11 @@ class APIClient:
                         params["sort"] = sort
                     res = self._get(url_path=url_path or resource_path, params=params, headers=headers)
                 elif method == "POST":
-                    body = {"filter": filter, "limit": current_limit, "cursor": next_cursor, **(other_params or {})}
+                    body = {"limit": current_limit, "cursor": next_cursor, **(other_params or {})}
+                    if filter:
+                        body["filter"] = filter  # type: ignore[assignment]
                     if sort is not None:
-                        body["sort"] = sort
+                        body["sort"] = sort  # type: ignore[assignment]
                     res = self._post(url_path=url_path or resource_path + "/list", json=body, headers=headers)
                 else:
                     raise ValueError(f"_list_generator parameter `method` must be GET or POST, not {method}")
