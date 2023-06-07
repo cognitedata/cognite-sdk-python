182c52763f188aa3a3b8cb93ced661846068b11e
Nimesha Wjewardana <109508509+nimeshawij@users.noreply.github.com>
2023-06-07 10:40:31 +0530

    reformatted tests/tests_integration/test_api/test_transformations/test_schema.py

--- tests/tests_integration/test_api/test_transformations/test_schema.py
+++ tests/tests_integration/test_api/test_transformations/test_schema.py
@@ -4,6 +4,7 @@ from cognite.client.data_classes.transformations.common import (
     ViewInfo,
 )
 
+
 class TestTransformationSchemaAPI:
     def test_assets(self, cognite_client):
         asset_columns = cognite_client.transformations.schema.retrieve(destination=TransformationDestination.assets())
@@ -36,14 +37,15 @@ class TestTransformationSchemaAPI:
         assert len([col for col in asset_columns if col.name == "externalId"]) > 0
 
     def test_nodes_with_view(self, cognite_client):
-        nodes_columns = cognite_client.transformations.schema.retrieve(destination=TransformationDestination.nodes(
-            view=ViewInfo(
-                space="test-space", external_id="testInstanceViewExternalId", version="testInstanceViewVersion"
+        nodes_columns = cognite_client.transformations.schema.retrieve(
+            destination=TransformationDestination.nodes(
+                view=ViewInfo(
+                    space="test-space", external_id="testInstanceViewExternalId", version="testInstanceViewVersion"
                 ),
-            instance_space="test-space"
+                instance_space="test-space",
             ),
-            conflict_mode= "upsert",
-            instance_type = "nodes"
+            conflict_mode="upsert",
+            instance_type="nodes",
         )
         assert len(nodes_columns) > 0
         assert len([col for col in nodes_columns if col.name == "space"]) > 0
@@ -51,14 +53,15 @@ class TestTransformationSchemaAPI:
         assert [col for col in nodes_columns if col.name == "externalId"][0].type.type == "string"
 
     def test_edges_with_view(self, cognite_client):
-        edges_columns = cognite_client.transformations.schema.retrieve(destination=TransformationDestination.edges(
-            view=ViewInfo(
-                space="test-space", external_id="testInstanceViewExternalId", version="testInstanceViewVersion"
+        edges_columns = cognite_client.transformations.schema.retrieve(
+            destination=TransformationDestination.edges(
+                view=ViewInfo(
+                    space="test-space", external_id="testInstanceViewExternalId", version="testInstanceViewVersion"
+                ),
+                instance_space="test-space",
             ),
-            instance_space="test-space"
-        ),
             conflict_mode="upsert",
-            instance_type="edges"
+            instance_type="edges",
         )
         assert len(edges_columns) > 0
         assert len([col for col in edges_columns if col.name == "space"]) > 0
@@ -66,18 +69,19 @@ class TestTransformationSchemaAPI:
         assert [col for col in edges_columns if col.name == "externalId"][0].type.type == "string"
 
     def test_instance_type_data_model(self, cognite_client):
-        type_columns = cognite_client.transformations.schema.retrieve(destination=TransformationDestination.instances(
-        data_model=DataModelInfo(
-            space="authorBook",
-            external_id="author_book",
-            version="2",
-            destination_type="AuthorBook_relation",
-            destination_relationship_from_type=None,
-        ),
-        instance_space="test-instanceSpace",
-    ),
+        type_columns = cognite_client.transformations.schema.retrieve(
+            destination=TransformationDestination.instances(
+                data_model=DataModelInfo(
+                    space="authorBook",
+                    external_id="author_book",
+                    version="2",
+                    destination_type="AuthorBook_relation",
+                    destination_relationship_from_type=None,
+                ),
+                instance_space="test-instanceSpace",
+            ),
             conflict_mode="upsert",
-            instance_type="nodes"
+            instance_type="nodes",
         )
         assert len(type_columns) > 0
         assert len([col for col in type_columns if col.name == "space"]) > 0
@@ -85,18 +89,19 @@ class TestTransformationSchemaAPI:
         assert [col for col in type_columns if col.name == "externalId"][0].type.type == "string"
 
     def test_instance_relationship_data_model(self, cognite_client):
-        type_columns = cognite_client.transformations.schema.retrieve(destination=TransformationDestination.instances(
-        data_model=DataModelInfo(
-            space="authorBook",
-            external_id="author_book",
-            version="2",
-            destination_type="AuthorBook_relation",
-            destination_relationship_from_type="author_book",
-        ),
-        instance_space="test-instanceSpace",
-    ),
+        type_columns = cognite_client.transformations.schema.retrieve(
+            destination=TransformationDestination.instances(
+                data_model=DataModelInfo(
+                    space="authorBook",
+                    external_id="author_book",
+                    version="2",
+                    destination_type="AuthorBook_relation",
+                    destination_relationship_from_type="author_book",
+                ),
+                instance_space="test-instanceSpace",
+            ),
             conflict_mode="upsert",
-            instance_type="edges"
+            instance_type="edges",
         )
         assert len(type_columns) > 0
         assert len([col for col in type_columns if col.name == "space"]) > 0
