727b1b938dc101602a85878fbe86077fa063b114
anders-albert <anders0albert@gmail.com>
2023-06-07 12:01:27 +0200

    tests: Added failing test

--- tests/tests_unit/test_data_classes/test_data_models/test_ids.py
+++ tests/tests_unit/test_data_classes/test_data_models/test_ids.py
@@ -1,4 +1,8 @@
-from cognite.client.data_classes.data_modeling.ids import ContainerId, ViewId
+from __future__ import annotations
+
+import pytest
+
+from cognite.client.data_classes.data_modeling.ids import ContainerId, ViewId, load_identifier
 
 
 class TestContainerReference:
@@ -13,3 +17,20 @@ class TestViewReference:
         data = {"space": "mySpace", "externalId": "myId", "version": "myVersion", "type": "view"}
 
         assert data == ViewId.load(data).dump(camel_case=True)
+
+
+class TestLoadIdentifier:
+    @pytest.mark.parametrize(
+        "ids, expected_dict, expected_is_singleton",
+        [
+            (("space", "container"), [{"space": "space", "externalId": "container"}], True),
+            ([("space", "container")], [{"space": "space", "externalId": "container"}], False),
+        ],
+    )
+    def test_load(self, ids, expected_dict: list | dict, expected_is_singleton: bool):
+        identifier = load_identifier(ids)
+
+        assert identifier.as_dicts() == expected_dict
+        assert (
+            identifier.is_singleton() == expected_is_singleton
+        ), f"Expected {expected_is_singleton} but got {identifier.is_singleton()}"
