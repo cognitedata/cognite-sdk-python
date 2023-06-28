adc01cf0069e5bebeee8edc9d7881c837bfaa195
anders-albert <anders0albert@gmail.com>
2023-06-28 08:55:34 +0200

    tests: Improved fixture

--- tests/tests_integration/test_api/test_data_modeling/test_views.py
+++ tests/tests_integration/test_api/test_data_modeling/test_views.py
@@ -15,16 +15,29 @@ from cognite.client.exceptions import CogniteAPIError
 
 
 @pytest.fixture()
-def cdf_views(cognite_client: CogniteClient) -> ViewList:
-    views = cognite_client.data_modeling.views.list(limit=-1)
-    assert len(views), "There must be at least one view in CDF"
-    return views
+def movie_views(cognite_client: CogniteClient) -> ViewList:
+    movie_view_ids = [
+        ViewId(space="IntegrationTestsImmutable", external_id="Actor", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="BestDirector", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="BestLeadingActor", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="BestLeadingActress", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="Director", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="Movie", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="Nomination", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="Person", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="Rating", version="2"),
+        ViewId(space="IntegrationTestsImmutable", external_id="Role", version="2"),
+    ]
+
+    movie_views = cognite_client.data_modeling.views.retrieve(ids=movie_view_ids)
+    assert len(movie_view_ids) == len(movie_views), "Some of the movie views are missing, please recreate them."
+    return movie_views
 
 
 class TestViewsAPI:
-    def test_list(self, cognite_client: CogniteClient, cdf_views: ViewList, integration_test_space: Space) -> None:
+    def test_list(self, cognite_client: CogniteClient, movie_views: ViewList, integration_test_space: Space) -> None:
         # Arrange
-        expected_views = ViewList([v for v in cdf_views if v.space == integration_test_space.space])
+        expected_views = ViewList([v for v in movie_views if v.space == integration_test_space.space])
 
         # Act
         actual_views = cognite_client.data_modeling.views.list(space=integration_test_space.space, limit=-1)
@@ -79,10 +92,9 @@ class TestViewsAPI:
             == []
         )
 
-    def test_retrieve_without_inherited_properties(self, cognite_client: CogniteClient, cdf_views: ViewList) -> None:
-        assert len(cdf_views) >= 1, "Please add at least one view to the test environment"
+    def test_retrieve_without_inherited_properties(self, cognite_client: CogniteClient, movie_views: ViewList) -> None:
         # Arrange
-        view = cdf_views[0]
+        view = movie_views[0]
 
         # Act
         retrieved = cognite_client.data_modeling.views.retrieve(view.as_id(), include_inherited_properties=False)[0]
@@ -90,10 +102,9 @@ class TestViewsAPI:
         # Assert
         assert retrieved is not None
 
-    def test_retrieve_multiple(self, cognite_client: CogniteClient, cdf_views: ViewList) -> None:
-        assert len(cdf_views) >= 2, "Please add at least two views to the test environment"
+    def test_retrieve_multiple(self, cognite_client: CogniteClient, movie_views: ViewList) -> None:
         # Arrange
-        ids = [ViewId(v.space, v.external_id, v.version) for v in cdf_views]
+        ids = [ViewId(v.space, v.external_id, v.version) for v in movie_views]
 
         # Act
         retrieved = cognite_client.data_modeling.views.retrieve(ids)
@@ -101,10 +112,9 @@ class TestViewsAPI:
         # Assert
         assert [view.as_id() for view in retrieved] == ids
 
-    def test_retrieve_multiple_with_missing(self, cognite_client: CogniteClient, cdf_views: ViewList) -> None:
-        assert len(cdf_views) >= 2, "Please add at least two views to the test environment"
+    def test_retrieve_multiple_with_missing(self, cognite_client: CogniteClient, movie_views: ViewList) -> None:
         # Arrange
-        ids_without_missing = [v.as_id() for v in cdf_views]
+        ids_without_missing = [v.as_id() for v in movie_views]
         ids_with_missing = [*ids_without_missing, ViewId("myNonExistingSpace", "myImaginaryView", "v0")]
 
         # Act
