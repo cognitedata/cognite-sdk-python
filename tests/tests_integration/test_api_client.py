e93ed7a2680ced8b4fc72d00eabe9a53baf7653b
anders-albert <anders0albert@gmail.com>
2023-07-19 09:18:22 +0200

    tests: Added a few more test for completeness

--- tests/tests_integration/test_api_client.py
+++ tests/tests_integration/test_api_client.py
@@ -8,7 +8,7 @@ import pytest
 from pytest import MonkeyPatch
 
 from cognite.client import CogniteClient
-from cognite.client.data_classes import Event
+from cognite.client.data_classes import Asset, Event
 from cognite.client.exceptions import CogniteAPIError, CogniteNotFoundError
 
 
@@ -275,3 +275,58 @@ class TestAPIClientUpsert:
         finally:
             # Just in case the even gets created
             cognite_client.events.delete(id=new_event.id, ignore_unknown_ids=True)
+
+    def test_upsert_with_empty_external_id(self, cognite_client: CogniteClient):
+        new_asset = Asset(
+            external_id="test_upsert_with_empty_external_id:new_asset",
+            name="test_upsert_with_empty_external_id",
+        )
+        existing_asset = Asset(
+            external_id="",
+            name="test_upsert_with_empty_external_id",
+        )
+        update_asset = Asset._load(existing_asset.dump(camel_case=True))
+        update_asset.name = "test_upsert_with_empty_external_id_updated"
+
+        try:
+            created = cognite_client.assets.create(existing_asset)
+            assert created.id is not None
+
+            # Act
+            updated = cognite_client.assets.upsert([new_asset, update_asset], mode="replace")
+
+            # Assert
+            assert updated[1].id == created.id
+            assert updated[1].name == update_asset.name
+        finally:
+            cognite_client.assets.delete(external_id=existing_asset.external_id, ignore_unknown_ids=True)
+
+    def test_upsert_with_patch_option(self, cognite_client: CogniteClient):
+        # Arrange
+        existing_event = Event(
+            external_id="test_upsert_with_patch_option:existing",
+            type="mypType1",
+            start_time=0,
+            end_time=1,
+            subtype="mySubType1",
+        )
+        updated_event = Event(
+            external_id=existing_event.external_id,
+            subtype="mySubType2",
+        )
+
+        try:
+            created = cognite_client.events.create(existing_event)
+            assert created.id is not None
+
+            # Act
+            updated = cognite_client.events.upsert(updated_event, mode="patch")
+
+            # Assert
+            assert updated.id == created.id
+            assert updated.subtype == updated_event.subtype
+            assert updated.type == existing_event.type
+            assert updated.start_time == existing_event.start_time
+            assert updated.end_time == existing_event.end_time
+        finally:
+            cognite_client.events.delete(external_id=existing_event.external_id, ignore_unknown_ids=True)
