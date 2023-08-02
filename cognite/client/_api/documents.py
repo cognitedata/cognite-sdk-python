91d4c90a26b34d6985595e37e618b0c966036d54
anders-albert <anders0albert@gmail.com>
2023-08-02 09:32:20 +0200

    refactor: Started implementing document class

--- cognite/client/_api/documents.py
+++ cognite/client/_api/documents.py
@@ -1,17 +1,22 @@
+from __future__ import annotations
+
 from cognite.client._api_client import APIClient
+from cognite.client._constants import DOCUMENT_LIST_LIMIT_DEFAULT
+from cognite.client.data_classes.documents import DocumentList
+from cognite.client.data_classes.filters import Filter
 
 
 class DocumentsAPI(APIClient):
     _RESOURCE_PATH = "/documents"
 
-    def aggregate(self):
+    def aggregate(self) -> None:
         ...
 
-    def retrieve(self, id: int):
+    def retrieve_content(self, id: int) -> str:
         ...
 
-    def search(self):
+    def search(self) -> None:
         ...
 
-    def list(self):
+    def list(self, filter: Filter | dict | None = None, limit: int = DOCUMENT_LIST_LIMIT_DEFAULT) -> DocumentList:
         ...
