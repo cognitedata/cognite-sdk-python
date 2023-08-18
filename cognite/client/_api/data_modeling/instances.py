781a2fa3ff9f5aa39acf59115d81ba6daa0cd6aa
Håkon V. Treider <haakonvt@gmail.com>
2023-08-19 00:23:52 +0200

    convert some clsmethods to static

--- cognite/client/_api/data_modeling/instances.py
+++ cognite/client/_api/data_modeling/instances.py
@@ -68,8 +68,8 @@ class _NodeOrEdgeList(CogniteResourceList):
 
 
 class _NodeOrEdgeResourceAdapter:
-    @classmethod
-    def _load(cls, data: str | dict, cognite_client: Optional[CogniteClient] = None) -> Node | Edge:
+    @staticmethod
+    def _load(data: str | dict, cognite_client: Optional[CogniteClient] = None) -> Node | Edge:
         data = json.loads(data) if isinstance(data, str) else data
         if data["instanceType"] == "node":
             return Node.load(data)
@@ -95,10 +95,8 @@ class _NodeOrEdgeApplyResultList(CogniteResourceList):
 
 
 class _NodeOrEdgeApplyResultAdapter:
-    @classmethod
-    def _load(
-        cls, data: str | dict, cognite_client: Optional[CogniteClient] = None
-    ) -> NodeApplyResult | EdgeApplyResult:
+    @staticmethod
+    def _load(data: str | dict, cognite_client: Optional[CogniteClient] = None) -> NodeApplyResult | EdgeApplyResult:
         data = json.loads(data) if isinstance(data, str) else data
         if data["instanceType"] == "node":
             return NodeApplyResult.load(data)
@@ -106,8 +104,8 @@ class _NodeOrEdgeApplyResultAdapter:
 
 
 class _NodeOrEdgeApplyAdapter:
-    @classmethod
-    def _load(cls, data: str | dict, cognite_client: Optional[CogniteClient] = None) -> NodeApply | EdgeApply:
+    @staticmethod
+    def _load(data: str | dict, cognite_client: Optional[CogniteClient] = None) -> NodeApply | EdgeApply:
         data = json.loads(data) if isinstance(data, str) else data
         if data["instanceType"] == "node":
             return NodeApply.load(data)
@@ -395,16 +393,14 @@ class InstancesAPI(APIClient):
             other_params["instanceType"] = instance_type
         return other_params
 
-    @classmethod
-    def _dump_instance_source(
-        cls, sources: ViewIdentifier | Sequence[ViewIdentifier] | View | Sequence[View]
-    ) -> list[dict]:
+    @staticmethod
+    def _dump_instance_source(sources: ViewIdentifier | Sequence[ViewIdentifier] | View | Sequence[View]) -> list[dict]:
         return [
             {"source": ViewId.load(dct).dump(camel_case=True)} for dct in _load_identifier(sources, "view").as_dicts()
         ]
 
-    @classmethod
-    def _dump_instance_sort(cls, sort: InstanceSort | dict) -> dict:
+    @staticmethod
+    def _dump_instance_sort(sort: InstanceSort | dict) -> dict:
         return sort.dump(camel_case=True) if isinstance(sort, InstanceSort) else sort
 
     def apply(
