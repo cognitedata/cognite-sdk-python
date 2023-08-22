2380e6a9a33a41b2963c077cd767222ff3e063d5
Erlend vollset <erlendvollset@gmail.com>
2023-08-22 11:38:13 +0200

    Retry all data modeling endpoints (#1325)

--- cognite/client/_api_client.py
+++ cognite/client/_api_client.py
@@ -78,6 +78,7 @@ class APIClient:
             "/raw/dbs/[^/]+/tables/[^/]+",
             "/context/entitymatching/(byids|list|jobs)",
             "/sessions/revoke",
+            "/models/.*",
         )
     }
 
