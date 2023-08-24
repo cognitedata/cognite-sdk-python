104f63fbb02babb07774f72ac5d665d820978892
Håkon V. Treider <haakonvt@gmail.com>
2023-08-24 12:03:56 +0200

    Bump pydoclint after yield-bugfix

--- cognite/client/_api/datapoint_tasks.py
+++ cognite/client/_api/datapoint_tasks.py
@@ -741,17 +741,10 @@ class SplittingFetchSubtask(SerialFetchSubtask):
         return None
 
     def _create_subtasks_idxs(self, n_new_tasks: int) -> Generator[tuple[float, ...], None, None]:
-        """Since this task may decide to split itself multiple times, we count backwards to keep order
-        (we rely on tuple sorting logic). Example using `self.subtask_idx=(4,)`:
-        - First split into e.g. 3 parts: (4,-3), (4,-2), (4,-1)
-        - Next, split into 2: (4, -5) and (4, -4). These now sort before the first split.
-
-        Args:
-            n_new_tasks (int): No description.
-
-        Yields:
-            tuple[float, ...]: No description.
-        """
+        # Since this task may decide to split itself multiple times, we count backwards to keep order
+        # (we rely on tuple sorting logic). Example using `self.subtask_idx=(4,)`:
+        # - First split into e.g. 3 parts: (4,-3), (4,-2), (4,-1)
+        # - Next, split into 2: (4, -5) and (4, -4). These now sort before the first split.
         end = self.split_subidx
         self.split_subidx -= n_new_tasks
         yield from ((*self.subtask_idx, i) for i in range(self.split_subidx, end))
