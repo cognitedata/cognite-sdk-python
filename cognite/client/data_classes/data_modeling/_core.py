6b80bf172775b5b7cb151608b6b0733e854ffce7
Håkon V. Treider <haakonvt@gmail.com>
2023-08-27 07:06:04 +0200

    Change base of InstanceSort and DataModelsSort. Bugfix Select.load of InstanceSort

--- cognite/client/data_classes/data_modeling/_core.py
+++ cognite/client/data_classes/data_modeling/_core.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import json
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, Any, Literal, TypeVar
 
-from cognite.client.data_classes._base import CogniteResource
+from cognite.client.data_classes._base import CogniteResource, basic_instance_dump
+from cognite.client.utils._auxiliary import json_dump_default
 from cognite.client.utils._text import convert_all_keys_to_snake_case
 
 if TYPE_CHECKING:
@@ -38,3 +39,40 @@ class DataModelingResource(CogniteResource):
 
 
 T_DataModelingResource = TypeVar("T_DataModelingResource", bound=DataModelingResource)
+
+
+class DataModelingSort:
+    def __init__(
+        self,
+        property: str | list[str] | tuple[str, ...],
+        direction: Literal["ascending", "descending"] = "ascending",
+        nulls_first: bool = False,
+    ) -> None:
+        self.property = property
+        self.direction = direction
+        self.nulls_first = nulls_first
+
+    def __str__(self) -> str:
+        return json.dumps(self.dump(), default=json_dump_default, indent=4)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    @classmethod
+    def load(cls: type[T_DataModelingSort], resource: dict) -> T_DataModelingSort:
+        if not isinstance(resource, dict):
+            raise TypeError(f"Resource must be mapping, not {type(resource)}")
+
+        instance = cls(property=resource["property"])
+        if "direction" in resource:
+            instance.direction = resource["direction"]
+        if "nulls_first" in resource:
+            instance.nulls_first = resource["nullsFirst"]
+
+        return instance
+
+    def dump(self, camel_case: bool = False) -> dict[str, Any]:
+        return basic_instance_dump(self, camel_case=camel_case)
+
+
+T_DataModelingSort = TypeVar("T_DataModelingSort", bound=DataModelingSort)
