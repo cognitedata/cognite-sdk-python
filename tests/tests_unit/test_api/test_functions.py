e2009be52c5b368b6f9763373056eb9148adcce2
Håkon V. Treider <haakonvt@gmail.com>
2023-09-05 20:02:22 +0200

    update fn tests

--- tests/tests_unit/test_api/test_functions.py
+++ tests/tests_unit/test_api/test_functions.py
@@ -410,10 +410,10 @@ class TestFunctionsAPI:
     def test_validate_folder(self, function_folder, function_path, exception):
         folder = os.path.join(os.path.dirname(__file__), "function_test_resources", function_folder)
         if exception is None:
-            validate_function_folder(folder, function_path, should_check_imports=False)
+            validate_function_folder(folder, function_path, skip_folder_validation=True)
         else:
             with pytest.raises(exception):
-                validate_function_folder(folder, function_path, should_check_imports=False)
+                validate_function_folder(folder, function_path, skip_folder_validation=True)
 
     @pytest.mark.parametrize(
         "function_folder, function_path, exception",
@@ -427,10 +427,10 @@ class TestFunctionsAPI:
     def test_imports_in_validate_folder(self, function_folder, function_path, exception):
         folder = os.path.join(os.path.dirname(__file__), "function_test_resources", function_folder)
         if exception is None:
-            validate_function_folder(folder, function_path, should_check_imports=True)
+            validate_function_folder(folder, function_path, skip_folder_validation=False)
         else:
             with pytest.raises(exception):
-                validate_function_folder(folder, function_path, should_check_imports=True)
+                validate_function_folder(folder, function_path, skip_folder_validation=False)
 
     @pytest.mark.parametrize(
         "function_folder, function_name, exception",
