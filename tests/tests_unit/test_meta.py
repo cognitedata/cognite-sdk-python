d3434dccdf91e18615bebce7ad32d35da1f657e7
Hayden Hohns <hayden.hohns@pgs.com>
2023-09-29 11:03:40 +0200

    attempt for doing camel case check in all classes

--- tests/tests_unit/test_meta.py
+++ tests/tests_unit/test_meta.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+ALL_FILEPATHS = Path("cognite/client/").glob("**/*.py")
+
 
 def test_assert_no_root_init_file():
     # We have an implicit namespace package under the namespace package directory: 'cognite'.
@@ -19,10 +21,20 @@ def test_ensure_all_files_use_future_annots():
         ]
         return all(skip not in str(path.as_posix()) for skip in skip_list)
 
-    all_filepaths = Path("cognite/client/").glob("**/*.py")
     err_msg = "File: '{}' is missing 'from __future__ import annotations' at line=0"
-
-    for filepath in filter(keep, all_filepaths):
+    for filepath in filter(keep, ALL_FILEPATHS):
         with filepath.open("r") as file:
             # We just read the first line from each file:
             assert file.readline() == "from __future__ import annotations\n", err_msg.format(filepath)
+
+
+def test_ensure_all_tests_use_camel_case_except_dump():
+    err_msg = "File: '{}' contains camel_case=True and not a dump at line {}."
+    for filepath in ALL_FILEPATHS:
+        if "tests/" in str(filepath):
+            with filepath.open("r") as file:
+                for line_num, line in enumerate(file.readlines()):
+                    if ".dump(camel_case" in line:
+                        continue
+
+                    assert "camel_case=True" not in line, err_msg.format(filepath, line_num)
