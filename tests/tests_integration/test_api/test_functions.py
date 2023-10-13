c4d843510ced1237982b5d1fc418474080dede69
Dmytro Donukis <dmytro.donukis@cognite.com>
2023-10-13 21:19:44 +0200

    add integration test for get_schedules

--- tests/tests_integration/test_api/test_functions.py
+++ tests/tests_integration/test_api/test_functions.py
@@ -16,3 +16,15 @@ class TestFunctionsAPI:
     def test_retrieve_unknown_ignore_unknowns(self, cognite_client: CogniteClient):
         res = cognite_client.functions.retrieve_multiple(external_ids=["this does not exist"], ignore_unknown_ids=True)
         assert len(res) == 0
+
+    def test_function_get_schedules(self, cognite_client: CogniteClient):
+        fn = cognite_client.functions.retrieve(external_id="integration_test-dummy")
+
+        schedules = fn.list_schedules(limit=15)
+        assert len({s.cron_expression for s in schedules}) == len({s.id for s in schedules}) == 15
+
+    def test_function_get_schedules_unlimited(self, cognite_client: CogniteClient):
+        fn = cognite_client.functions.retrieve(external_id="integration_test-dummy")
+
+        schedules = fn.list_schedules(limit=-1)
+        assert len({s.cron_expression for s in schedules}) == len({s.id for s in schedules}) == 100
