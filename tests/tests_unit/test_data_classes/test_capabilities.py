2a7c3435636391d61ce0d197302d4aaf8312ee8a
Håkon V. Treider <haakonvt@gmail.com>
2023-11-07 11:07:30 +0100

    add test test_loads_to_known on all acls to ensure no known becomes unknown

--- tests/tests_unit/test_data_classes/test_capabilities.py
+++ tests/tests_unit/test_data_classes/test_capabilities.py
@@ -13,6 +13,71 @@ from cognite.client.data_classes.capabilities import (
 )
 
 
+def all_acls():
+    yield from [
+        {"assetsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"assetsAcl": {"actions": ["READ", "WRITE"], "scope": {"datasetScope": {"ids": ["372"]}}}},
+        {"dataModelInstancesAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"dataModelsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"datasetsAcl": {"actions": ["READ", "WRITE", "OWNER"], "scope": {"all": {}}}},
+        {"datasetsAcl": {"actions": ["READ", "WRITE", "OWNER"], "scope": {"idScope": {"ids": ["2918026428"]}}}},
+        {"entitymatchingAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"eventsAcl": {"actions": ["READ", "WRITE"], "scope": {"datasetScope": {"ids": ["372"]}}}},
+        {"eventsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"eventsAcl": {"actions": ["READ"], "scope": {"datasetScope": {"ids": ["233257", "372989"]}}}},
+        {"extractionConfigsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"extractionPipelinesAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"extractionRunsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"filesAcl": {"actions": ["READ", "WRITE"], "scope": {"datasetScope": {"ids": ["2332579", "372"]}}}},
+        {"filesAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"functionsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"groupsAcl": {"actions": ["READ", "CREATE", "UPDATE", "DELETE"], "scope": {"currentuserscope": {}}}},
+        {"groupsAcl": {"actions": ["LIST", "READ", "DELETE", "UPDATE", "CREATE"], "scope": {"all": {}}}},
+        {"labelsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"projectsAcl": {"actions": ["UPDATE", "LIST", "READ"], "scope": {"all": {}}}},
+        {"rawAcl": {"actions": ["READ", "WRITE", "LIST"], "scope": {"all": {}}}},
+        {
+            "rawAcl": {
+                "actions": ["READ", "WRITE", "LIST"],
+                "scope": {"tableScope": {"dbsToTables": {"test db 1": {"tables": ["empty tbl", "test tbl 1"]}}}},
+            }
+        },
+        {
+            "rawAcl": {
+                "actions": ["READ", "WRITE", "LIST"],
+                "scope": {"tableScope": {"dbsToTables": {"no table in this": {}}}},
+            }
+        },
+        {"relationshipsAcl": {"actions": ["READ"], "scope": {"all": {}}}},
+        {"relationshipsAcl": {"actions": ["READ"], "scope": {"datasetScope": {"ids": ["372", "2332579"]}}}},
+        {
+            "securityCategoriesAcl": {
+                "actions": ["MEMBEROF", "LIST", "CREATE", "UPDATE", "DELETE"],
+                "scope": {"idscope": {"ids": ["7398", "3018"]}},
+            }
+        },
+        {
+            "securityCategoriesAcl": {
+                "actions": ["DELETE", "MEMBEROF", "LIST", "CREATE", "UPDATE"],
+                "scope": {"all": {}},
+            }
+        },
+        {"seismicAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"sequencesAcl": {"actions": ["WRITE"], "scope": {"datasetScope": {"ids": ["2332579", "372"]}}}},
+        {"sequencesAcl": {"actions": ["READ"], "scope": {"all": {}}}},
+        {"sessionsAcl": {"actions": ["LIST", "CREATE", "DELETE"], "scope": {"all": {}}}},
+        {"templateGroupsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"templateInstancesAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"threedAcl": {"actions": ["READ", "CREATE", "UPDATE", "DELETE"], "scope": {"all": {}}}},
+        {"timeSeriesAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+        {"timeSeriesAcl": {"actions": ["READ"], "scope": {"idscope": {"ids": ["2495"]}}}},
+        {"timeSeriesAcl": {"actions": ["READ", "WRITE"], "scope": {"datasetScope": {"ids": ["233579", "372"]}}}},
+        {"timeSeriesAcl": {"actions": ["WRITE", "READ"], "scope": {"assetRootIdScope": {"rootIds": ["58"]}}}},
+        {"transformationsAcl": {"actions": ["READ", "WRITE"], "scope": {"datasetScope": {"ids": ["94"]}}}},
+        {"transformationsAcl": {"actions": ["READ", "WRITE"], "scope": {"all": {}}}},
+    ]
+
+
 @pytest.fixture
 def cap_with_extra_key():
     return {
@@ -30,6 +95,15 @@ def unknown_cap_with_extra_key():
 
 
 class TestCapabilities:
+    @pytest.mark.parametrize("access_control", list(all_acls()))
+    def test_loads_to_known(self, access_control):
+        cap = Capability.load(access_control)
+
+        assert not isinstance(cap, UnknownAcl)
+        assert not isinstance(cap.scope, UnknownScope)
+        for action in cap.actions:
+            assert action is not UnknownAcl.Action.Unknown
+
     @pytest.mark.parametrize(
         "raw",
         [
