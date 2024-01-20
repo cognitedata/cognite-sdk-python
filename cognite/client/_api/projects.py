e7ac55d59d3ce7798ff3fa24f9d980328ee12162
anders-albert <anders0albert@gmail.com>
2024-01-20 13:33:04 +0100

    docs: Set docstrings

--- cognite/client/_api/projects.py
+++ cognite/client/_api/projects.py
@@ -19,16 +19,62 @@ class ProjectsAPI(APIClient):
         ...
 
     def create(self, item: ProjectWrite | Sequence[ProjectWrite]) -> Project | ProjectList:
-        """`Create a project <https://developer.cognite.com/api#tag/Projects/operation/createProject>`_"""
+        """`Create a project <https://developer.cognite.com/api#tag/Projects/operation/createProject>`_
+
+        Args:
+            item (ProjectWrite | Sequence[ProjectWrite]): Project(s) to create
+
+        Returns:
+            Project | ProjectList: Created project(s)
+
+        Examples:
+            Create a new project with the name "my project"
+
+            >>> from cognite.client import CogniteClient
+            >>> from cognite.client.data_classes import ProjectWrite
+            >>> c = CogniteClient()
+            >>> project = ProjectWrite(name="my project", url_name="my-project", parent_project_url_name="root")
+            >>> res = c.iam.projects.create(project)
+        """
         return self._create_multiple(item, list_cls=ProjectList, resource_cls=Project, input_resource_cls=ProjectWrite)
 
     def retrieve(self, project: str) -> Project:
-        """`Retrieve a project <https://developer.cognite.com/api#tag/Projects/operation/getProject>`_"""
+        """`Retrieve a project <https://developer.cognite.com/api#tag/Projects/operation/getProject>`_
+
+        Args:
+            project (str): Project to retrieve
+
+        Returns:
+            Project: The requested project
+
+        Examples:
+
+            Retrieve the project with the name "publicdata"
+
+                >>> from cognite.client import CogniteClient
+                >>> c = CogniteClient()
+                >>> res = c.iam.projects.retrieve("publicdata")
+        """
         item = self._get(f"{self._RESOURCE_PATH}/{quote(project, '')}")
         return Project._load(item.json(), cognite_client=self._cognite_client)
 
     def update(self, item: ProjectUpdate) -> Project:
-        """`Update a project <https://developer.cognite.com/api#tag/Projects/operation/updateProject>`_"""
+        """`Update a project <https://developer.cognite.com/api#tag/Projects/operation/updateProject>`_
+
+        Args:
+            item (ProjectUpdate): Project to update
+
+        Returns:
+            Project: Updated project
+
+        Examples:
+
+            >>> from cognite.client import CogniteClient
+            >>> from cognite.client.data_classes import ProjectUpdate
+            >>> c = CogniteClient()
+            >>> my_update = ProjectUpdate("my_project").name.set("new name").oidc_configuration.modify.skew_ms.set(100)
+            >>> res = c.iam.projects.update(my_update)
+        """
         project = item._project
         response = self._post(
             url_path=f"{self._RESOURCE_PATH}/{quote(project, '')}/update", json=item.dump(camel_case=True)
@@ -36,6 +82,18 @@ class ProjectsAPI(APIClient):
         return Project._load(response.json(), cognite_client=self._cognite_client)
 
     def list(self) -> ProjectURLNameList:
-        """`List all projects <https://developer.cognite.com/api#tag/Projects/operation/listProjects>`_"""
+        """`List all projects <https://developer.cognite.com/api#tag/Projects/operation/listProjects>`_
+
+        Returns:
+            ProjectURLNameList: List of project URL names
+
+        Examples:
+
+            List all projects
+
+                >>> from cognite.client import CogniteClient
+                >>> c = CogniteClient()
+                >>> res = c.iam.projects.list()
+        """
         items = self._get(self._RESOURCE_PATH)
         return ProjectURLNameList.load(items.json(), cognite_client=self._cognite_client)
