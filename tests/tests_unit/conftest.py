b3a064bac28ccabac81703a96177a5a046bce139
Håkon V. Treider <haakonvt@gmail.com>
2024-01-22 11:26:28 +0100

    update tests

--- tests/tests_unit/conftest.py
+++ tests/tests_unit/conftest.py
@@ -8,7 +8,7 @@ from cognite.client import ClientConfig, CogniteClient
 from cognite.client.credentials import Token
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture
 def cognite_client():
     cnf = ClientConfig(client_name="any", project="dummy", credentials=Token("bla"))
     yield CogniteClient(cnf)
