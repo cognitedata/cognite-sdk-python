a5a38ee802dd4671ca6527660f364a6201b62f26
Håkon V. Treider <haakonvt@gmail.com>
2024-01-31 15:35:10 +0100

    push a test that's currently failing but shouldnt

--- tests/tests_unit/test_api_client.py
+++ tests/tests_unit/test_api_client.py
@@ -1249,6 +1249,8 @@ class TestRetryableEndpoints:
                     (method, "https://api.cognitedata.com/api/v1/projects/bla", True)
                     for method in {"GET", "PUT", "PATCH"}
                 ),
+                #### Annotations
+                ("POST", "https://api.c.com/api/v1/projects/bla/annotations/list", True),
                 #### Files
                 ("POST", "https://api.c.com/api/v1/projects/bla/files/downloadlink?extendedExpiration=true", True),
                 #### Timeseries
