bad18406657d4eae68997f7d99670f2a216f724b
anders-albert <anders0albert@gmail.com>
2024-02-28 16:47:22 +0100

    refactor: implement source selector

--- cognite/client/data_classes/data_modeling/query.py
+++ cognite/client/data_classes/data_modeling/query.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from collections import UserDict
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Any, Literal, Mapping, cast
+from typing import TYPE_CHECKING, Any, Literal, Mapping, Sequence, cast
 
 from typing_extensions import Self
 
 from cognite.client.data_classes._base import CogniteObject
-from cognite.client.data_classes.data_modeling.ids import ContainerId, PropertyId, ViewId
+from cognite.client.data_classes.data_modeling.ids import ContainerId, PropertyId, ViewId, ViewIdentifier
 from cognite.client.data_classes.data_modeling.instances import (
     Edge,
     EdgeListWithCursor,
@@ -17,7 +17,7 @@ from cognite.client.data_classes.data_modeling.instances import (
     NodeListWithCursor,
     PropertyValue,
 )
-from cognite.client.data_classes.data_modeling.views import PropertyUnit, SourceCore
+from cognite.client.data_classes.data_modeling.views import PropertyUnit, View
 from cognite.client.data_classes.filters import Filter
 from cognite.client.utils._importing import local_import
 
@@ -25,23 +25,58 @@ if TYPE_CHECKING:
     from cognite.client import CogniteClient
 
 
-class SourceSelector(SourceCore):
-    def __init__(self, source: ViewId, properties: list[str], target_units: list[PropertyUnit] | None = None):
-        super().__init__(source, target_units)
-        self.properties = properties
+@dataclass
+class SourceSelector(CogniteObject):
+    source: ViewId
+    properties: list[str] | None = None
+    target_units: list[PropertyUnit] | None = None
 
     def dump(self, camel_case: bool = True) -> dict[str, Any]:
-        output = super().dump(camel_case)
-        output["properties"] = self.properties
+        output: dict[str, Any] = {"source": self.source.dump(camel_case)}
+        if self.properties:
+            output["properties"] = self.properties
+        if self.target_units:
+            output["targetUnits" if camel_case else "target_units"] = [
+                unit.dump(camel_case) for unit in self.target_units
+            ]
         return output
 
     @classmethod
-    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
-        return cls(
-            source=ViewId.load(resource["source"]),
-            properties=resource["properties"],
-            target_units=[PropertyUnit.load(unit) for unit in resource.get("targetUnits", [])] or None,
-        )
+    def _load(
+        cls,
+        resource: dict[str, Any] | SourceSelector | ViewIdentifier | View,
+        cognite_client: CogniteClient | None = None,
+    ) -> Self:
+        if isinstance(resource, cls):
+            return resource
+        elif isinstance(resource, dict):
+            if "source" in resource:
+                view_id = ViewId.load(resource["source"])
+            else:
+                # This is in case only a ViewId is passed in.
+                view_id = ViewId.load(resource)
+            return cls(
+                source=view_id,
+                properties=resource.get("properties"),
+                target_units=[PropertyUnit.load(unit) for unit in resource.get("targetUnits", [])] or None,
+            )
+
+        if isinstance(resource, View):
+            view_id = resource.as_id()
+        else:
+            view_id = ViewId.load(resource)  # type: ignore[arg-type]
+        return cls(source=view_id)
+
+    @classmethod
+    def _load_list(
+        cls, data: ViewIdentifier | View | SourceSelector | Sequence[ViewIdentifier | View | SourceSelector]
+    ) -> list[SourceSelector]:
+        if isinstance(data, (View, SourceSelector, ViewId)) or (
+            isinstance(data, tuple) and 2 <= len(data) <= 3 and all(isinstance(v, str) for v in data)
+        ):
+            data = [data]
+
+        return [cls._load(v) for v in data]  # type: ignore[arg-type]
 
 
 @dataclass
