f2a206d69f6e1877ca3d47e893988f3f3f3b3e9a
Håkon V. Treider <haakonvt@gmail.com>
2024-03-20 23:52:19 +0100

    refactor SyntheticDatapointsAPI

--- cognite/client/_api/synthetic_time_series.py
+++ cognite/client/_api/synthetic_time_series.py
@@ -2,14 +2,16 @@ from __future__ import annotations
 
 import re
 from datetime import datetime
-from typing import TYPE_CHECKING, Any, cast
+from functools import cached_property
+from types import MappingProxyType
+from typing import TYPE_CHECKING, Any, Sequence, cast
 
 from cognite.client._api_client import APIClient
 from cognite.client.data_classes import Datapoints, DatapointsList, TimeSeries
+from cognite.client.utils._auxiliary import is_unlimited
 from cognite.client.utils._concurrency import execute_tasks
 from cognite.client.utils._importing import local_import
 from cognite.client.utils._time import timestamp_to_ms
-from cognite.client.utils.useful_types import SequenceNotStr
 
 if TYPE_CHECKING:
     import sympy
@@ -27,11 +29,11 @@ class SyntheticDatapointsAPI(APIClient):
 
     def query(
         self,
-        expressions: str | sympy.Expr | SequenceNotStr[str | sympy.Expr],
+        expressions: str | sympy.Basic | Sequence[str | sympy.Basic],
         start: int | str | datetime,
         end: int | str | datetime,
         limit: int | None = None,
-        variables: dict[str, str | TimeSeries] | None = None,
+        variables: dict[str | sympy.Symbol, str | TimeSeries] | None = None,
         aggregate: str | None = None,
         granularity: str | None = None,
         target_unit: str | None = None,
@@ -40,11 +42,11 @@ class SyntheticDatapointsAPI(APIClient):
         """`Calculate the result of a function on time series. <https://developer.cognite.com/api#tag/Synthetic-Time-Series/operation/querySyntheticTimeseries>`_
 
         Args:
-            expressions (str | sympy.Expr | SequenceNotStr[str | sympy.Expr]): Functions to be calculated. Supports both strings and sympy expressions. Strings can have either the API `ts{}` syntax, or contain variable names to be replaced using the `variables` parameter.
+            expressions (str | sympy.Basic | Sequence[str | sympy.Basic]): Functions to be calculated. Supports both strings and sympy expressions. Strings can have either the API `ts{}` syntax, or contain variable names to be replaced using the `variables` parameter.
             start (int | str | datetime): Inclusive start.
             end (int | str | datetime): Exclusive end
             limit (int | None): Number of datapoints per expression to retrieve.
-            variables (dict[str, str | TimeSeries] | None): An optional map of symbol replacements.
+            variables (dict[str | sympy.Symbol, str | TimeSeries] | None): An optional map of symbol replacements.
             aggregate (str | None): use this aggregate when replacing entries from `variables`, does not affect time series given in the `ts{}` syntax.
             granularity (str | None): use this granularity with the aggregate.
             target_unit (str | None): use this target_unit when replacing entries from `variables`, does not affect time series given in the `ts{}` syntax.
@@ -55,46 +57,55 @@ class SyntheticDatapointsAPI(APIClient):
 
         Examples:
 
-            Request a synthetic time series query with direct syntax
+            Request a synthetic time series query with direct syntax:
 
                 >>> from cognite.client import CogniteClient
                 >>> client = CogniteClient()
-                >>> dps = client.time_series.data.synthetic.query(expressions="TS{id:123} + TS{externalId:'abc'}", start="2w-ago", end="now")
+                >>> dps = client.time_series.data.synthetic.query(
+                ...     expressions="TS{id:123} + TS{externalId:'abc'}",
+                ...     start="2w-ago",
+                ...     end="now")
 
             Use variables to re-use an expression:
 
-                >>> vars = {"A": "my_ts_external_id", "B": client.time_series.retrieve(id=1)}
-                >>> dps = client.time_series.data.synthetic.query(expressions="A+B", start="2w-ago", end="now", variables=vars)
+                >>> ts = client.time_series.retrieve(id=123)
+                >>> variables = {"A": ts, "B": "my_ts_external_id"}
+                >>> dps = client.time_series.data.synthetic.query(
+                ...     expressions="A+B", start="2w-ago", end="now", variables=variables)
 
             Use sympy to build complex expressions:
 
                 >>> from sympy import symbols, cos, sin
-                >>> a = symbols('a')
-                >>> dps = client.time_series.data.synthetic.query([sin(a), cos(a)], start="2w-ago", end="now", variables={"a": "my_ts_external_id"}, aggregate='interpolation', granularity='1m', target_unit='temperature:deg_c')
+                >>> x, y = symbols("x y")
+                >>> dps = client.time_series.data.synthetic.query(
+                ...     [sin(x), y*cos(x)],
+                ...     start="2w-ago",
+                ...     end="now",
+                ...     variables={x: "foo", y: "bar"},
+                ...     aggregate="interpolation",
+                ...     granularity="15m",
+                ...     target_unit="temperature:deg_c")
         """
-        if limit is None or limit == -1:
+        if is_unlimited(limit):
             limit = cast(int, float("inf"))
 
-        tasks = []
-        expressions_to_iterate = expressions if isinstance(expressions, SequenceNotStr) else [expressions]
+        if single_expr := isinstance(expressions, (str, sympy.Basic)):
+            expressions = [expressions]
 
-        for exp in expressions_to_iterate:
+        tasks = []
+        for user_expr in expressions:
             expression, short_expression = self._build_expression(
-                exp, variables, aggregate, granularity, target_unit, target_unit_system
+                user_expr, variables, aggregate, granularity, target_unit, target_unit_system
             )
             query = {"expression": expression, "start": timestamp_to_ms(start), "end": timestamp_to_ms(end)}
-            values: list[float] = []  # mypy
-            query_datapoints = Datapoints(value=values, error=[])
-            query_datapoints.external_id = short_expression
-
+            query_datapoints = Datapoints(external_id=short_expression, value=[], error=[])
             tasks.append((query, query_datapoints, limit))
 
         datapoints_summary = execute_tasks(self._fetch_datapoints, tasks, max_workers=self._config.max_workers)
         datapoints_summary.raise_compound_exception_if_failed_tasks()
-
         return (
             DatapointsList(datapoints_summary.results, cognite_client=self._cognite_client)
-            if isinstance(expressions, list)
+            if not single_expr
             else datapoints_summary.results[0]
         )
 
@@ -110,79 +121,98 @@ class SyntheticDatapointsAPI(APIClient):
             query["start"] = data["datapoints"][-1]["timestamp"] + 1
         return datapoints
 
-    @staticmethod
     def _build_expression(
-        expression: str | sympy.Expr,
-        variables: dict[str, Any] | None = None,
+        self,
+        expression: str | sympy.Basic,
+        variables: dict[str | sympy.Symbol, str | TimeSeries] | None = None,
         aggregate: str | None = None,
         granularity: str | None = None,
         target_unit: str | None = None,
         target_unit_system: str | None = None,
     ) -> tuple[str, str]:
-        if expression.__class__.__module__.startswith("sympy."):
-            expression_str = SyntheticDatapointsAPI._sympy_to_sts(expression)
-            if not variables:
+        sympy = local_import("sympy")
+        if isinstance(expression, sympy.Basic):
+            if variables:
+                expression_str = self._process_sympy_expression(expression)
+            else:
                 raise ValueError(
                     "sympy expressions are only supported in combination with the `variables` parameter to map symbols to time series."
                 )
+        elif isinstance(expression, str):
+            expression_str = expression
         else:
-            expression_str = cast(str, expression)
+            raise TypeError(f"expression must be str or a sympy expressioin, not {type(expression)}")
+
         if aggregate and granularity:
             aggregate_str = f",aggregate:'{aggregate}',granularity:'{granularity}'"
-        else:
+        elif not aggregate and not granularity:
             aggregate_str = ""
+        else:
+            raise ValueError("Pass either both of 'aggregate' and 'granularity', or neither")
+
+        target_unit_str = ""
         if target_unit:
             if target_unit_system:
-                raise ValueError("Only one of targetUnit and targetUnitSystem can be specified.")
+                raise ValueError("Only one of 'target_unit' and 'target_unit_system' can be specified.")
             target_unit_str = f",targetUnit:'{target_unit}'"
         elif target_unit_system:
             target_unit_str = f",targetUnitSystem:'{target_unit_system}'"
-        else:
-            target_unit_str = ""
-        expression_with_ts: str = expression_str
-        if variables:
-            for k, v in variables.items():
-                if isinstance(v, TimeSeries):
-                    v = v.external_id
-                expression_with_ts = re.sub(
-                    re.compile(rf"\b{k}\b"),
-                    f"ts{{externalId:'{v}'{aggregate_str}{target_unit_str}}}",
-                    expression_with_ts,
-                )
+
+        if not variables:
+            return expression_str, expression_str
+
+        to_substitute = {}
+        for k, v in variables.items():
+            if isinstance(k, sympy.Symbol):
+                k = k.name
+            if isinstance(v, TimeSeries):
+                if v.external_id is None:
+                    raise ValueError(f"TimeSeries passed in 'variables' is missing required field 'external_id' ({v})")
+                v = v.external_id
+            to_substitute[re.escape(cast(str, k))] = f"ts{{externalId:'{v}'{aggregate_str}{target_unit_str}}}"
+
+        # Substitute all variables in one go to avoid substitution of prior substitutions:
+        pattern = re.compile(r"\b" + r"\b|\b".join(to_substitute) + r"\b")  # note: \b marks a word boundary
+        expression_with_ts = pattern.sub(lambda match: to_substitute[match[0]], expression_str)
         return expression_with_ts, expression_str
 
-    @staticmethod
-    def _sympy_to_sts(expression: str | sympy.Expr) -> str:
-        sympy_module = local_import("sympy")
+    @cached_property
+    def _supported_sympy_infix_ops(self) -> MappingProxyType[type[sympy.Basic], str]:
+        sympy = local_import("sympy")
+        return MappingProxyType({sympy.Add: "+", sympy.Mul: "*"})
 
-        infix_ops = {sympy_module.Add: "+", sympy_module.Mul: "*"}
-        functions = {
-            sympy_module.cos: "cos",
-            sympy_module.sin: "sin",
-            sympy_module.sqrt: "sqrt",
-            sympy_module.log: "ln",
-            sympy_module.exp: "exp",
-            sympy_module.Abs: "abs",
-        }
+    @cached_property
+    def _supported_sympy_functions(self) -> MappingProxyType[type[sympy.Basic], str]:
+        sympy = local_import("sympy")
+        return MappingProxyType(
+            {
+                sympy.cos: "cos",
+                sympy.sin: "sin",
+                sympy.sqrt: "sqrt",
+                sympy.log: "ln",
+                sympy.exp: "exp",
+                sympy.Abs: "abs",
+            }
+        )
 
-        def process_symbol(sym: Any) -> str:
-            if isinstance(sym, sympy_module.AtomicExpr):
-                if isinstance(sym, sympy_module.NumberSymbol):
-                    return str(sym.evalf(15))
-                else:
-                    return str(sym)
+    def _process_sympy_expression(self, expression: sympy.Basic) -> str:
+        sympy = local_import("sympy")
 
-            infixop = infix_ops.get(sym.__class__)
-            if infixop:
-                return "(" + infixop.join(process_symbol(s) for s in sym.args) + ")"
+        if isinstance(expression, sympy.AtomicExpr):
+            if isinstance(expression, sympy.NumberSymbol):
+                return str(expression.evalf(15)).rstrip("0")
+            else:
+                return str(expression).rstrip("0")
 
-            if isinstance(sym, sympy_module.Pow):
-                if sym.args[1] == -1:
-                    return f"(1/{process_symbol(sym.args[0])})"
-                return f"pow({','.join(map(process_symbol, sym.args))})"
+        expr_cls = type(expression)
+        if infix_op := self._supported_sympy_infix_ops.get(expr_cls):
+            return "(" + infix_op.join(self._process_sympy_expression(s) for s in expression.args) + ")"
 
-            if funop := functions.get(sym.__class__):
-                return f"{funop}({','.join(map(process_symbol, sym.args))})"
-            raise ValueError(f"Unsupported sympy class {sym.__class__} encountered in expression")
+        if isinstance(expression, sympy.Pow):
+            if expression.args[1] == -1:
+                return f"(1/{self._process_sympy_expression(expression.args[0])})"
+            return f"pow({','.join(map(self._process_sympy_expression, expression.args))})"
 
-        return process_symbol(expression)
+        if fn_op := self._supported_sympy_functions.get(expr_cls):
+            return f"{fn_op}({','.join(map(self._process_sympy_expression, expression.args))})"
+        raise TypeError(f"Unsupported sympy class {expr_cls} encountered in expression")
