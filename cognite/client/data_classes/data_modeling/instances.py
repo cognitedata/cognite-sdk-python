598e145bef0aa18004f41b480b19a29c465bd05e
anders-albert <anders0albert@gmail.com>
2024-03-24 10:53:38 +0100

    refactor: added data classes for Typing information

--- cognite/client/data_classes/data_modeling/instances.py
+++ cognite/client/data_classes/data_modeling/instances.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import threading
 import warnings
 from abc import ABC, abstractmethod
-from collections import defaultdict
+from collections import UserDict, defaultdict
 from dataclasses import dataclass
 from datetime import datetime
 from typing import (
@@ -40,6 +40,7 @@ from cognite.client.data_classes.data_modeling.core import (
 )
 from cognite.client.data_classes.data_modeling.data_types import (
     DirectRelationReference,
+    PropertyType,
     UnitReference,
     UnitSystemReference,
 )
@@ -1060,3 +1061,68 @@ class TargetUnit(CogniteObject):
             if "externalId" in resource["unit"]
             else UnitSystemReference.load(resource["unit"]),
         )
+
+
+@dataclass
+class TypePropertyDefinition(CogniteObject):
+    type: PropertyType
+    nullable: bool
+    auto_increment: bool
+    default_value: str | int | dict | None = None
+    name: str | None = None
+    description: str | None = None
+
+    def dump(self, camel_case: bool = True) -> dict[str, Any]:
+        return {
+            "type": self.type.dump(camel_case),
+            "nullable": self.nullable,
+            "autoIncrement": self.auto_increment,
+            "defaultValue": self.default_value,
+            "name": self.name,
+            "description": self.description,
+        }
+
+    @classmethod
+    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> TypePropertyDefinition:
+        return cls(
+            type=PropertyType.load(resource["type"]),
+            nullable=resource["nullable"],
+            auto_increment=resource["autoIncrement"],
+            default_value=resource.get("defaultValue"),
+            name=resource.get("name"),
+            description=resource.get("description"),
+        )
+
+
+class TypeInformation(UserDict, CogniteObject):
+    def __init__(self, data: dict[str, dict[str, dict[str, TypePropertyDefinition]]] | None = None) -> None:
+        super().__init__(data or {})
+
+    def dump(self, camel_case: bool = True) -> dict[str, Any]:
+        output: dict[str, Any] = {}
+        for space_name, space_data in self.data.items():
+            output.setdefault(space_name, {})
+            for view_or_container_id, view_data in space_data.items():
+                output[space_name].setdefault(view_or_container_id, {})
+                for type_name, type_data in view_data.items():
+                    output[space_name][view_or_container_id][type_name] = type_data.dump(camel_case)
+        return output
+
+    @classmethod
+    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> TypeInformation:
+        return cls(
+            {
+                space_name: {
+                    view_or_container_id: {
+                        type_name: TypePropertyDefinition.load(type_data) for type_name, type_data in view_data.items()
+                    }
+                    for view_or_container_id, view_data in space_data.items()
+                }
+                for space_name, space_data in resource.items()
+            }
+        )
+
+    def to_pandas(self) -> pd.DataFrame: ...
+
+    def _repr_html_(self) -> str:
+        return self.to_pandas()._repr_html_()
