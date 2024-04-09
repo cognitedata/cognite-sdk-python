9b830ae43550c8a6776c1af4088de3f5f3ecab02
Håkon V. Treider <haakonvt@gmail.com>
2024-04-09 12:56:29 +0200

    make threadpool executor return ordered results

--- tests/tests_unit/test_utils/test_concurrency.py
+++ tests/tests_unit/test_utils/test_concurrency.py
@@ -1,10 +1,10 @@
+import random
 import time
 from concurrent.futures import ThreadPoolExecutor
 
 import pytest
 
 from cognite.client.exceptions import CogniteAPIError
-from cognite.client.utils._auxiliary import no_op
 from cognite.client.utils._concurrency import (
     ConcurrencySettings,
     MainThreadExecutor,
@@ -37,15 +37,44 @@ class TestExecutor:
 
     @pytest.mark.parametrize(
         "executor",
-        (ConcurrencySettings.get_mainthread_executor(), ConcurrencySettings.get_thread_pool_executor(2)),
+        (ConcurrencySettings.get_mainthread_executor(), ConcurrencySettings.get_thread_pool_executor(10)),
     )
-    def test_executors(self, executor) -> None:
+    def test_executors__results_ordering_match_tasks(self, executor) -> None:
         assert ConcurrencySettings.executor_type == "threadpool"
 
-        task_summary = execute_tasks(no_op, [(i,) for i in range(10)], 10, executor=executor)
+        # We use a task that yields thread control and use N tasks >> max_workers to really test ordering:
+        task_summary = execute_tasks(
+            lambda i: time.sleep(random.random() / 50) or i,
+            tasks=[(i,) for i in range(50)],
+            max_workers=10,
+            executor=executor,
+        )
         task_summary.raise_compound_exception_if_failed_tasks()
 
-        assert sorted(task_summary.results) == list(range(10))
+        assert task_summary.results == list(range(50))
+
+    def test_executors__results_ordering_match_tasks_even_with_failures(self) -> None:
+        executor = ConcurrencySettings.get_thread_pool_executor(10)
+
+        def test_fn(i: int) -> int:
+            time.sleep(random.random() / 50)
+            if i in range(20, 23):
+                raise ValueError
+            return i
+
+        task_summary = execute_tasks(
+            test_fn,
+            tasks=[(i,) for i in range(50)],
+            max_workers=10,
+            executor=executor,
+        )
+        exp_res = [*range(20), *range(23, 50)]
+        assert task_summary.results == exp_res
+        assert task_summary.successful_tasks == [(res,) for res in exp_res]
+        assert set(task_summary.failed_tasks) == set([(20,), (22,), (21,)])
+
+        with pytest.raises(ValueError):
+            task_summary.raise_compound_exception_if_failed_tasks()
 
     @pytest.mark.parametrize("fail_fast", (False, True))
     def test_fail_fast__execute_tasks_serially(self, fail_fast):
