dc890d20236f82506f166e4c223132b059c23717
Johan Løwig Rambech Dahl <40041033+CogniteJohan@users.noreply.github.com>
2024-04-24 11:55:28 +0200

    Xid_for_function_schedule_creation (#1726)
    Co-authored-by: Håkon V. Treider <haakonvt@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,11 @@ Changes are grouped as follows
 - `Fixed` for any bug fixes.
 - `Security` in case of vulnerabilities.
 
+## [7.38.2] - 2024-04-23
+### Added
+- Added new parameter `function_external_id` to `FunctionScheduleAPI.create` as a convenience to the user. Note
+  that schedules must be attached to a Function by (internal) ID, so a lookup is first done on behalf of the user.
+
 ## [7.38.1] - 2024-04-23
 ### Added
 - Added missing `partitions` parameter to `list()` and `__call__()` methods for `FilesAPI`.
--- cognite/client/_api/functions.py
+++ cognite/client/_api/functions.py
@@ -985,7 +985,8 @@ class FunctionSchedulesAPI(APIClient):
         self,
         name: str,
         cron_expression: str,
-        function_id: int,
+        function_id: int | None = None,
+        function_external_id: str | None = None,
         client_credentials: dict | ClientCredentials | None = None,
         description: str = "",
         data: dict | None = None,
@@ -995,7 +996,8 @@ class FunctionSchedulesAPI(APIClient):
         Args:
             name (str): Name of the schedule.
             cron_expression (str): Cron expression.
-            function_id (int): Id of the function to attach the schedule to.
+            function_id (int | None): Id of the function to attach the schedule to.
+            function_external_id (str | None): External id of the function to attach the schedule to. Will be converted to (internal) ID before creating the schedule.
             client_credentials (dict | ClientCredentials | None): Instance of ClientCredentials or a dictionary containing client credentials: 'client_id' and 'client_secret'.
             description (str): Description of the schedule.
             data (dict | None): Data to be passed to the scheduled run.
@@ -1006,6 +1008,10 @@ class FunctionSchedulesAPI(APIClient):
         Warning:
             Do not pass secrets or other confidential information via the ``data`` argument. There is a dedicated
             ``secrets`` argument in FunctionsAPI.create() for this purpose.
+
+            Passing the reference to the Function by ``function_external_id`` is just here as a convenience to the user.
+            The API require that all schedules *must* be attached to a Function by (internal) ID for authentication-
+            and security purposes. This means that the lookup to get the ID is first done on behalf of the user.
 
         Examples:
 
@@ -1037,6 +1043,8 @@ class FunctionSchedulesAPI(APIClient):
                 ... )
 
         """
+        identifier = _get_function_identifier(function_id, function_external_id)
+        function_id = _get_function_internal_id(self._cognite_client, identifier)
         nonce = create_session_and_return_nonce(
             self._cognite_client, api_name="Functions API", client_credentials=client_credentials
         )
