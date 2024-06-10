fbae46884b66c49b9925bf30d1c0955118c3b5c9
Anders Hafreager <andershaf@gmail.com>
2024-06-10 10:47:06 +0200

    Added CI for pyodide

---
+++ scripts/test-pyodide.js
@@ -0,0 +1,52 @@
+const { loadPyodide } = require("pyodide");
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+// The Cognite Python SDK wheel filename will be sent in as environment variable
+const wheelFilePath = path.join(__dirname, process.env.SDK_FILE_PATH);
+
+// Create an HTTP server to serve the wheel file
+const server = http.createServer((req, res) => {
+  fs.readFile(wheelFilePath, (err, data) => {
+    if (err) {
+      // Handle file read errors
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 - Internal Server Error');
+    } else {
+      // Serve the file content
+      res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+      res.end(data);
+    }
+  });
+});
+
+// Start the server and listen on the defined port. Then try to install the SDK in Python
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}. Now trying to install sdk.`);
+  
+  async function test_cognite_sdk() {
+    let pyodide = await loadPyodide();
+    await pyodide.loadPackage("micropip");
+    const micropip = pyodide.pyimport("micropip");
+    // Read packages to install from environment variable as JSON
+
+    const packages = JSON.parse(process.env.PACKAGES);
+    for (const pkg of packages) {
+      await micropip.install(pkg);
+    }
+    // await micropip.install("pyodide-http");
+    // await micropip.install('http://localhost:3000/cognite_sdk-7.49.0-py3-none-any.whl');
+    await pyodide.runPythonAsync("from cognite.client import CogniteClient");
+    
+    return pyodide.runPythonAsync("1+1");
+  }
+
+  test_cognite_sdk().then((result) => {
+    console.log("Python says that 1+1 =", result);
+    server.close();
+  });
+});
