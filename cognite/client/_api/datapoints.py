716fe5293dc5e92a02e0020e2f3b2d888e02e49a
Håkon V. Treider <haakonvt@gmail.com>
2024-06-14 11:41:04 +0200

    deprecate retrieve_dataframe_in_tz

--- cognite/client/_api/datapoints.py
+++ cognite/client/_api/datapoints.py
@@ -6,6 +6,7 @@ import heapq
 import itertools
 import math
 import time
+import warnings
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from itertools import chain
@@ -1083,6 +1084,14 @@ class DatapointsAPI(APIClient):
     ) -> pd.DataFrame:
         """Get datapoints directly in a pandas dataframe in the same timezone as ``start`` and ``end``.
 
+        .. admonition:: Deprecation Warning
+
+            This SDK function is deprecated and will be removed in the next major release. Reason: Cognite Data
+            Fusion now has native support for timezone and calendar-based aggregations. Please consider migrating
+            already today: The API also supports fixed offsets, yields more accurate results and have better support
+            for exotic timezones and unusual DST offsets. You can use the normal retrieve methods instead, just
+            pass 'timezone' as a parameter.
+
         This is a convenience method extending the Time Series API capabilities to make timezone-aware datapoints
         fetching easy with daylight saving time (DST) transitions taken care of automatically. It builds on top
         of the methods ``retrieve_arrays`` and ``retrieve_dataframe``.
@@ -1163,6 +1172,15 @@ class DatapointsAPI(APIClient):
                 ...     start=datetime(2020, 1, 1, tzinfo=ZoneInfo("America/New_York")),
                 ...     end=datetime(2022, 12, 31, tzinfo=ZoneInfo("America/New_York")))
         """
+        warnings.warn(
+            (
+                "This SDK function is deprecated and will be removed in the next major release. Reason: Cognite Data "
+                "Fusion now has native support for timezone and calendar-based aggregations. Please consider "
+                "migrating already today: The API yields more accurate results and have better support for exotic "
+                "timezones. You can use the normal retrieve methods instead, just pass 'timezone' as a parameter."
+            ),
+            UserWarning,
+        )
         _, pd = local_import("numpy", "pandas")  # Verify that deps are available or raise CogniteImportError
 
         if not exactly_one_is_not_none(id, external_id):
@@ -1173,7 +1191,6 @@ class DatapointsAPI(APIClient):
                 "Got only one of 'aggregates' and 'granularity'. "
                 "Pass both to get aggregates, or neither to get raw data"
             )
-
         tz = validate_timezone(start, end)
         if aggregates is None and granularity is None:
             # For raw data, we only need to convert the timezone:
