ef9680f4aa123d9cb0ba6c36ee331a3f273c5d6f
anders-albert <anders0albert@gmail.com>
2024-06-15 21:30:41 +0200

    refactor: updated cdm init

--- cognite/client/data_classes/cdm/__init__.py
+++ cognite/client/data_classes/cdm/__init__.py
@@ -1,8 +1,30 @@
 from cognite.client.config import global_config
 
 if global_config.cdm_model_version == "v1":
-    from cognite.client.data_classes.cdm.v1 import Sourceable
+    from cognite.client.data_classes.cdm.v1 import (
+        Activity,
+        Asset,
+        AssetType,
+        Connection3D,
+        Describable,
+        Equipment,
+        Object3D,
+        Schedulable,
+        Sourceable,
+        TimeSeriesBase,
+    )
 
-    __all__ = ["Sourceable"]
+    __all__ = [
+        "Sourceable",
+        "Describable",
+        "Schedulable",
+        "Connection3D",
+        "Object3D",
+        "Asset",
+        "AssetType",
+        "Activity",
+        "Equipment",
+        "TimeSeriesBase",
+    ]
 else:
     raise ImportError(f"Unknown CDM model version: {global_config.cdm_model_version}")
