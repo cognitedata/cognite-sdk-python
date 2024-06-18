6626e78bccff212e279e1c5629c6a24e90830809
anders-albert <anders0albert@gmail.com>
2024-06-18 17:01:29 +0200

    refactor; Writable Typed node and edges

---
+++ cognite/client/data_classes/data_modeling/typed_instances.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from cognite.client.data_classes.data_modeling.data_types import (
+    DirectRelationReference,
+)
+
+
+class TypedNodeWrite:
+    def __init__(
+        self,
+        space: str,
+        external_id: str,
+        existing_version: int | None = None,
+        type: DirectRelationReference | tuple[str, str] | None = None,
+    ) -> None:
+        self.space = space
+        self.external_id = external_id
+        self.existing_version = existing_version
+        self.type = DirectRelationReference.load(type) if type else None
+
+
+class TypedEdgeWrite:
+    def __init__(
+        self,
+        space: str,
+        external_id: str,
+        type: DirectRelationReference | tuple[str, str],
+        start_node: DirectRelationReference | tuple[str, str],
+        end_node: DirectRelationReference | tuple[str, str],
+        existing_version: int | None = None,
+    ) -> None:
+        self.space = space
+        self.external_id = external_id
+        self.type = DirectRelationReference.load(type)
+        self.start_node = DirectRelationReference.load(start_node)
+        self.end_node = DirectRelationReference.load(end_node)
+        self.existing_version = existing_version
