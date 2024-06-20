034f8f5f0547798e4051d760bd2c25660de8f43c
anders-albert <anders0albert@gmail.com>
2024-06-20 19:57:28 +0200

    refactor: added warning

--- cognite/client/data_classes/cdm/__init__.py
+++ cognite/client/data_classes/cdm/__init__.py
@@ -1,4 +1,7 @@
 from cognite.client.config import global_config
+from cognite.client.utils._experimental import FeaturePreviewWarning
+
+FeaturePreviewWarning("alpha", "alpha", "Core Data Model").warn()
 
 if global_config.cdm_model_version == "v1":
     from cognite.client.data_classes.cdm.v1 import (
