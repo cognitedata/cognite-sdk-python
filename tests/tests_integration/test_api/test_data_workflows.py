2aa4995bfab646057ea1c454d858e0e6d354c80b
Bert Verstraete <bertverstraete22@gmail.com>
2024-07-04 11:30:26 +0200

    [CDF-21996] Make sdk data workflows test more robust (#1835)
    Initially, only a single execution was created for the test suite, which always was successful before the fixture dependant tests ran. Now, however, there's multiple executions and some are not terminal yet. To fix the test, we just select the first completed (i.e., now immutable) execution.

--- tests/tests_integration/test_api/test_data_workflows.py
+++ tests/tests_integration/test_api/test_data_workflows.py
@@ -392,10 +392,12 @@ class TestWorkflowExecutions:
         cognite_client: CogniteClient,
         workflow_execution_list: WorkflowExecutionList,
     ) -> None:
-        assert workflow_execution_list, "There should be at least one workflow execution to test retrieve detailed with"
-        retrieved = cognite_client.workflows.executions.retrieve_detailed(workflow_execution_list[0].id)
-
-        assert retrieved.as_execution().dump() == workflow_execution_list[0].dump()
+        workflow_execution_completed = cognite_client.workflows.executions.list(statuses="completed", limit=1)
+        assert (
+            workflow_execution_completed
+        ), "There should be at least one workflow execution to test retrieve detailed with"
+        retrieved = cognite_client.workflows.executions.retrieve_detailed(workflow_execution_completed[0].id)
+        assert retrieved.as_execution().dump() == workflow_execution_completed[0].dump()
         assert retrieved.executed_tasks
 
     def test_retrieve_non_existing_workflow_execution(self, cognite_client: CogniteClient) -> None:
