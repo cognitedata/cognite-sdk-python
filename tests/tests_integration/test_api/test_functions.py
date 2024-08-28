c3b41c732e1a70ed6078df680477a7f42abf3602
anders-albert <anders0albert@gmail.com>
2024-08-28 10:32:10 +0200

    tests: updated tests

--- tests/tests_integration/test_api/test_functions.py
+++ tests/tests_integration/test_api/test_functions.py
@@ -77,7 +77,7 @@ class TestFunctionsAPI:
             assert False, "Expected at least one function"
 
     def test_call_function_with_oneshot(self, cognite_client: CogniteClient, a_function: Function) -> None:
-        session = cognite_client.iam.sessions.create(one_shot_token_exchange=True)
+        session = cognite_client.iam.sessions.create(session_type="ONESHOT_TOKEN_EXCHANGE")
 
         response = cognite_client.functions.call(id=a_function.id, wait=True, nonce=session.nonce)
 
