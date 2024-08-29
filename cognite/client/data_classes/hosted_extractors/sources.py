c663cd262f87833b006c9ed6e4e1b796eda6220d
anders-albert <anders0albert@gmail.com>
2024-08-29 09:11:27 +0200

    refactor: fix Source classes

--- cognite/client/data_classes/hosted_extractors/sources.py
+++ cognite/client/data_classes/hosted_extractors/sources.py
@@ -1,44 +1,27 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import TYPE_CHECKING, Any, Literal
+from typing import TYPE_CHECKING, ClassVar, Literal
 
-from typing_extensions import Self, TypeAlias
+from typing_extensions import TypeAlias
 
 from cognite.client.data_classes._base import (
+    CogniteResource,
     CogniteResourceList,
     ExternalIDTransformerMixin,
+    T_WriteClass,
     WriteableCogniteResource,
     WriteableCogniteResourceList,
 )
-from cognite.client.utils.useful_types import SequenceNotStr
 
 if TYPE_CHECKING:
-    from cognite.client import CogniteClient
+    pass
 
 
 SourceType: TypeAlias = Literal["mqtt5", "mqtt3", "eventhub"]
 
 
-class _SourceCore(WriteableCogniteResource["SourceWrite"], ABC):
-    def __init__(
-        self,
-        type: SourceType,
-        external_id: str,
-        host: str,
-        event_hub_name: str,
-        key_name: str,
-        consumer_group: str | None = None,
-    ) -> None:
-        self.type = type
-        self.external_id = external_id
-        self.host = host
-        self.event_hub_name = event_hub_name
-        self.key_name = key_name
-        self.consumer_group = consumer_group
-
-
-class SourceWrite(_SourceCore):
+class SourceWrite(CogniteResource, ABC):
     """A hosted extractor source represents an external source system on the internet.
     The source resource in CDF contains all the information the extractor needs to
     connect to the external source system.
@@ -46,7 +29,40 @@ class SourceWrite(_SourceCore):
     This is the write/request format of the source resource.
 
     Args:
-        type (SourceType): Source type.
+        external_id (str): The external ID provided by the client. Must be unique for the resource type.
+    """
+
+    _type: ClassVar[str]
+
+    def __init__(self, external_id: str) -> None:
+        self.external_id = external_id
+
+
+class Source(WriteableCogniteResource[T_WriteClass], ABC):
+    """A hosted extractor source represents an external source system on the internet.
+    The source resource in CDF contains all the information the extractor needs to
+    connect to the external source system.
+
+    This is the read/response format of the source resource.
+
+    Args:
+        external_id (str): The external ID provided by the client. Must be unique for the resource type.
+    """
+
+    _type: ClassVar[str]
+
+    def __init__(self, external_id: str) -> None:
+        self.external_id = external_id
+
+
+class EventHubSourceWrite(SourceWrite):
+    """A hosted extractor source represents an external source system on the internet.
+    The source resource in CDF contains all the information the extractor needs to
+    connect to the external source system.
+
+    This is the write/request format of the source resource.
+
+    Args:
         external_id (str): The external ID provided by the client. Must be unique for the resource type.
         host (str): URL of the event hub consumer endpoint.
         event_hub_name (str): Name of the event hub
@@ -57,7 +73,6 @@ class SourceWrite(_SourceCore):
 
     def __init__(
         self,
-        type: SourceType,
         external_id: str,
         host: str,
         event_hub_name: str,
@@ -65,26 +80,18 @@ class SourceWrite(_SourceCore):
         key_value: str,
         consumer_group: str | None = None,
     ) -> None:
-        super().__init__(type, external_id, host, event_hub_name, key_name, consumer_group)
+        super().__init__(external_id)
+        self.host = host
+        self.event_hub_name = event_hub_name
+        self.key_name = key_name
         self.key_value = key_value
-
-    @classmethod
-    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> SourceWrite:
-        return cls(
-            type=resource["type"],
-            external_id=resource["externalId"],
-            host=resource["host"],
-            event_hub_name=resource["eventHubName"],
-            key_name=resource["keyName"],
-            key_value=resource["keyValue"],
-            consumer_group=resource.get("consumerGroup"),
-        )
+        self.consumer_group = consumer_group
 
     def as_write(self) -> SourceWrite:
         return self
 
 
-class Source(_SourceCore):
+class EventHubSource(Source):
     """A hosted extractor source represents an external source system on the internet.
     The source resource in CDF contains all the information the extractor needs to
     connect to the external source system.
@@ -92,7 +99,6 @@ class Source(_SourceCore):
     This is the read/response format of the source resource.
 
     Args:
-        type (SourceType): Source type.
         external_id (str): The external ID provided by the client. Must be unique for the resource type.
         host (str): URL of the event hub consumer endpoint.
         event_hub_name (str): Name of the event hub
@@ -104,7 +110,6 @@ class Source(_SourceCore):
 
     def __init__(
         self,
-        type: SourceType,
         external_id: str,
         host: str,
         event_hub_name: str,
@@ -113,28 +118,18 @@ class Source(_SourceCore):
         last_updated_time: int,
         consumer_group: str | None = None,
     ) -> None:
-        super().__init__(type, external_id, host, event_hub_name, key_name, consumer_group)
+        super().__init__(external_id)
+        self.host = host
+        self.event_hub_name = event_hub_name
+        self.key_name = key_name
+        self.consumer_group = consumer_group
         self.created_time = created_time
         self.last_updated_time = last_updated_time
 
-    @classmethod
-    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
-        return cls(
-            type=resource["type"],
-            external_id=resource["externalId"],
-            host=resource["host"],
-            event_hub_name=resource["eventHubName"],
-            key_name=resource["keyName"],
-            created_time=resource["createdTime"],
-            last_updated_time=resource["lastUpdatedTime"],
-            consumer_group=resource.get("consumerGroup"),
-        )
-
-    def as_write(self, key_value: str | None = None) -> SourceWrite:
+    def as_write(self, key_value: str | None = None) -> EventHubSourceWrite:
         if key_value is None:
             raise ValueError("key_value must be provided")
-        return SourceWrite(
-            type=self.type,
+        return EventHubSourceWrite(
             external_id=self.external_id,
             host=self.host,
             event_hub_name=self.event_hub_name,
@@ -151,9 +146,7 @@ class SourceWriteList(CogniteResourceList[SourceWrite], ExternalIDTransformerMix
 class SourceList(WriteableCogniteResourceList[SourceWrite, Source], ExternalIDTransformerMixin):
     _RESOURCE = Source
 
-    def as_write(self, key_values: SequenceNotStr[str] | None = None) -> SourceWriteList:
-        if key_values is None:
-            raise ValueError("key_values must be provided")
-        if len(self) != len(key_values):
-            raise ValueError("key_values must be the same length as the sources")
-        return SourceWriteList([source.as_write(key_value) for source, key_value in zip(self, key_values)])
+    def as_write(
+        self,
+    ) -> SourceWriteList:
+        raise TypeError(f"{type(self).__name__} cannot be converted to write")
