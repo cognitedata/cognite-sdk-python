1e9002fda4e1bdb0ce3eafc4128d1ae78d0a86e4
anders-albert <anders0albert@gmail.com>
2024-08-30 08:49:54 +0200

    tests: Added integration tests

---
+++ tests/tests_integration/test_api/test_hosted_extractors/test_mappings.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import pytest
+
+from cognite.client import CogniteClient
+from cognite.client.data_classes.hosted_extractors import (
+    CustomMapping,
+    Mapping,
+    MappingList,
+    MappingUpdate,
+    MappingWrite,
+)
+from cognite.client.exceptions import CogniteAPIError
+from cognite.client.utils._text import random_string
+
+
+@pytest.fixture
+def one_mapping(cognite_client: CogniteClient) -> Mapping:
+    my_mapping = MappingWrite(
+        external_id=f"myNewMapping-{random_string(10)}",
+        mapping=CustomMapping("some expression"),
+        published=False,
+        input="json",
+    )
+    retrieved = cognite_client.hosted_extractors.mappings.retrieve(my_mapping.external_id, ignore_unknown_ids=True)
+    if retrieved:
+        yield retrieved
+    created = cognite_client.hosted_extractors.mappings.create(my_mapping)
+    yield created
+
+    cognite_client.hosted_extractors.mappings.delete(created.external_id, ignore_unknown_ids=True)
+
+
+class TestMappings:
+    def test_create_update_retrieve_delete(self, cognite_client: CogniteClient) -> None:
+        my_mapping = MappingWrite(
+            external_id=f"myNewMappingForTesting-{random_string(10)}",
+            mapping=CustomMapping("select * from table"),
+            published=False,
+            input="xml",
+        )
+        created: Mapping | None = None
+        try:
+            created = cognite_client.hosted_extractors.mappings.create(my_mapping)
+            assert isinstance(created, Mapping)
+            update = MappingUpdate(external_id=my_mapping.external_id).published.set(True)
+            updated = cognite_client.hosted_extractors.mappings.update(update)
+            assert updated.published is True
+            retrieved = cognite_client.hosted_extractors.mappings.retrieve(created.external_id)
+            assert retrieved is not None
+            assert retrieved.external_id == created.external_id
+            assert retrieved.published == created.published
+
+            cognite_client.hosted_extractors.mappings.delete(created.external_id)
+
+            with pytest.raises(CogniteAPIError):
+                cognite_client.hosted_extractors.mappings.retrieve(created.external_id)
+
+            cognite_client.hosted_extractors.mappings.retrieve(created.external_id, ignore_unknown_ids=True)
+
+        finally:
+            if created:
+                cognite_client.hosted_extractors.mappings.delete(created.external_id, ignore_unknown_ids=True)
+
+    @pytest.mark.usefixtures("one_mapping")
+    def test_list(self, cognite_client: CogniteClient) -> None:
+        res = cognite_client.hosted_extractors.mappings.list(limit=1)
+        assert len(res) == 1
+        assert isinstance(res, MappingList)
+
+    def test_update_using_write_object(self, cognite_client: CogniteClient) -> None:
+        my_mapping = MappingWrite(
+            external_id=f"myNewMappingTestingForUpdate-{random_string(10)}",
+            mapping=CustomMapping("select * from table"),
+            published=False,
+            input="csv",
+        )
+        created: Mapping | None = None
+        try:
+            created = cognite_client.hosted_extractors.mappings.create(my_mapping)
+
+            update = MappingWrite(
+                external_id=my_mapping.external_id,
+                mapping=CustomMapping("select * from table"),
+                published=True,
+                input="json",
+            )
+
+            updated = cognite_client.hosted_extractors.mappings.update(update)
+            assert updated.published is True
+        finally:
+            if created:
+                cognite_client.hosted_extractors.mappings.delete(created.external_id, ignore_unknown_ids=True)
