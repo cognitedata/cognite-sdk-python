6f780f3a0aea5515392b1acacebc2a261fd78942
Andreas Dalhaug <andreas.dalhaug@cognite.com>
2024-09-23 09:14:55 +0200

    Update events.py
    changed ' to " to pass linting

--- cognite/client/_api/events.py
+++ cognite/client/_api/events.py
@@ -635,7 +635,7 @@ class EventsAPI(APIClient):
 
         Args:
             item (Event | EventWrite | Sequence[Event | EventWrite]): Event or list of events to upsert.
-            mode (Literal['patch', 'replace']): Whether to patch or replace in the case the events are existing. If you set 'patch', the call will only update fields with non-null values (default). Setting 'replace' will unset any fields that are not specified.
+            mode (Literal["patch", "replace"]): Whether to patch or replace in the case the events are existing. If you set 'patch', the call will only update fields with non-null values (default). Setting 'replace' will unset any fields that are not specified.
 
         Returns:
             Event | EventList: The upserted event(s).
