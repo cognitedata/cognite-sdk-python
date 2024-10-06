7ac873b87bd207d8744690b6ce8b66e88b0ce192
anders-albert <anders0albert@gmail.com>
2024-10-06 09:46:57 +0200

    refactor: setup plumming

---
+++ cognite/client/data_classes/postgres_gateway/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from cognite.client.data_classes.postgres_gateway.users import (
+    FdwUser,
+    FdwUserList,
+    FdwUserUpdate,
+    FdwUserWrite,
+    SessionCredentials,
+)
+
+__all__ = ["FdwUser", "FdwUserList", "FdwUserUpdate", "FdwUserWrite", "SessionCredentials"]
