79030a6135351766c32910fc0fd9ee7de687cdc1
anders-albert <anders0albert@gmail.com>
2024-10-10 13:01:29 +0200

    tests: added failing tests

---
+++ tests/tests_unit/test_data_classes/test_hosted_extractors/test_jobs.py
@@ -0,0 +1,21 @@
+from cognite.client.data_classes.hosted_extractors.jobs import Job
+
+
+class TestJob:
+    def test_load_yaml_dump_unknown_config(self) -> None:
+        raw_yaml = """externalId: myJob
+sourceId: my_eventhub
+destinationId: EventHubTarget
+targetStatus: running
+status: running
+createdTime: 123
+lastUpdatedTime: 1234
+format:
+  type: value
+  encoding: utf16
+  compression: gzip
+config:
+  some: new_config
+  that: has not been seen before"""
+
+        assert Job.load(raw_yaml).dump_yaml() == raw_yaml
