32f044a7f30f780ec7e112315958f3d2744273ea
anders-albert <anders0albert@gmail.com>
2024-10-13 10:03:30 +0200

    refactor; tables dataclasses

--- cognite/client/data_classes/postgres_gateway/tables.py
+++ cognite/client/data_classes/postgres_gateway/tables.py
@@ -1,20 +1,14 @@
-from __future__ import annotations
-
-from abc import ABC
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Any, NoReturn, Sequence, Literal
+from typing import TYPE_CHECKING, Any, Literal, TypeAlias, cast
 
 from typing_extensions import Self
 
-from cognite.client.utils.useful_types import SequenceNotStr
 from cognite.client.data_classes._base import (
     CogniteObject,
-    CognitePrimitiveUpdate,
-    CogniteResource,
     CogniteResourceList,
-    CogniteUpdate,
-    ExternalIDTransformerMixin,
-    PropertySpec,
+    UnknownCogniteObject,
     WriteableCogniteResource,
     WriteableCogniteResourceList,
 )
@@ -22,68 +16,81 @@ from cognite.client.data_classes._base import (
 if TYPE_CHECKING:
     from cognite.client import CogniteClient
 
-
-@dataclass
-class Unknown(CogniteObject):
-    type: Literal["raw_rows", "view"] | None
-    tablename: str | None
+ColumnType: TypeAlias = Literal[
+    "TEXT",
+    "VARCHAR",
+    "BOOL",
+    "BIGINT",
+    "DOUBLE PRECISION",
+    "REAL",
+    "TIMESTAMPTZ",
+    "JSON",
+    "TEXT[]",
+    "VARCHAR[]",
+    "BOOL[]",
+    "BIGINT[]",
+    "DOUBLE PRECISION[]",
+    "REAL[]",
+    "JSON[]",
+    "BYTEA",
+]
 
 
 @dataclass
 class RawTableOptions(CogniteObject):
-    database: str | None
-    table: str | None
+    database: str
+    table: str
     primary_key: str | None
 
+    @classmethod
+    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
+        return cls(
+            database=resource["database"],
+            table=resource["table"],
+            primary_key=resource.get("primaryKey"),
+        )
+
 
 @dataclass
-class FdmTableOptions(CogniteObject):
-    space: str | None
-    external_id: str | None
+class ViewTableOptions(CogniteObject):
+    space: str
+    external_id: str
     version: str | None
 
-
-@dataclass
-class CreateFdm(CogniteObject):
-    unknown: Unknown
-    unknown: Unknown | None
-    
     @classmethod
     def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
         return cls(
-            unknown=Unknown._load(resource["unknown"], cognite_client),
-            unknown=Unknown._load(resource["unknown"], cognite_client) if "unknown" in resource else None,
+            space=resource["space"],
+            external_id=resource["externalId"],
+            version=resource.get("version"),
         )
-    
-    def dump(self, camel_case: bool = True) -> dict[str, Any]:
-        output = super().dump(camel_case=camel_case)
-        output["unknown"] = self.unknown.dump(camel_case=camel_case)
-        if isinstance(self.unknown, Unknown):
-            output["unknown"] = self.unknown.dump(camel_case=camel_case)
-        
-        return output
-
 
 
 @dataclass
-class CreateRawTable(CogniteObject):
-    unknown: Unknown
-    unknown: Unknown | None
-    
+class Column(CogniteObject):
+    property_name: str
+    type: ColumnType
+
     @classmethod
     def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
         return cls(
-            unknown=Unknown._load(resource["unknown"], cognite_client),
-            unknown=Unknown._load(resource["unknown"], cognite_client) if "unknown" in resource else None,
+            property_name=resource["propertyName"],
+            type=resource["type"],
         )
-    
+
     def dump(self, camel_case: bool = True) -> dict[str, Any]:
-        output = super().dump(camel_case=camel_case)
-        output["unknown"] = self.unknown.dump(camel_case=camel_case)
-        if isinstance(self.unknown, Unknown):
-            output["unknown"] = self.unknown.dump(camel_case=camel_case)
-        
-        return output
+        return {
+            "propertyName": self.property_name,
+            "type": self.type,
+        }
+
+    @classmethod
+    def _load_columns(cls, data: dict[str, Any]) -> list[Self]:
+        return [cls._load(column) for column in data]
+
+    @classmethod
+    def _dump_columns(cls, camel_case: bool, columns: Sequence[Self]) -> dict[str, Any]:
+        raise NotImplementedError()
 
 
 class _TableCore(WriteableCogniteResource["TableWrite"], ABC):
@@ -97,59 +104,194 @@ class TableWrite(_TableCore, ABC):
     """View and create foreign **tables** for a given **user**.
 
     This is the write/request format of the table.
+    """
+
+    def as_write(self) -> Self:
+        return self
+
+    @classmethod
+    def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
+        type_ = resource.get("type")
+        if type_ is None and hasattr(cls, "_type"):
+            type_ = cls._type
+        elif type_ is None:
+            raise KeyError("type")
+        try:
+            return cast(Self, _TABLE_WRITE_CLASS_BY_TYPE[type_]._load_table(resource))
+        except KeyError:
+            raise TypeError(f"Unknown source type: {type_}")
+
+    @classmethod
+    @abstractmethod
+    def _load_table(cls, data: dict[str, Any]) -> Self:
+        raise NotImplementedError()
+
+
+class RawTableWrite(TableWrite):
+    """Foreign tables.
+
+    This is the read/response format of the raw table.
 
     Args:
+        tablename (str): Name of the foreign table.
+        options (RawTableOptions): Table options
+        columns (Sequence[Column]): Foreign table columns.
 
     """
 
-    def __init__(self, ) -> None:
-    
-    
-    def as_write(self) -> TableWrite:
-        return self
+    def __init__(self, tablename: str, options: RawTableOptions, columns: Sequence[Column]) -> None:
+        super().__init__(tablename=tablename)
+        self.options = options
+        self.columns = columns
+
+    @classmethod
+    def _load_table(cls, data: dict[str, Any]) -> Self:
+        return cls(
+            tablename=data["tablename"],
+            options=RawTableOptions._load(data["options"]),
+            columns=Column._load_columns(data["columns"]),
+        )
+
+    def dump(self, camel_case: bool = True) -> dict[str, Any]:
+        return {
+            "tablename": self.tablename,
+            "options": self.options.dump(camel_case=camel_case),
+            "columns": Column._dump_columns(camel_case, self.columns),
+        }
 
 
-class Table(_TableCore):
+class ViewTableWrite(TableWrite):
     """Foreign tables.
-
-    
 
     This is the read/response format of the custom table.
 
     Args:
-        columns (dict | None): Foreign table columns.
-        options (Unknown | None): Table options
-        type (Literal["raw_rows", "built_in", "nodes", "edges"] | None): The type of table (usually built_in) but could be any of raw_rows, nodes or edges when custom.
-        tablename (str | None): None
+        tablename (str): Name of the foreign table.
+        options (ViewTableOptions): Table options
+    """
+
+    def __init__(self, tablename: str, options: ViewTableOptions) -> None:
+        super().__init__(tablename=tablename)
+        self.options = options
+
+    @classmethod
+    def _load_table(cls, data: dict[str, Any]) -> Self:
+        return cls(
+            tablename=data["tablename"],
+            options=ViewTableOptions._load(data["options"]),
+        )
+
+    def dump(self, camel_case: bool = True) -> dict[str, Any]:
+        return {
+            "tablename": self.tablename,
+            "options": self.options.dump(camel_case=camel_case),
+        }
+
+
+class Table(_TableCore, ABC):
+    """Foreign tables.
+
+    This is the read/response format of the custom table.
 
     """
 
-    def __init__(self, columns: dict | None = None, options: Unknown | None = None, type: Literal["raw_rows", "built_in", "nodes", "edges"] | None = None, tablename: str | None = None) -> None:
-        self.columns = columns
-        self.options = options
-        self.type = type
-        self.tablename = tablename
+    def __init__(self, tablename: str, created_time: int) -> None:
+        super().__init__(tablename=tablename)
+        self.created_time = created_time
 
     @classmethod
     def _load(cls, resource: dict[str, Any], cognite_client: CogniteClient | None = None) -> Self:
+        type_ = resource.get("type")
+        if type_ is None and hasattr(cls, "_type"):
+            type_ = cls._type
+        elif type_ is None:
+            raise KeyError("type")
+        table_cls = _TABLE_CLASS_BY_TYPE.get(type_)
+        if table_cls is None:
+            return UnknownCogniteObject(resource)  # type: ignore[return-value]
+        return cast(Self, table_cls._load_table(resource))
+
+    @classmethod
+    @abstractmethod
+    def _load_table(cls, data: dict[str, Any]) -> Self:
+        raise NotImplementedError()
+
+
+class TableRaw(Table):
+    """Foreign tables.
+
+    This is the read/response format of the raw table.
+
+    Args:
+        tablename (str): Name of the foreign table.
+        options (RawTableOptions): Table options
+        columns (list[Column]): Foreign table columns.
+        created_time (int): Time when the table was created.
+
+    """
+
+    def __init__(self, tablename: str, options: RawTableOptions, columns: list[Column], created_time: int) -> None:
+        super().__init__(tablename=tablename, created_time=created_time)
+        self.options = options
+        self.columns = columns
+
+    @classmethod
+    def _load_table(cls, data: dict[str, Any]) -> Self:
         return cls(
-            columns=resource.get("<UNKNOWN>"),
-            options=Unknown._load(resource["options"], cognite_client) if "options" in resource else None,
-            type=resource.get("type"),
-            tablename=resource.get("tablename"),
-        ) 
+            tablename=data["tablename"],
+            options=RawTableOptions._load(data["options"]),
+            columns=Column._load_columns(data["columns"]),
+            created_time=data.get("created_time"),
+        )
 
     def dump(self, camel_case: bool = True) -> dict[str, Any]:
-        output = super().dump(camel_case=camel_case)
-        if isinstance(self.options, Unknown):
-            output["options"] = self.options.dump(camel_case=camel_case)
-        
-        return output
+        return {
+            "tablename": self.tablename,
+            "options": self.options.dump(camel_case=camel_case),
+            "columns": Column._dump_columns(camel_case, self.columns),
+        }
+
+    def as_write(self) -> RawTableWrite:
+        return RawTableWrite(
+            tablename=self.tablename,
+            options=self.options,
+            columns=self.columns,
+        )
 
 
-    def as_write(self) -> TableWrite:
-        return TableWrite(
+class TableView(Table):
+    """Foreign tables.
 
+    This is the read/response format of the custom table.
+
+    Args:
+        tablename (str): Name of the foreign table.
+        options (ViewTableOptions): Table options
+        created_time (int): Time when the table was created.
+    """
+
+    def __init__(self, tablename: str, options: ViewTableOptions, created_time: int) -> None:
+        super().__init__(tablename=tablename, created_time=created_time)
+        self.options = options
+
+    @classmethod
+    def _load_table(cls, data: dict[str, Any]) -> Self:
+        return cls(
+            tablename=data["tablename"],
+            options=ViewTableOptions._load(data["options"]),
+            created_time=data.get("created_time"),
+        )
+
+    def dump(self, camel_case: bool = True) -> dict[str, Any]:
+        return {
+            "tablename": self.tablename,
+            "options": self.options.dump(camel_case=camel_case),
+        }
+
+    def as_write(self) -> ViewTableWrite:
+        return ViewTableWrite(
+            tablename=self.tablename,
+            options=self.options,
         )
 
 
@@ -163,3 +305,13 @@ class TableList(WriteableCogniteResourceList[TableWrite, Table]):
     def as_write(self) -> TableWriteList:
         return TableWriteList([item.as_write() for item in self.data])
 
+
+_TABLE_WRITE_CLASS_BY_TYPE: dict[str, type[TableWrite]] = {
+    subclass._type: subclass  # type: ignore[misc, attr-defined]
+    for subclass in TableWrite.__subclasses__()
+}
+
+_TABLE_CLASS_BY_TYPE: dict[str, type[Table]] = {
+    subclass._type: subclass  # type: ignore[misc, attr-defined]
+    for subclass in Table.__subclasses__()
+}
