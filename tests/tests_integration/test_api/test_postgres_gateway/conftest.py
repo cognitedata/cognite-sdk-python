ee9a51f1394a26a5ae069276ffcd665f1a1ca2bf
anders-albert <anders0albert@gmail.com>
2024-10-13 09:31:21 +0200

    refactor; shell for testing

--- tests/tests_integration/test_api/test_postgres_gateway/conftest.py
+++ tests/tests_integration/test_api/test_postgres_gateway/conftest.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import pytest
 
 from cognite.client import CogniteClient
-from cognite.client.data_classes.postgres_gateway import SessionCredentials
+from cognite.client.data_classes.postgres_gateway import SessionCredentials, User, UserWrite
 
 
 @pytest.fixture
@@ -18,3 +18,11 @@ def another_fresh_credentials(cognite_client: CogniteClient) -> SessionCredentia
     new_session = cognite_client.iam.sessions.create(session_type="ONESHOT_TOKEN_EXCHANGE")
     yield SessionCredentials(nonce=new_session.nonce)
     cognite_client.iam.sessions.revoke(new_session.id)
+
+
+@pytest.fixture
+def one_user(cognite_client: CogniteClient, fresh_credentials: SessionCredentials) -> User:
+    my_user = UserWrite(credentials=fresh_credentials)
+    created = cognite_client.postgres_gateway.users.create(my_user)
+    yield created
+    cognite_client.postgres_gateway.users.delete(created.username, ignore_unknown_ids=True)
