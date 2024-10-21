53f8ffb0e9c1b049e552d862ff1bbeb93842b5d7
Håkon V. Treider <haakonvt@gmail.com>
2024-10-21 20:02:40 +0200

    fix: Not filter only accepts a single filter

--- cognite/client/data_classes/filters.py
+++ cognite/client/data_classes/filters.py
@@ -405,6 +405,9 @@ class Not(CompoundFilter):
 
     _filter_name = "not"
 
+    def __init__(self, filter: Filter) -> None:
+        super().__init__(filter)
+
     def _filter_body(self, camel_case_property: bool) -> dict:
         return self._filters[0].dump(camel_case_property)
 
