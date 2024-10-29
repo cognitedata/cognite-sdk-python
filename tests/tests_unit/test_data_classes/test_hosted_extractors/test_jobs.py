ee2320e7cbce4bd12984a2c4d8e7f8c0f5a8a992
Håkon V. Treider <haakonvt@gmail.com>
2024-10-29 12:21:29 +0100

    remove yaml tags for `tuple` in `dump_yaml` methods (#1992)

--- tests/tests_unit/test_data_classes/test_hosted_extractors/test_jobs.py
+++ tests/tests_unit/test_data_classes/test_hosted_extractors/test_jobs.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import yaml
 
 from cognite.client.data_classes.hosted_extractors.jobs import Job
@@ -5,19 +7,22 @@ from cognite.client.data_classes.hosted_extractors.jobs import Job
 
 class TestJob:
     def test_load_yaml_dump_unknown_config(self) -> None:
-        raw_yaml = """externalId: myJob
-sourceId: my_eventhub
-destinationId: EventHubTarget
-targetStatus: running
-status: running
-createdTime: 123
-lastUpdatedTime: 1234
-format:
-  type: value
-  encoding: utf16
-  compression: gzip
-config:
-  some: new_config
-  that: has not been seen before"""
-
+        raw_yaml = textwrap.dedent(
+            """\
+            externalId: myJob
+            sourceId: my_eventhub
+            destinationId: EventHubTarget
+            targetStatus: running
+            status: running
+            createdTime: 123
+            lastUpdatedTime: 1234
+            format:
+              type: value
+              encoding: utf16
+              compression: gzip
+            config:
+              some: new_config
+              that: has not been seen before
+            """
+        )
         assert Job.load(raw_yaml).dump() == yaml.safe_load(raw_yaml)
