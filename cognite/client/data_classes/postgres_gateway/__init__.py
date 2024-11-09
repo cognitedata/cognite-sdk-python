3dde78c91072aba43b4de9c1da5b113e2598081c
anders-albert <anders0albert@gmail.com>
2024-11-09 11:19:02 +0100

    refactor: added UserCreated class

--- cognite/client/data_classes/postgres_gateway/__init__.py
+++ cognite/client/data_classes/postgres_gateway/__init__.py
@@ -3,10 +3,21 @@ from __future__ import annotations
 from cognite.client.data_classes.postgres_gateway.users import (
     SessionCredentials,
     User,
+    UserCreated,
+    UserCreatedList,
     UserList,
     UserUpdate,
     UserWrite,
     UserWriteList,
 )
 
-__all__ = ["User", "UserList", "UserUpdate", "UserWrite", "UserWriteList", "SessionCredentials"]
+__all__ = [
+    "User",
+    "UserList",
+    "UserUpdate",
+    "UserWrite",
+    "UserWriteList",
+    "SessionCredentials",
+    "UserCreated",
+    "UserCreatedList",
+]
