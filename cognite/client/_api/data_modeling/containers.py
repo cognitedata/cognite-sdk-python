d22b7369db3ba5e94ded8898368357445c00cfd2
Håkon V. Treider <haakonvt@gmail.com>
2024-11-14 14:29:50 +0100

    docs: add container example with all type combinations

--- cognite/client/_api/data_modeling/containers.py
+++ cognite/client/_api/data_modeling/containers.py
@@ -288,13 +288,12 @@ class ContainersAPI(APIClient):
                 >>> from cognite.client.data_classes.data_modeling import ContainerApply, ContainerProperty, Text
                 >>> client = CogniteClient()
                 >>> container = [ContainerApply(space="mySpace", external_id="myContainer",
-                ...     properties={"name": ContainerProperty(type=Text(), name="name")})]
+                ...     properties={"name": ContainerProperty(type=Text, name="name")})]
                 >>> res = client.data_modeling.containers.apply(container)
 
             Create new container with unit-aware properties:
 
-                >>> from cognite.client import CogniteClient
-                >>> from cognite.client.data_classes.data_modeling import ContainerApply, ContainerProperty, Float64
+                >>> from cognite.client.data_classes.data_modeling import Float64
                 >>> from cognite.client.data_classes.data_modeling.data_types import UnitReference
                 >>> client = CogniteClient()
                 >>> container = ContainerApply(
@@ -326,6 +325,55 @@ class ContainersAPI(APIClient):
                 ...     }
                 ... )
                 >>> res = client.data_modeling.containers.apply(container)
+
+            Example container with all available properties (for illustration purposes). Note that
+            ``ContainerProperty`` has several options not shown here, like ``name``, ``description``,
+            ``nullable``, ``auto_increment``, ``default_value`` and ``immutable`` that may be specified,
+            depending on the choice of property type (e.g. ``auto_increment`` only works with integer types).
+
+                >>> from cognite.client.data_classes.data_modeling.data_types import UnitReference, EnumValue
+                >>> from cognite.client.data_classes.data_modeling.data_types import (
+                ...     Boolean, Date, DirectRelation, Enum, FileReference, Float32, Float64,
+                ...     Int32, Int64, Json, SequenceReference, Text, TimeSeriesReference, Timestamp
+                ... )
+                >>> container_properties = {
+                ...     "prop01": ContainerProperty(Boolean),
+                ...     "prop02": ContainerProperty(Boolean(is_list=True)),
+                ...     "prop03": ContainerProperty(Date),
+                ...     "prop04": ContainerProperty(Date(is_list=True)),
+                ...     "prop05": ContainerProperty(Timestamp),
+                ...     "prop06": ContainerProperty(Timestamp(is_list=True)),
+                ...     "prop07": ContainerProperty(Text),
+                ...     "prop08": ContainerProperty(Text(is_list=True)),
+                ...     # Note: DirectRelation(list) support `container`: The (optional) required type for the node
+                ...     #       the direct relation points to.
+                ...     "prop09": ContainerProperty(DirectRelation),
+                ...     "prop10": ContainerProperty(DirectRelation(is_list=True)),
+                ...     # Note: Enum also support `unknown_value`: The value to use when the enum value is unknown.
+                ...     "prop11": ContainerProperty(
+                ...         Enum({"Closed": EnumValue("Valve is closed"),
+                ...               "Opened": EnumValue("Valve is opened")})),
+                ...     # Note: Floats support unit references, e.g. `unit=UnitReference("pressure:bar")`:
+                ...     "prop12": ContainerProperty(Float32),
+                ...     "prop13": ContainerProperty(Float32(is_list=True)),
+                ...     "prop14": ContainerProperty(Float64),
+                ...     "prop15": ContainerProperty(Float64(is_list=True)),
+                ...     "prop16": ContainerProperty(Int32),
+                ...     "prop17": ContainerProperty(Int32(is_list=True)),
+                ...     "prop18": ContainerProperty(Int64),
+                ...     "prop19": ContainerProperty(Int64(is_list=True)),
+                ...     "prop20": ContainerProperty(Json),
+                ...     "prop21": ContainerProperty(Json(is_list=True)),
+                ...     "prop22": ContainerProperty(SequenceReference),
+                ...     "prop22": ContainerProperty(SequenceReference(is_list=True)),
+                ...     # Note: It is adviced to represent files and time series directly as nodes
+                ...     #       instead of referencing existing:
+                ...     "prop23": ContainerProperty(FileReference),
+                ...     "prop23": ContainerProperty(FileReference(is_list=True)),
+                ...     "prop24": ContainerProperty(TimeSeriesReference),
+                ...     "prop24": ContainerProperty(TimeSeriesReference(is_list=True)),
+                ... }
+
         """
         return self._create_multiple(
             list_cls=ContainerList,
