2a06f9887ba8cea033aa0d018b2079ee00af7128
Luis Pereira <luis.pereira@cognite.com>
2024-12-10 10:57:03 +0100

    fix

--- tests/tests_integration/test_api/test_simulators/test_simulators.py
+++ tests/tests_integration/test_api/test_simulators/test_simulators.py
@@ -42,16 +42,11 @@ def seed_simulator(cognite_client: CogniteClient, seed_resource_names) -> None:
     simulator_external_id = seed_resource_names["simulator_external_id"]
     simulators = cognite_client.simulators.list()
     simulator_exists = len(list(filter(lambda x: x.external_id == simulator_external_id, simulators))) > 0
-    if simulator_exists:
+    if not simulator_exists:
         cognite_client.post(
-            f"/api/v1/projects/{cognite_client.config.project}/simulators/delete",
-            json={"items": [{"externalId": seed_resource_names["simulator_external_id"]}]},
+            f"/api/v1/projects/{cognite_client.config.project}/simulators",
+            json={"items": [simulator]},
         )
-
-    cognite_client.post(
-        f"/api/v1/projects/{cognite_client.config.project}/simulators",
-        json={"items": [simulator]},
-    )
 
 
 @pytest.fixture
