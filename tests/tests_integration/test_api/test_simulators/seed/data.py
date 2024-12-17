ae25ff057a875b2a17b8d91a91443b48f9d718e0
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-12-17 09:23:58 +0000

    add simulator test

---
+++ tests/tests_integration/test_api/test_simulators/seed/data.py
@@ -0,0 +1,201 @@
+data_set_id = 97552494921583
+
+resource_names = {
+    "simulator_external_id": "py_sdk_integration_tests",
+    "simulator_integration_external_id": "py_sdk_integration_tests_connector",
+    "simulator_model_external_id": "py_sdk_integration_tests_model",
+    "simulator_model_revision_external_id": "pysdk_model_revision",
+    "simulator_model_file_external_id": "ShowerMixer_simulator_model_file_3",
+    "simulator_routine_external_id": "pysdk_routine",
+    "simulator_routine_revision_external_id": "pysdk_routine_revision",
+    "simulator_test_data_set_id": data_set_id,
+}
+
+simulator = {
+    "name": resource_names["simulator_external_id"],
+    "externalId": resource_names["simulator_external_id"],
+    "fileExtensionTypes": ["dwxmz"],
+    "modelTypes": [{"name": "Steady State", "key": "SteadyState"}],
+    "stepFields": [
+        {
+            "stepType": "get/set",
+            "fields": [
+                {
+                    "name": "objectName",
+                    "label": "Simulation Object Name",
+                    "info": "Enter the name of the DWSIM object, i.e. Feed",
+                },
+                {
+                    "name": "objectProperty",
+                    "label": "Simulation Object Property",
+                    "info": "Enter the property of the DWSIM object, i.e. Temperature",
+                },
+            ],
+        },
+        {
+            "stepType": "command",
+            "fields": [
+                {
+                    "name": "command",
+                    "label": "Command",
+                    "info": "Select a command",
+                    "options": [{"label": "Solve Flowsheet", "value": "Solve"}],
+                }
+            ],
+        },
+    ],
+    "unitQuantities": [
+        {
+            "name": "mass",
+            "label": "Mass",
+            "units": [{"label": "kg", "name": "kg"}, {"label": "g", "name": "g"}, {"label": "lb", "name": "lb"}],
+        },
+        {
+            "name": "time",
+            "label": "Time",
+            "units": [{"label": "s", "name": "s"}, {"label": "min.", "name": "min."}, {"label": "h", "name": "h"}],
+        },
+        {
+            "name": "accel",
+            "label": "Acceleration",
+            "units": [
+                {"label": "m/s2", "name": "m/s2"},
+                {"label": "cm/s2", "name": "cm/s2"},
+                {"label": "ft/s2", "name": "ft/s2"},
+            ],
+        },
+        {
+            "name": "force",
+            "label": "Force",
+            "units": [
+                {"label": "N", "name": "N"},
+                {"label": "dyn", "name": "dyn"},
+                {"label": "kgf", "name": "kgf"},
+                {"label": "lbf", "name": "lbf"},
+            ],
+        },
+        {
+            "name": "volume",
+            "label": "Volume",
+            "units": [
+                {"label": "m3", "name": "m3"},
+                {"label": "cm3", "name": "cm3"},
+                {"label": "L", "name": "L"},
+                {"label": "ft3", "name": "ft3"},
+                {"label": "bbl", "name": "bbl"},
+                {"label": "gal[US]", "name": "gal[US]"},
+                {"label": "gal[UK]", "name": "gal[UK]"},
+            ],
+        },
+        {
+            "name": "density",
+            "label": "Density",
+            "units": [
+                {"label": "kg/m3", "name": "kg/m3"},
+                {"label": "g/cm3", "name": "g/cm3"},
+                {"label": "lbm/ft3", "name": "lbm/ft3"},
+            ],
+        },
+        {
+            "name": "diameter",
+            "label": "Diameter",
+            "units": [{"label": "mm", "name": "mm"}, {"label": "in", "name": "in"}],
+        },
+        {
+            "name": "distance",
+            "label": "Distance",
+            "units": [{"label": "m", "name": "m"}, {"label": "ft", "name": "ft"}, {"label": "cm", "name": "cm"}],
+        },
+        {
+            "name": "heatflow",
+            "label": "Heat Flow",
+            "units": [
+                {"label": "kW", "name": "kW"},
+                {"label": "kcal/h", "name": "kcal/h"},
+                {"label": "BTU/h", "name": "BTU/h"},
+                {"label": "BTU/s", "name": "BTU/s"},
+                {"label": "cal/s", "name": "cal/s"},
+                {"label": "HP", "name": "HP"},
+                {"label": "kJ/h", "name": "kJ/h"},
+                {"label": "kJ/d", "name": "kJ/d"},
+                {"label": "MW", "name": "MW"},
+                {"label": "W", "name": "W"},
+                {"label": "BTU/d", "name": "BTU/d"},
+                {"label": "MMBTU/d", "name": "MMBTU/d"},
+                {"label": "MMBTU/h", "name": "MMBTU/h"},
+                {"label": "kcal/s", "name": "kcal/s"},
+                {"label": "kcal/h", "name": "kcal/h"},
+                {"label": "kcal/d", "name": "kcal/d"},
+            ],
+        },
+        {
+            "name": "pressure",
+            "label": "Pressure",
+            "units": [
+                {"label": "Pa", "name": "Pa"},
+                {"label": "atm", "name": "atm"},
+                {"label": "kgf/cm2", "name": "kgf/cm2"},
+                {"label": "kgf/cm2g", "name": "kgf/cm2g"},
+                {"label": "lbf/ft2", "name": "lbf/ft2"},
+                {"label": "kPa", "name": "kPa"},
+                {"label": "kPag", "name": "kPag"},
+                {"label": "bar", "name": "bar"},
+                {"label": "barg", "name": "barg"},
+                {"label": "ftH2O", "name": "ftH2O"},
+                {"label": "inH2O", "name": "inH2O"},
+                {"label": "inHg", "name": "inHg"},
+                {"label": "mbar", "name": "mbar"},
+                {"label": "mH2O", "name": "mH2O"},
+                {"label": "mmH2O", "name": "mmH2O"},
+                {"label": "mmHg", "name": "mmHg"},
+                {"label": "MPa", "name": "MPa"},
+                {"label": "psi", "name": "psi"},
+                {"label": "psig", "name": "psig"},
+            ],
+        },
+        {
+            "name": "velocity",
+            "label": "Velocity",
+            "units": [
+                {"label": "m/s", "name": "m/s"},
+                {"label": "cm/s", "name": "cm/s"},
+                {"label": "mm/s", "name": "mm/s"},
+                {"label": "km/h", "name": "km/h"},
+                {"label": "ft/h", "name": "ft/h"},
+                {"label": "ft/min", "name": "ft/min"},
+                {"label": "ft/s", "name": "ft/s"},
+                {"label": "in/s", "name": "in/s"},
+            ],
+        },
+        {
+            "name": "temperature",
+            "label": "Temperature",
+            "units": [
+                {"label": "K", "name": "K"},
+                {"label": "R", "name": "R"},
+                {"label": "C", "name": "C"},
+                {"label": "F", "name": "F"},
+            ],
+        },
+        {
+            "name": "volumetricFlow",
+            "label": "Volumetric Flow",
+            "units": [
+                {"label": "m3/h", "name": "m3/h"},
+                {"label": "cm3/s", "name": "cm3/s"},
+                {"label": "L/h", "name": "L/h"},
+                {"label": "L/min", "name": "L/min"},
+                {"label": "L/s", "name": "L/s"},
+                {"label": "ft3/h", "name": "ft3/h"},
+                {"label": "ft3/min", "name": "ft3/min"},
+                {"label": "ft3/s", "name": "ft3/s"},
+                {"label": "gal[US]/h", "name": "gal[US]/h"},
+                {"label": "gal[US]/min", "name": "gal[US]/min"},
+                {"label": "gal[US]/s", "name": "gal[US]/s"},
+                {"label": "gal[UK]/h", "name": "gal[UK]/h"},
+                {"label": "gal[UK]/min", "name": "gal[UK]/min"},
+                {"label": "gal[UK]/s", "name": "gal[UK]/s"},
+            ],
+        },
+    ],
+}
