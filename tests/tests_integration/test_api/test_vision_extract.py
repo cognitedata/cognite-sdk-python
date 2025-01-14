5f3ab4ef4f86d810d66d1a59b738e82a1dd0fbf3
Håkon V. Treider <haakonvt@gmail.com>
2025-01-14 13:48:11 +0100

    add url is_retryable tests. fix tests...

--- tests/tests_integration/test_api/test_vision_extract.py
+++ tests/tests_integration/test_api/test_vision_extract.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterator
+
 import pytest
 
 from cognite.client import CogniteClient
@@ -13,9 +15,9 @@ from cognite.client.data_classes.contextualization import (
 
 # TODO(VIS-986): replace this file generator with a hard-coded ID of an actual image
 @pytest.fixture(scope="class")
-def file_id(cognite_client: CogniteClient) -> int:
+def file_id(cognite_client: CogniteClient, os_and_py_version: str) -> Iterator[int]:
     # Create a test file
-    name = "vision_extract_test_file"
+    name = "vision_extract_test_file" + os_and_py_version
     file = cognite_client.files.create(FileMetadata(external_id=name, name=name), overwrite=True)[0]
     yield file.id
 
