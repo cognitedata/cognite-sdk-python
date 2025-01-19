efdbb0f32c32ed39cdd50bfc29fe410ee575fddb
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2025-01-19 22:03:20 +0000

    update model revisions in testing.py

--- cognite/client/testing.py
+++ cognite/client/testing.py
@@ -45,7 +45,7 @@ from cognite.client._api.relationships import RelationshipsAPI
 from cognite.client._api.sequences import SequencesAPI, SequencesDataAPI
 from cognite.client._api.simulators import SimulatorsAPI
 from cognite.client._api.simulators.integrations import SimulatorIntegrationsAPI
-from cognite.client._api.simulators.models import SimulatorModelsAPI
+from cognite.client._api.simulators.models import SimulatorModelRevisionsAPI, SimulatorModelsAPI
 from cognite.client._api.synthetic_time_series import SyntheticDatapointsAPI
 from cognite.client._api.templates import (
     TemplateGroupsAPI,
@@ -148,6 +148,7 @@ class CogniteClientMock(MagicMock):
         self.simulators = MagicMock(spec=SimulatorsAPI)
         self.simulators.integrations = MagicMock(spec_set=SimulatorIntegrationsAPI)
         self.simulators.models = MagicMock(spec_set=SimulatorModelsAPI)
+        self.simulators.models.revisions = MagicMock(spec_set=SimulatorModelRevisionsAPI)
 
         self.sequences = MagicMock(spec=SequencesAPI)
         self.sequences.rows = MagicMock(spec_set=SequencesDataAPI)
