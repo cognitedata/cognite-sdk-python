c03de06b354f788c85d22115dfe1e51a7e9f81f3
Håkon V. Treider <haakonvt@gmail.com>
2025-01-20 23:37:05 +0100

    fix tests/tests_unit/test_api/test_data_sets.py

--- tests/tests_unit/test_api/test_data_sets.py
+++ tests/tests_unit/test_api/test_data_sets.py
@@ -7,46 +7,46 @@ from tests.utils import get_url, jsgz_load
 
 
 @pytest.fixture
+def example_data_set():
+    return {
+        "id": 0,
+        "externalId": "string",
+        "name": "string",
+        "description": "string",
+        "metadata": {"metadata-key": "metadata-value"},
+        "writeProtected": False,
+        "createdTime": 0,
+        "lastUpdatedTime": 0,
+    }
+
+
+@pytest.fixture
 def dataset_update():
     yield DataSetUpdate(id=1).description.set("_")
 
 
 @pytest.fixture
-def mock_ds_response(httpx_mock, cognite_client):
-    response_body = {
-        "items": [
-            {
-                "id": 0,
-                "externalId": "string",
-                "name": "string",
-                "description": "string",
-                "metadata": {"metadata-key": "metadata-value"},
-                "writeProtected": False,
-                "createdTime": 0,
-                "lastUpdatedTime": 0,
-            }
-        ]
-    }
+def mock_ds_response(httpx_mock, cognite_client, example_data_set):
+    response_body = {"items": [example_data_set]}
     url_pattern = re.compile(
         re.escape(get_url(cognite_client.data_sets)) + r"/datasets(?:/byids|/update|/delete|/list|$|\?.+)"
     )
-    # ....assert_all_requests_are_fired = False  # TODO
 
-    httpx_mock.add_response(method="POST", url=url_pattern, status_code=200, json=response_body)
-    httpx_mock.add_response(method="GET", url=url_pattern, status_code=200, json=response_body)
+    httpx_mock.add_response(method="POST", url=url_pattern, status_code=200, json=response_body, is_optional=True)
+    httpx_mock.add_response(method="GET", url=url_pattern, status_code=200, json=response_body, is_optional=True)
     yield httpx_mock
 
 
 class TestDataset:
-    def test_retrieve_single(self, cognite_client, mock_ds_response):
+    def test_retrieve_single(self, cognite_client, mock_ds_response, example_data_set):
         res = cognite_client.data_sets.retrieve(id=1)
         assert isinstance(res, DataSet)
-        assert mock_ds_response.get_requests()[0].response.json()["items"][0] == res.dump(camel_case=True)
+        assert example_data_set == res.dump(camel_case=True)
 
-    def test_retrieve_multiple(self, cognite_client, mock_ds_response):
+    def test_retrieve_multiple(self, cognite_client, mock_ds_response, example_data_set):
         res = cognite_client.data_sets.retrieve_multiple(ids=[1])
         assert isinstance(res, DataSetList)
-        assert mock_ds_response.get_requests()[0].response.json()["items"] == res.dump(camel_case=True)
+        assert [example_data_set] == res.dump(camel_case=True)
 
     def test_list_with_timestamp_range(self, cognite_client, mock_ds_response):
         cognite_client.data_sets.list(created_time=TimestampRange(min=20))
@@ -62,21 +62,21 @@ class TestDataset:
         list(cognite_client.data_sets(write_protected=True, limit=10))
         calls = mock_ds_response.get_requests()
         assert 1 == len(calls)
-        assert {"cursor": None, "limit": 10, "filter": {"writeProtected": True}} == jsgz_load(calls[0].content)
+        assert {"limit": 10, "filter": {"writeProtected": True}} == jsgz_load(calls[0].content)
 
-    def test_create_single(self, cognite_client, mock_ds_response):
+    def test_create_single(self, cognite_client, mock_ds_response, example_data_set):
         res = cognite_client.data_sets.create(DataSet(external_id="1", name="blabla"))
         assert isinstance(res, DataSet)
-        assert mock_ds_response.get_requests()[0].response.json()["items"][0] == res.dump(camel_case=True)
+        assert example_data_set == res.dump(camel_case=True)
 
-    def test_create_multiple(self, cognite_client, mock_ds_response):
+    def test_create_multiple(self, cognite_client, mock_ds_response, example_data_set):
         res = cognite_client.data_sets.create([DataSet(external_id="1")])
         assert isinstance(res, DataSetList)
-        assert mock_ds_response.get_requests()[0].response.json()["items"] == res.dump(camel_case=True)
+        assert [example_data_set] == res.dump(camel_case=True)
 
-    def test_iter_single(self, cognite_client, mock_ds_response):
+    def test_iter_single(self, cognite_client, mock_ds_response, example_data_set):
         for dataset in cognite_client.data_sets:
-            assert mock_ds_response.get_requests()[0].response.json()["items"][0] == dataset.dump(camel_case=True)
+            assert example_data_set == dataset.dump(camel_case=True)
 
     @pytest.mark.skip("delete not implemented")
     def test_delete_single(self, cognite_client, mock_ds_response):
@@ -90,23 +90,23 @@ class TestDataset:
         assert {"items": [{"id": 1}]} == jsgz_load(mock_ds_response.get_requests()[0].content)
         assert res is None
 
-    def test_update_with_resource_class(self, cognite_client, mock_ds_response):
+    def test_update_with_resource_class(self, cognite_client, mock_ds_response, example_data_set):
         res = cognite_client.data_sets.update(DataSet(id=1))
         assert isinstance(res, DataSet)
-        assert mock_ds_response.get_requests()[0].response.json()["items"][0] == res.dump(camel_case=True)
+        assert example_data_set == res.dump(camel_case=True)
 
-    def test_update_with_update_class(self, cognite_client, mock_ds_response, dataset_update):
+    def test_update_with_update_class(self, cognite_client, mock_ds_response, dataset_update, example_data_set):
         res = cognite_client.data_sets.update(dataset_update)
         assert isinstance(res, DataSet)
-        assert mock_ds_response.get_requests()[0].response.json()["items"][0] == res.dump(camel_case=True)
+        assert example_data_set == res.dump(camel_case=True)
 
-    def test_update_multiple(self, cognite_client, mock_ds_response, dataset_update):
+    def test_update_multiple(self, cognite_client, mock_ds_response, dataset_update, example_data_set):
         res = cognite_client.data_sets.update([dataset_update])
         assert isinstance(res, DataSetList)
-        assert mock_ds_response.get_requests()[0].response.json()["items"] == res.dump(camel_case=True)
+        assert [example_data_set] == res.dump(camel_case=True)
 
     def test_event_update_object(self):
-        assert isinstance(
+        update = (
             DataSetUpdate(1)
             .description.set("")
             .description.set(None)
@@ -117,9 +117,9 @@ class TestDataset:
             .write_protected.set(False)
             .write_protected.set(None)
             .name.set("")
-            .name.set(None),
-            DataSetUpdate,
+            .name.set(None)
         )
+        assert isinstance(update, DataSetUpdate)
 
 
 @pytest.fixture
