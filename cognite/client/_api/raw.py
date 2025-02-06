823be2d155fea73502baf1442831ca1c85217bf4
Håkon V. Treider <haakonvt@gmail.com>
2025-02-06 22:57:01 +0100

    add drop_none_values to fix httpx usage of url params that are null

--- cognite/client/_api/raw.py
+++ cognite/client/_api/raw.py
@@ -13,6 +13,7 @@ from cognite.client._constants import _RUNNING_IN_BROWSER, DEFAULT_LIMIT_READ
 from cognite.client.data_classes import raw
 from cognite.client.data_classes.raw import Database, DatabaseList, Row, RowCore, RowList, RowWrite
 from cognite.client.utils._auxiliary import (
+    drop_none_values,
     find_duplicates,
     is_finite,
     is_unlimited,
@@ -401,11 +402,13 @@ class RawRowsAPI(APIClient):
                 chunk_size=chunk_size,
                 method="GET",
                 limit=limit,
-                filter={
-                    "minLastUpdatedTime": min_last_updated_time,
-                    "maxLastUpdatedTime": max_last_updated_time,
-                    "columns": self._make_columns_param(columns),
-                },
+                filter=drop_none_values(
+                    {
+                        "minLastUpdatedTime": min_last_updated_time,
+                        "maxLastUpdatedTime": max_last_updated_time,
+                        "columns": self._make_columns_param(columns),
+                    },
+                ),
             )
         return self._list_generator_concurrent(
             db_name=db_name,
@@ -772,11 +775,13 @@ class RawRowsAPI(APIClient):
     ) -> list[str]:
         return self._get(
             url_path=interpolate_and_url_encode("/raw/dbs/{}/tables/{}/cursors", db_name, table_name),
-            params={
-                "minLastUpdatedTime": min_last_updated_time,
-                "maxLastUpdatedTime": max_last_updated_time,
-                "numberOfCursors": n_cursors,
-            },
+            params=drop_none_values(
+                {
+                    "minLastUpdatedTime": min_last_updated_time,
+                    "maxLastUpdatedTime": max_last_updated_time,
+                    "numberOfCursors": n_cursors,
+                }
+            ),
         ).json()["items"]
 
     def list(
