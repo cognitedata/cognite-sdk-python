0830b5cdf7b0a5cef84525f2a92f6ea9198ee695
Luis Pereira <luis.pereira@cognite.com>
2025-02-12 20:24:12 -0300

    fmt

--- cognite/client/_api/simulators/__init__.py
+++ cognite/client/_api/simulators/__init__.py
@@ -5,8 +5,8 @@ from typing import TYPE_CHECKING, overload
 
 from cognite.client._api.simulators.integrations import SimulatorIntegrationsAPI
 from cognite.client._api.simulators.models import SimulatorModelsAPI
-from cognite.client._api_client import APIClient
 from cognite.client._api.simulators.routines import SimulatorRoutinesAPI
+from cognite.client._api_client import APIClient
 from cognite.client._constants import DEFAULT_LIMIT_READ
 from cognite.client.data_classes.simulators.simulators import Simulator, SimulatorList
 from cognite.client.utils._experimental import FeaturePreviewWarning
