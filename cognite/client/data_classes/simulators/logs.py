f4e5bf7b35afc9df6e9ebf4f4c3b91099bb91554
Luis Pereira <luis.pereira@cognite.com>
2025-03-04 14:45:03 +0100

    fix

--- cognite/client/data_classes/simulators/logs.py
+++ cognite/client/data_classes/simulators/logs.py
@@ -21,6 +21,14 @@ Severity = Literal["Debug", "Information", "Warning", "Error"]
 
 @dataclass
 class SimulatorLogData(CogniteObject):
+    """
+    Simulator log data represents a single log entry in a simulator log.
+    Args:
+        timestamp (int): Timestamp of the log message.
+        message (str): Log message.
+        severity (Severity): Log severity level.
+    """
+
     timestamp: int
     message: str
     severity: Severity
@@ -35,6 +43,22 @@ class SimulatorLogData(CogniteObject):
 
 
 class SimulatorLog(CogniteResource):
+    """
+    Simulator logs track what happens during simulation runs, model parsing, and generic connector logic.
+    They provide valuable information for monitoring, debugging, and auditing.
+
+    Simulator logs capture important events, messages, and exceptions that occur during the execution of simulations, model parsing, and connector operations.
+    They help users identify issues, diagnose problems, and gain insights into the behavior of the simulator integrations.
+
+    Args:
+        id (int): A unique id of a simulator resource log.
+        data (Sequence[SimulatorLogData]): Log data of the simulator resource.
+        created_time (int): The number of milliseconds since epoch.
+        last_updated_time (int): The number of milliseconds since epoch.
+        data_set_id (int): Dataset id of the resource.
+        severity (Severity | None): Minimum severity level of the log data. This overrides connector configuration minimum severity level and can be used for more granular control.
+    """
+
     def __init__(
         self,
         id: int,
