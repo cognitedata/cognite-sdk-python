c8f62dd93d74af08110a456ff5ef2ea6464f017c
Pål Rønning <ronpal@users.noreply.github.com>
2025-03-11 09:56:55 +0100

    Added authentication property to update class

--- cognite/client/data_classes/hosted_extractors/sources.py
+++ cognite/client/data_classes/hosted_extractors/sources.py
@@ -809,6 +809,10 @@ class RestSourceUpdate(SourceUpdate):
         def set(self, value: int) -> RestSourceUpdate:
             return self._set(value)
 
+    class _AuthenticationUpdate(CognitePrimitiveUpdate):
+        def set(self, value: Authentication | None) -> RestSourceUpdate:
+            return self._set(value.dump() if value else None)
+
     @property
     def host(self) -> _HostUpdate:
         return RestSourceUpdate._HostUpdate(self, "host")
@@ -820,6 +824,10 @@ class RestSourceUpdate(SourceUpdate):
     @property
     def port(self) -> _PortUpdate:
         return RestSourceUpdate._PortUpdate(self, "port")
+
+    @property
+    def authentication(self) -> _AuthenticationUpdate:
+        return RestSourceUpdate._AuthenticationUpdate(self, "authentication")
 
     @classmethod
     def _get_update_properties(cls, item: CogniteResource | None = None) -> list[PropertySpec]:
