61a55e55e1e42e62f3b6517328731f5b0029466f
Ivan Polomani <ivan.polomanyi@cognite.com>
2025-03-27 16:48:34 +0100

    add routine revision filter class

--- cognite/client/data_classes/simulators/filters.py
+++ cognite/client/data_classes/simulators/filters.py
@@ -4,6 +4,8 @@ from collections.abc import Sequence
 from typing import Any, Literal
 
 from cognite.client.data_classes._base import CogniteFilter, CogniteSort
+from cognite.client.data_classes.shared import TimestampRange
+from cognite.client.utils.useful_types import SequenceNotStr
 
 
 class SimulatorIntegrationFilter(CogniteFilter):
@@ -59,3 +61,21 @@ class PropertySort(CogniteSort):
         dumped = super().dump(camel_case=camel_case)
         dumped["property"] = self.property
         return dumped
+
+
+class SimulatorRoutineRevisionsFilter(CogniteFilter):
+    def __init__(
+        self,
+        routine_external_ids: SequenceNotStr[str] | None = None,
+        all_versions: bool | None = None,
+        model_external_ids: SequenceNotStr[str] | None = None,
+        simulator_integration_external_ids: SequenceNotStr[str] | None = None,
+        simulator_external_ids: SequenceNotStr[str] | None = None,
+        created_time: TimestampRange | None = None,
+    ) -> None:
+        self.model_external_ids = model_external_ids
+        self.all_versions = all_versions
+        self.routine_external_ids = routine_external_ids
+        self.simulator_integration_external_ids = simulator_integration_external_ids
+        self.simulator_external_ids = simulator_external_ids
+        self.created_time = created_time
