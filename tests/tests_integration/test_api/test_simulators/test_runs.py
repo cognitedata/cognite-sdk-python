431f50e8ffc87b493126e1c2d158f2ee51751d3e
Luis Pereira <luis.pereira@cognite.com>
2025-04-03 01:42:48 +0200

    fix

--- tests/tests_integration/test_api/test_simulators/test_runs.py
+++ tests/tests_integration/test_api/test_simulators/test_runs.py
@@ -12,27 +12,18 @@ class TestSimulatorRuns:
         self, cognite_client: CogniteClient, seed_simulator_routine_revisions, seed_resource_names
     ) -> None:
         routine_external_id = seed_resource_names["simulator_routine_external_id"]
-        runs = [
-            SimulationRunWrite(
-                run_type="external",
-                routine_external_id=routine_external_id,
-            )
-        ]
-        created_runs = cognite_client.simulators.runs.create(runs)
-        assert len(created_runs) == 1
-        assert created_runs[0].run_type == "external"
+        created_runs = cognite_client.simulators.runs.create(
+            [
+                SimulationRunWrite(
+                    run_type="external",
+                    routine_external_id=routine_external_id,
+                )
+            ]
+        )
         assert created_runs[0].routine_external_id == routine_external_id
-        assert created_runs[0].status == "ready"
+        assert created_runs[0].id is not None
 
-    def test_list_runs(self, cognite_client: CogniteClient, seed_resource_names) -> None:
-        routine_external_id = seed_resource_names["simulator_routine_external_id"]
-        runs = cognite_client.simulators.runs.list()
-        assert len(runs) > 0
-
-        list_by_routine = cognite_client.simulators.runs.list(routine_external_ids=[routine_external_id])
-        assert all(run.routine_external_id == routine_external_id for run in list_by_routine)
-
-    def test_list_runs_by_status(self, cognite_client: CogniteClient, seed_resource_names) -> None:
+    def test_list_filtering(self, cognite_client: CogniteClient, seed_resource_names) -> None:
         routine_external_id = seed_resource_names["simulator_routine_external_id"]
         status = ["running", "success", "failure"]
         for _ in range(3):
@@ -45,7 +36,6 @@ class TestSimulatorRuns:
                 ]
             )
             assert created_runs[0].routine_external_id == routine_external_id
-            run_id = created_runs[0].id
             status_to_be_set = status.pop(0)
 
             cognite_client.simulators._post(
@@ -53,15 +43,18 @@ class TestSimulatorRuns:
                 json={
                     "items": [
                         {
-                            "id": run_id,
+                            "id": created_runs[0].id,
                             "status": status_to_be_set,
                         }
                     ]
                 },
             )
 
-            list_runs_status_filter = cognite_client.simulators.runs.list(
+            filter_by_status = cognite_client.simulators.runs.list(
                 status=status_to_be_set, routine_external_ids=[routine_external_id]
             )
-            assert list_runs_status_filter[0].status == status_to_be_set
-            assert list_runs_status_filter[0].id == run_id
+            assert filter_by_status[0].status == status_to_be_set
+            assert filter_by_status[0].id == created_runs[0].id
+
+        filter_by_routine = cognite_client.simulators.runs.list(routine_external_ids=[routine_external_id])
+        assert len(filter_by_routine) >= 3
