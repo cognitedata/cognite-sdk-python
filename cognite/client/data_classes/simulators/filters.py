b87f42d7e582576b958904b35539860c61f9855b
Luis Pereira <luis.pereira@cognite.com>
2025-04-06 23:04:44 +0200

    fix

--- cognite/client/data_classes/simulators/filters.py
+++ cognite/client/data_classes/simulators/filters.py
@@ -8,6 +8,14 @@ from cognite.client.data_classes.shared import TimestampRange
 from cognite.client.utils.useful_types import SequenceNotStr
 
 
+def parse_str_or_sequence(value: str | Sequence[str] | None) -> list[str] | None:
+    if isinstance(value, str):
+        return [value]
+    elif isinstance(value, Sequence):
+        return list(value)
+    return None
+
+
 class SimulatorIntegrationFilter(CogniteFilter):
     def __init__(
         self,
@@ -52,27 +60,11 @@ class SimulatorRunsFilter(CogniteFilter):
         model_revision_external_ids: str | Sequence[str] | None = None,
     ) -> None:
         self.model_external_ids = [model_external_ids] if isinstance(model_external_ids, str) else model_external_ids
-        self.simulator_integration_external_ids = (
-            [simulator_integration_external_ids]
-            if isinstance(simulator_integration_external_ids, str)
-            else simulator_integration_external_ids
-        )
-        self.simulator_external_ids = (
-            [simulator_external_ids] if isinstance(simulator_external_ids, str) else simulator_external_ids
-        )
-        self.routine_external_ids = (
-            [routine_external_ids] if isinstance(routine_external_ids, str) else routine_external_ids
-        )
-        self.routine_revision_external_ids = (
-            [routine_revision_external_ids]
-            if isinstance(routine_revision_external_ids, str)
-            else routine_revision_external_ids
-        )
-        self.model_revision_external_ids = (
-            [model_revision_external_ids]
-            if isinstance(model_revision_external_ids, str)
-            else model_revision_external_ids
-        )
+        self.simulator_integration_external_ids = parse_str_or_sequence(simulator_integration_external_ids)
+        self.simulator_external_ids = parse_str_or_sequence(simulator_external_ids)
+        self.routine_external_ids = parse_str_or_sequence(routine_external_ids)
+        self.routine_revision_external_ids = parse_str_or_sequence(routine_revision_external_ids)
+        self.model_revision_external_ids = parse_str_or_sequence(model_revision_external_ids)
         self.status = status
         self.run_type = run_type
 
