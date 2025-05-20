97787c92b1ce8be7d6f93b6a8140704c0a9b7d7c
Luis Pereira <91738660+lpereiracgn@users.noreply.github.com>
2025-05-20 11:39:00 +0200

    add support for simulation run data (#2151)

--- tests/tests_integration/test_api/test_simulators/test_runs.py
+++ tests/tests_integration/test_api/test_simulators/test_runs.py
@@ -1,7 +1,12 @@
 import pytest
 
 from cognite.client._cognite_client import CogniteClient
-from cognite.client.data_classes.simulators.runs import SimulationRunWrite
+from cognite.client.data_classes.simulators.runs import (
+    SimulationInput,
+    SimulationOutput,
+    SimulationRunWrite,
+    SimulationValueUnitName,
+)
 
 
 @pytest.mark.usefixtures("seed_resource_names", "seed_simulator_routine_revisions")
@@ -56,3 +61,64 @@ class TestSimulatorRuns:
         assert len(created_runs) == 1
         assert created_runs[0].routine_external_id == routine_external_id
         assert created_runs[0].id is not None
+
+    def test_list_run_data(self, cognite_client: CogniteClient, seed_resource_names) -> None:
+        routine_external_id = seed_resource_names["simulator_routine_external_id"]
+        created_run = cognite_client.simulators.runs.create(
+            [
+                SimulationRunWrite(
+                    run_type="external",
+                    routine_external_id=routine_external_id,
+                )
+            ]
+        )
+
+        outputs = [
+            SimulationOutput(
+                reference_id="ST",
+                simulator_object_reference={"address": "test_out"},
+                value=18.5,
+                value_type="DOUBLE",
+                unit=SimulationValueUnitName(name="C"),
+            ).dump(),
+        ]
+
+        inputs = [
+            SimulationInput(
+                reference_id="CWT",
+                value=11.0,
+                value_type="DOUBLE",
+                overridden=True,
+                unit=SimulationValueUnitName(name="C"),
+            ).dump(),
+            SimulationInput(
+                reference_id="CWP",
+                overridden=True,
+                value=[5.0],
+                value_type="DOUBLE_ARRAY",
+                unit=SimulationValueUnitName(name="bar"),
+            ).dump(),
+        ]
+
+        cognite_client.simulators._post(
+            "/simulators/run/callback",
+            json={
+                "items": [
+                    {
+                        "id": created_run[0].id,
+                        "status": "success",
+                        "outputs": outputs,
+                        "inputs": inputs,
+                    }
+                ]
+            },
+        )
+
+        get_run_data = cognite_client.simulators.runs.list_run_data(
+            run_id=created_run[0].id,
+        )
+
+        assert len(get_run_data) == 1
+        assert get_run_data[0].run_id == created_run[0].id
+        assert get_run_data[0].inputs[0].dump() == inputs[0]
+        assert get_run_data[0].outputs[0].dump() == outputs[0]
