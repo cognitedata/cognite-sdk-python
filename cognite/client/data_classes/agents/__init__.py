7aae071a8120e45b22a16ed4dae8ed8c50539ccf
Kelvin  Sundli <kelvin.sundli@cognite.com>
2025-06-14 16:46:34 +0200

    expose agent data classes

--- cognite/client/data_classes/agents/__init__.py
+++ cognite/client/data_classes/agents/__init__.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
 from cognite.client.data_classes.agents.agent_tools import AgentTool, AgentToolList, AgentToolWrite, AgentToolWriteList
+from cognite.client.data_classes.agents.agents import Agent, AgentList, AgentWrite, AgentWriteList
 
 __all__ = [
+    "Agent",
+    "AgentList",
     "AgentTool",
     "AgentToolList",
     "AgentToolWrite",
     "AgentToolWriteList",
+    "AgentWrite",
+    "AgentWriteList",
 ]
