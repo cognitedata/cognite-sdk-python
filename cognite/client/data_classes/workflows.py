10f7977e0daec3f45459f9f415a632bd3291be0f
Sachin Bhagwat <sachin.bhagwat@cognite.com>
2025-07-24 12:49:40 +0530

    removed the print statements

--- cognite/client/data_classes/workflows.py
+++ cognite/client/data_classes/workflows.py
@@ -1415,7 +1415,8 @@ class WorkflowScheduledTriggerRule(WorkflowTriggerRule):
 
     Args:
         cron_expression (str): The cron specification for the scheduled trigger.
-        timezone (str | None): The timezone for the scheduled trigger. This is optional.
+        timezone (str | None): The timezone in which the scheduled trigger should be evaluated.
+            If not provided, UTC will be used as the default timezone on the server side.
     """
 
     _trigger_type = "schedule"
