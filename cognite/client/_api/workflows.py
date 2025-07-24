37dfa164023eb8cb8b9031abc6466160fa3cdd0e
Sachin Bhagwat <sachin.bhagwat@cognite.com>
2025-07-24 10:30:52 +0530

    removed the print statements

--- cognite/client/_api/workflows.py
+++ cognite/client/_api/workflows.py
@@ -122,14 +122,10 @@ class WorkflowTriggerAPI(APIClient):
         )
         dumped = workflow_trigger.dump(camel_case=True)
         dumped["authentication"] = {"nonce": nonce}
-        print("final request")
-        print(dumped)
         response = self._post(
             url_path=self._RESOURCE_PATH,
             json={"items": [dumped]},
         )
-        print("final response")
-        print(response.json())
         return WorkflowTrigger._load(response.json().get("items")[0])
 
     # TODO: remove method and associated data classes in next major release
