2cad0e988ad1efb125b4390bc3656fb571b9b720
anders-albert <anders0albert@gmail.com>
2025-07-26 14:26:35 +0200

    tests: more tests

--- tests/tests_integration/test_api/test_principals.py
+++ tests/tests_integration/test_api/test_principals.py
@@ -1,7 +1,8 @@
 import pytest
 
 from cognite.client import CogniteClient
-from cognite.client.data_classes.principals import Principal, PrincipalList
+from cognite.client.data_classes.principals import Principal, PrincipalList, ServicePrincipal
+from cognite.client.exceptions import CogniteAPIError
 
 
 @pytest.fixture(scope="session")
@@ -18,13 +19,26 @@ def me(client: CogniteClient) -> Principal:
     return client.iam.principals.me()
 
 
+@pytest.fixture(scope="session")
+def three_principals(client: CogniteClient) -> PrincipalList:
+    """Fixture to retrieve a list of three principals."""
+    principals = client.iam.principals.list(limit=3)
+    assert len(principals) == 3, "Expected exactly three principals to be returned."
+    return principals
+
+
+@pytest.fixture(scope="session")
+def service_account(client: CogniteClient) -> ServicePrincipal:
+    """Fixture to retrieve a service account principal."""
+    service_account = client.iam.principals.list(types="service_account", limit=1)
+    assert len(service_account) == 1, "Expected exactly one service account principal to be returned."
+    assert isinstance(service_account[0], ServicePrincipal), "Expected service account to be returned."
+    return service_account[0]
+
+
 class TestPrincipalsAPI:
     def test_me(self, client: CogniteClient, me: Principal) -> None:
         assert isinstance(me, Principal)
-
-    def test_list_principals(self, client: CogniteClient, me: Principal) -> None:
-        principals = client.iam.principals.list(limit=3)
-        assert 0 < len(principals) <= 3
 
     def test_filter_principals_by_type(self, client: CogniteClient, me: Principal) -> None:
         type_ = me._type
@@ -32,12 +46,38 @@ class TestPrincipalsAPI:
         assert len(principals) > 0
         assert any(p.id == me.id for p in principals), "The principal should be included in the list of principals."
 
-    def test_retrieve_principal(self, client: CogniteClient, me: Principal) -> None:
-        retrieved = client.iam.principals.retrieve(id=me.id)
-        assert retrieved.dump() == me.dump()
+    def test_retrieve_principal(self, client: CogniteClient, three_principals: PrincipalList) -> None:
+        retrieved = client.iam.principals.retrieve(id=three_principals[0].id)
+        assert retrieved.dump() == three_principals[0].dump(), (
+            "Retrieved principal should match the first principal in the list."
+        )
 
-    def test_retrieve_multiple_principals(self, client: CogniteClient, me: Principal) -> None:
-        retrieved = client.iam.principals.retrieve_multiple(id=[me.id])
+    def test_retrieve_non_existing_principal(self, client: CogniteClient) -> None:
+        assert client.iam.principals.retrieve(external_id="non-existing-principal-id") is None
+
+    def test_retrieve_multiple_principals(self, client: CogniteClient, three_principals: PrincipalList) -> None:
+        retrieved = client.iam.principals.retrieve_multiple(
+            ids=three_principals.as_ids(), external_ids=["not_existing"], ignore_unknown_ids=True
+        )
         assert isinstance(retrieved, PrincipalList)
-        assert len(retrieved) == 1
-        assert retrieved[0].dump() == me.dump()
+        assert len(retrieved) == len(three_principals)
+        assert retrieved.dump() == three_principals.dump(), "Retrieved principals should match the original list."
+
+    def test_retrieve_multiple_non_existing_raise(self, client: CogniteClient) -> None:
+        with pytest.raises(CogniteAPIError) as exc_info:
+            client.iam.principals.retrieve_multiple(
+                external_ids=["non-existing-principal-id"], ignore_unknown_ids=False
+            )
+
+        assert "not found" in str(exc_info.value), (
+            "Should raise an error when trying to retrieve non-existing principals without ignoring unknown IDs."
+        )
+
+    def test_retrieve_by_external_id(self, client: CogniteClient, service_account: ServicePrincipal) -> None:
+        assert service_account.external_id is not None, "Service account should have an external ID."
+        retrieved = client.iam.principals.retrieve_multiple(external_ids=[service_account.external_id])
+        assert isinstance(retrieved, PrincipalList)
+        assert len(retrieved) == 1, "Expected exactly one principal to be retrieved by external ID."
+        assert retrieved[0].dump() == service_account.dump(), (
+            "Retrieved principal by external ID should match the service account principal."
+        )
