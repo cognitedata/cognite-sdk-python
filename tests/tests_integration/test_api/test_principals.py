aeac264d153ef26d73438c2c9adfa7ed535dfc58
anders-albert <anders0albert@gmail.com>
2025-07-26 14:09:49 +0200

    tests: update

--- tests/tests_integration/test_api/test_principals.py
+++ tests/tests_integration/test_api/test_principals.py
@@ -5,28 +5,39 @@ from cognite.client.data_classes.principals import Principal, PrincipalList
 
 
 @pytest.fixture(scope="session")
-def me(cognite_client: CogniteClient) -> Principal:
+def client(cognite_client_cdf_authenticated: CogniteClient) -> CogniteClient:
+    """These endpoints requires an CDF authenticated client.
+    Ref https://api-docs.cognite.com/20230101/tag/Principals#section/Authentication-for-this-API
+    """
+    return cognite_client_cdf_authenticated
+
+
+@pytest.fixture(scope="session")
+def me(client: CogniteClient) -> Principal:
     """Fixture to retrieve the principal of the user running the tests."""
-    return cognite_client.iam.principals.me()
+    return client.iam.principals.me()
 
 
 class TestPrincipalsAPI:
-    def test_list_principals(self, cognite_client: CogniteClient, me: Principal) -> None:
-        principals = cognite_client.iam.principals.list(limt=3)
+    def test_me(self, client: CogniteClient, me: Principal) -> None:
+        assert isinstance(me, Principal)
+
+    def test_list_principals(self, client: CogniteClient, me: Principal) -> None:
+        principals = client.iam.principals.list(limit=3)
         assert 0 < len(principals) <= 3
 
-    def test_filter_principals_by_type(self, cognite_client: CogniteClient, me: Principal) -> None:
+    def test_filter_principals_by_type(self, client: CogniteClient, me: Principal) -> None:
         type_ = me._type
-        principals = cognite_client.iam.principals.list(types=type_, limit=-1)
+        principals = client.iam.principals.list(types=type_, limit=-1)
         assert len(principals) > 0
-        assert any(p.id == me.id for p in principals)
+        assert any(p.id == me.id for p in principals), "The principal should be included in the list of principals."
 
-    def test_retrieve_principal(self, cognite_client: CogniteClient, me: Principal) -> None:
-        retrieved = cognite_client.iam.principals.retrieve(id=me.id)
+    def test_retrieve_principal(self, client: CogniteClient, me: Principal) -> None:
+        retrieved = client.iam.principals.retrieve(id=me.id)
         assert retrieved.dump() == me.dump()
 
-    def test_retrieve_multiple_principals(self, cognite_client: CogniteClient, me: Principal) -> None:
-        retrieved = cognite_client.iam.principals.retrieve_multiple(id=[me.id])
+    def test_retrieve_multiple_principals(self, client: CogniteClient, me: Principal) -> None:
+        retrieved = client.iam.principals.retrieve_multiple(id=[me.id])
         assert isinstance(retrieved, PrincipalList)
         assert len(retrieved) == 1
         assert retrieved[0].dump() == me.dump()
