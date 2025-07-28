f50831af75134cafb582547bb41cec3b679018c2
Ivan Polomani <ivan.polomanyi@cognite.com>
2025-07-28 19:40:00 +0200

    test: add cleanup for integrations

--- tests/tests_integration/test_api/test_simulators/test_integrations.py
+++ tests/tests_integration/test_api/test_simulators/test_integrations.py
@@ -4,8 +4,22 @@ import pytest
 
 from cognite.client._cognite_client import CogniteClient
 from cognite.client.data_classes.simulators.filters import SimulatorIntegrationFilter
+from cognite.client.exceptions import CogniteAPIError
 from cognite.client.utils._text import random_string
 from tests.tests_integration.test_api.test_simulators.seed.data import simulator_integration
+
+
+def cleanup_inactive_integrations(cognite_client: CogniteClient, external_id_to_keep: str) -> None:
+    """Cleanup simulator integrations except the one with the given external_id."""
+    try:
+        all_integrations = cognite_client.simulators.integrations.list(limit=None)
+        integrations_to_delete = [
+            item for item in all_integrations if not item.active and item.external_id != external_id_to_keep
+        ]
+        if len(integrations_to_delete) > 0:
+            cognite_client.simulators.integrations.delete(ids=[item.id for item in integrations_to_delete])
+    except CogniteAPIError:
+        pass
 
 
 @pytest.mark.usefixtures("seed_resource_names", "seed_simulator_integration")
@@ -44,16 +58,20 @@ class TestSimulatorIntegrations:
         assert log.data[0].severity == "Debug"
 
     def test_delete_integrations(self, cognite_client: CogniteClient, seed_resource_names) -> None:
-        simulator_integration["heartbeat"] = int(time.time() * 1000)
-        simulator_integration["externalId"] = random_string(50)
-        simulator_integration["dataSetId"] = seed_resource_names["simulator_test_data_set_id"]
+        test_integration = simulator_integration.copy()
+        test_integration["heartbeat"] = int(time.time() * 1000)
+        test_integration["externalId"] = random_string(50)
+        test_integration["dataSetId"] = seed_resource_names["simulator_test_data_set_id"]
 
-        cognite_client.simulators._post("/simulators/integrations", json={"items": [simulator_integration]})
+        try:
+            cognite_client.simulators._post("/simulators/integrations", json={"items": [test_integration]})
 
-        all_integrations = cognite_client.simulators.integrations.list(limit=None)
-        assert all_integrations.get(external_id=simulator_integration["externalId"]) is not None
+            all_integrations = cognite_client.simulators.integrations.list(limit=None)
+            assert all_integrations.get(external_id=test_integration["externalId"]) is not None
 
-        cognite_client.simulators.integrations.delete(external_ids=simulator_integration["externalId"])
+            cognite_client.simulators.integrations.delete(external_ids=test_integration["externalId"])
 
-        all_integrations = cognite_client.simulators.integrations.list(limit=None)
-        assert all_integrations.get(external_id=simulator_integration["externalId"]) is None
+            all_integrations = cognite_client.simulators.integrations.list(limit=None)
+            assert all_integrations.get(external_id=test_integration["externalId"]) is None
+        finally:
+            cleanup_inactive_integrations(cognite_client, seed_resource_names["simulator_integration_external_id"])
