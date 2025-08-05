c0ab2abcc5075c1564fafa1d7f39e96f82ad0b56
Luis Pereira <91738660+lpereiracgn@users.noreply.github.com>
2025-08-05 23:49:35 +0200

    chore: improve validation message for SimulatorModelRevisionExternalDependency

--- cognite/client/data_classes/simulators/models.py
+++ cognite/client/data_classes/simulators/models.py
@@ -406,4 +406,5 @@ class SimulatorModelRevisionExternalDependency(CogniteObject):
         elif isinstance(resource, list):
             return [cls._load(res, cognite_client) for res in resource if isinstance(res, dict)]
         else:
-            raise TypeError("Expected a dict or a list of dicts.")
+            class_name = cls.__class__.__name__
+            raise TypeError(f"Expected a dict or a list of instances of {class_name}, but got: {type(resource)}")
