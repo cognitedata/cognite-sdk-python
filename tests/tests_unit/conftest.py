bec9924afc502678fec160c19ef12e1396cfa3dd
anders-albert <anders0albert@gmail.com>
2025-08-19 15:26:07 +0200

    tests: refactor out client config

--- tests/tests_unit/conftest.py
+++ tests/tests_unit/conftest.py
@@ -11,11 +11,20 @@ from cognite.client.credentials import Token
 collect_ignore = ["test_api/function_test_resources"]
 
 
+@pytest.fixture(scope="session")
+def client_config() -> ClientConfig:
+    """Fixture to provide a CogniteClient configuration."""
+    return ClientConfig(
+        client_name="test_client",
+        project="dummy_project",
+        credentials=Token("dummy_token"),
+    )
+
+
 # TODO: This class-scoped client causes side-effects between tests...
 @pytest.fixture(scope="class")
-def cognite_client():
-    cnf = ClientConfig(client_name="any", project="dummy", credentials=Token("bla"))
-    yield CogniteClient(cnf)
+def cognite_client(client_config: ClientConfig) -> CogniteClient:
+    yield CogniteClient(client_config)
 
 
 @pytest.fixture(scope="session")
