7c0279dc021ac3babf1ac6f165d72c57cd46ad30
anders-albert <anders0albert@gmail.com>
2025-08-29 13:43:17 +0200

    tests: extending

--- tests/tests_integration/test_api/test_organization.py
+++ tests/tests_integration/test_api/test_organization.py
@@ -1,13 +1,23 @@
+import pytest
+
 from cognite.client import CogniteClient
 from cognite.client._api.organization import OrgAPI
 
 
-class TestOrganizationAPI:
-    def test_get_organization(self, cognite_client: CogniteClient) -> None:
-        org_api = OrgAPI(cognite_client.config, cognite_client._API_VERSION, cognite_client)
+@pytest.fixture(scope="session")
+def org_api(cognite_client_cdf_authenticated: CogniteClient) -> OrgAPI:
+    client = cognite_client_cdf_authenticated
+    return OrgAPI(client.config, client._API_VERSION, client)
 
+
+class TestOrganizationAPI:
+    def test_get_organization(self, org_api: OrgAPI) -> None:
         organization = org_api._organization
         assert isinstance(organization, str)
         assert organization != ""
-        if cognite_client.config.project in {"python-sdk-contributor", "python-sdk-test", "python-sdk-test-prod"}:
+        if org_api._config.project in {"python-sdk-contributor", "python-sdk-test", "python-sdk-test-prod"}:
             assert organization == "cog-python-sdk"
+
+    def test_get_request_org_endpoint(self, org_api: OrgAPI) -> None:
+        response = org_api._get("/principals")
+        assert response.status_code == 200
