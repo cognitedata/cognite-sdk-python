0b7e1aa22e4d647e055dfa9a148acf5ada141cec
erlendvollset <erlendvollset@gmail.com>
2025-09-04 11:40:08 +0200

    Introduce DefaultResourceGenerator
    Used to create read versions of resource data classes without having to provide all arguments

--- tests/tests_unit/test_api/test_events.py
+++ tests/tests_unit/test_api/test_events.py
@@ -10,6 +10,7 @@ from cognite.client.data_classes import (
     EventWrite,
     TimestampRange,
 )
+from tests.tests_unit.conftest import DefaultResourceGenerator
 from tests.utils import get_url, jsgz_load
 
 
@@ -167,24 +168,7 @@ class TestEvents:
         assert res is None
 
     def test_update_with_resource_class(self, cognite_client, mock_events_response, example_event):
-        res = cognite_client.events.update(
-            Event(
-                id=1,
-                last_updated_time=123,
-                created_time=123,
-                external_id=None,
-                data_set_id=None,
-                start_time=None,
-                end_time=None,
-                type=None,
-                subtype=None,
-                description=None,
-                metadata=None,
-                asset_ids=None,
-                source=None,
-                cognite_client=None,
-            )
-        )
+        res = cognite_client.events.update(DefaultResourceGenerator.event(id=1))
         assert isinstance(res, Event)
         assert example_event == res.dump(camel_case=True)
 
