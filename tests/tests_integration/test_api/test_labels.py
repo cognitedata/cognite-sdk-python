12579e4561c0ec3fe9099377db9a620091b70778
erlendvollset <erlendvollset@gmail.com>
2025-09-08 17:12:01 +0200

    Run mypy on tests_integration/test_api/test_labels.py

--- tests/tests_integration/test_api/test_labels.py
+++ tests/tests_integration/test_api/test_labels.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterator
 from unittest import mock
 
 import pytest
@@ -14,26 +15,27 @@ from cognite.client.data_classes import (
 )
 from cognite.client.exceptions import CogniteNotFoundError
 from cognite.client.utils._text import random_string
+from tests.utils import get_or_raise
 
 
 @pytest.fixture
-def new_label(cognite_client):
+def new_label(cognite_client: CogniteClient) -> Iterator[LabelDefinition]:
     tp = cognite_client.labels.create(LabelDefinitionWrite(external_id=random_string(30), name="mandatory"))
     yield tp
     cognite_client.labels.delete(external_id=tp.external_id)
 
 
 @pytest.fixture
-def post_spy(cognite_client):
+def post_spy(cognite_client: CogniteClient) -> Iterator[None]:
     with mock.patch.object(cognite_client.labels, "_post", wraps=cognite_client.labels._post) as _:
         yield
 
 
 class TestLabelsAPI:
-    def test_list(self, cognite_client, new_label, post_spy) -> None:
+    def test_list(self, cognite_client: CogniteClient, new_label: LabelDefinition, post_spy: None) -> None:
         res = cognite_client.labels.list(limit=100)
         assert 0 < len(res) <= 100
-        assert 1 == cognite_client.labels._post.call_count
+        assert 1 == cognite_client.labels._post.call_count  # type: ignore[attr-defined]
 
     def test_retrieve_existing_labels(self, cognite_client: CogniteClient, new_label: LabelDefinition) -> None:
         res = cognite_client.labels.retrieve(external_id=new_label.external_id)
@@ -61,20 +63,20 @@ class TestLabelsAPI:
         res = cognite_client.labels.retrieve(external_id="this does not exist", ignore_unknown_ids=True)
         assert res is None
 
-    def test_create_asset_with_label(self, cognite_client, new_label) -> None:
+    def test_create_asset_with_label(self, cognite_client: CogniteClient, new_label: LabelDefinition) -> None:
         ac = cognite_client.assets.create(AssetWrite(name="any", labels=[Label(external_id=new_label.external_id)]))
         assert isinstance(ac, Asset)
-        assert len(ac.labels) == 1
+        assert len(ac.labels or []) == 1
         cognite_client.assets.delete(id=ac.id)
 
-    def test_update_asset_with_label(self, cognite_client, new_label) -> None:
+    def test_update_asset_with_label(self, cognite_client: CogniteClient, new_label: LabelDefinition) -> None:
         ac = cognite_client.assets.create(AssetWrite(name="any", description="delete me"))
         assert not ac.labels
         update = AssetUpdate(id=ac.id).labels.add([new_label.external_id])
         assert isinstance(update, AssetUpdate)
         ua = cognite_client.assets.update(update)
-        assert len(ua.labels) == 1
-        assert new_label.external_id == ua.labels[0].external_id
+        assert len(ua.labels or []) == 1
+        assert new_label.external_id == get_or_raise(ua.labels)[0].external_id
 
         update = AssetUpdate(id=ac.id).labels.remove([new_label.external_id])
         assert isinstance(update, AssetUpdate)
