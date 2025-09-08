023d6c83902ae511e533ed6b20384a5e247c3599
Håkon V. Treider <haakonvt@gmail.com>
2025-09-08 22:25:32 +0200

    fix: reintroduce _base_url_with_base_path on API client to handle Org. API

--- cognite/client/_org_client.py
+++ cognite/client/_org_client.py
@@ -9,28 +9,28 @@ from cognite.client.exceptions import CogniteAPIError
 
 
 class OrgAPIClient(APIClient, ABC):
-    _auth_url = "https://auth.cognite.com"
+    _AUTH_URL = "https://auth.cognite.com"
 
-    def _get_base_url_with_base_path(self) -> str:
-        """Get base URL with base path including organization and api version if applicable"""
-        base_path = ""
-        if self._api_version:
-            base_path = f"/api/{self._api_version}/orgs/{self._organization}"
-        # The OrganizationAPi uses the auth_url as the base for these endpoints instead of the
+    @cached_property
+    def _base_url_with_base_path(self) -> str:
+        # The OrganizationAPI uses the auth_url as the base for these endpoints instead of the
         # base_url like the rest of the SDK.
-        return urljoin(self._auth_url, base_path)
+        assert self._api_version, "API version must be set for organization endpoints"
+        return urljoin(self._AUTH_URL, f"/api/{self._api_version}/orgs/{self._organization}")
 
     @cached_property
     def _organization(self) -> str:
-        headers = self._configure_headers(
-            additional_headers=self._config.headers.copy(),
-            api_subversion=self._api_subversion,
-        )
-        # This is an internal endpoint, not part of the public API
-        full_url = urljoin(self._config.base_url, f"/api/v1/projects/{self._config.project}")
-        response = self._http_client_with_retry("GET", url=full_url, headers=headers)
-        if response.status_code != 200:
+        # This is an internal endpoint, not part of the public API - and we call it manually to avoid
+        # the overridden _base_url_with_base_path property:
+        from cognite.client.utils._concurrency import ConcurrencySettings
+
+        headers = self._configure_headers(additional_headers=None, api_subversion=self._api_subversion)
+        full_url = urljoin(self._config.base_url, f"/api/{self._api_version}/projects/{self._config.project}")
+        executor = ConcurrencySettings._get_event_loop_executor()
+        response = executor.run_coro(self._http_client_with_retry("GET", url=full_url, headers=headers))
+        try:
+            return response.raise_for_status().json()["organization"]
+        except Exception:
             raise CogniteAPIError(
                 "Could not look-up organization", response.status_code, response.headers.get("x-request-id")
             )
-        return response.json()["organization"]
