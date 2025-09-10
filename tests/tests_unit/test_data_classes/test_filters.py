e2677ab7f8f0dead88fde77040b59b7803b208c3
Håkon V. Treider <haakonvt@gmail.com>
2025-09-10 11:30:17 +0200

    add: unit tests

--- tests/tests_unit/test_data_classes/test_filters.py
+++ tests/tests_unit/test_data_classes/test_filters.py
@@ -1,6 +1,27 @@
+import re
+
 import pytest
 
-from cognite.client.data_classes.filters import Filter, In
+from cognite.client.data_classes.filters import And, Filter, In, Or
+from tests.utils import FakeCogniteResourceGenerator
+
+
+def test_filters_raise_on_and_or_or() -> None:
+    # We've seen multiple users are trying to combine filters using `and` and `or`, which does not work,
+    # as the behavior can not be overloaded in Python (must use & and |).
+    fcrg = FakeCogniteResourceGenerator()
+    # Create two random filters:
+    flt1 = fcrg.create_instance(Filter)
+    flt2 = fcrg.create_instance(Filter)
+
+    match_str = re.escape("You can not combine filters using 'and' or 'or', you must use the And filter (&)")
+    with pytest.raises(RuntimeError, match=match_str):
+        flt1 and flt2  # type: ignore[operator]
+    with pytest.raises(RuntimeError, match=match_str):
+        flt1 or flt2  # type: ignore[operator]
+
+    assert type(flt1 & flt2) is And
+    assert type(flt1 | flt2) is Or
 
 
 @pytest.mark.parametrize(
