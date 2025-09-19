374184b47aaa28203ad33f0c370d5345806782c9
Håkon V. Treider <haakonvt@gmail.com>
2025-09-19 14:35:49 +0200

    add test on DebugParameters dump

--- tests/tests_unit/test_data_classes/test_data_models/test_debug_notices.py
+++ tests/tests_unit/test_data_classes/test_data_models/test_debug_notices.py
@@ -1,6 +1,27 @@
 from __future__ import annotations
 
-from cognite.client.data_classes.data_modeling.debug import DebugNotice, UnknownDebugNotice
+from typing import Any
+
+import pytest
+
+from cognite.client.data_classes.data_modeling.debug import DebugNotice, DebugParameters, UnknownDebugNotice
+
+
+class TestDebugParameters:
+    @pytest.mark.parametrize(
+        "emit_results, timeout, profile, camel_case, expected",
+        [
+            (False, 1234, True, True, {"emitResults": False, "timeout": 1234, "profile": True}),
+            (False, 1234, True, False, {"emit_results": False, "timeout": 1234, "profile": True}),
+            (True, None, False, True, {"emitResults": True, "profile": False}),
+            (True, None, False, False, {"emit_results": True, "profile": False}),
+        ],
+    )
+    def test_debug_parameters(
+        self, emit_results: bool, timeout: int | None, profile: bool, camel_case: bool, expected: dict[str, Any]
+    ) -> None:
+        params = DebugParameters(emit_results=emit_results, timeout=timeout, profile=profile)
+        assert params.dump(camel_case=camel_case) == expected
 
 
 class TestDebugNotices:
