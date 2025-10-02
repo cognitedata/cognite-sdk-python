35d15447e78ec06161414e2f9f0a95fdf1e84a6d
Håkon V. Treider <haakonvt@gmail.com>
2025-10-03 00:38:05 +0200

    run script to create all sync APIs + sync CogniteClient

---
+++ cognite/client/_sync_api/iam/token.py
@@ -0,0 +1,44 @@
+"""
+===============================================================================
+5268479111509d912fc224eb231afa08
+This file is auto-generated from the Async API modules, - do not edit manually!
+===============================================================================
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from cognite.client import AsyncCogniteClient
+from cognite.client.data_classes.iam import TokenInspection
+from cognite.client.utils._async_helpers import run_sync
+
+if TYPE_CHECKING:
+    pass
+
+
+class SyncTokenAPI:
+    """Auto-generated, do not modify manually."""
+
+    def __init__(self, async_client: AsyncCogniteClient):
+        self.__async_client = async_client
+
+    def inspect(self) -> TokenInspection:
+        """
+        Inspect a token.
+
+        Get details about which projects it belongs to and which capabilities are granted to it.
+
+        Returns:
+            TokenInspection: The object with token inspection details.
+
+        Example:
+
+            Inspect token::
+
+                >>> from cognite.client import CogniteClient, AsyncCogniteClient
+                >>> client = CogniteClient()
+                >>> # async_client = AsyncCogniteClient()  # another option
+                >>> res = client.iam.token.inspect()
+        """
+        return run_sync(self.__async_client.iam.token.inspect())
