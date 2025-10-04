7b7e57a656b24a980c06d0a10ab50409b0546657
Håkon V. Treider <haakonvt@gmail.com>
2025-10-04 21:10:54 +0200

    add run_sync function to _utils/async_helpers.py

--- cognite/client/utils/_async_helpers.py
+++ cognite/client/utils/_async_helpers.py
@@ -1,26 +1,26 @@
 import asyncio
-from collections.abc import AsyncIterator, Iterator
-from typing import TypeVar
+from collections.abc import AsyncIterator, Coroutine, Iterator
+from typing import Any, TypeVar
 
 from cognite.client.utils._concurrency import ConcurrencySettings
 
-T = TypeVar("T")
+_T = TypeVar("_T")
 _SENTINEL = object()
 
 
-class SyncIterator(Iterator[T]):
+class SyncIterator(Iterator[_T]):
     """A synchronous iterator that wraps an async iterator. Used in the sync Cognite client to
     avoid loading all items into memory before yielding them as it just wraps the async client."""
 
-    def __init__(self, async_iter: AsyncIterator[T]) -> None:
+    def __init__(self, async_iter: AsyncIterator[_T]) -> None:
         self._async_iter = async_iter
         self._run_coro = ConcurrencySettings._get_event_loop_executor().run_coro
 
-    def __iter__(self) -> Iterator[T]:
+    def __iter__(self) -> Iterator[_T]:
         return self
 
-    def __next__(self) -> T:
-        async def get_next() -> T:
+    def __next__(self) -> _T:
+        async def get_next() -> _T:
             try:
                 return await self._async_iter.__anext__()
             except StopAsyncIteration:
@@ -30,6 +30,11 @@ class SyncIterator(Iterator[T]):
         if (result := self._run_coro(get_next())) is _SENTINEL:
             raise StopIteration
         return result
+
+
+def run_sync(coro: Coroutine[_T, Any, _T]) -> _T:
+    executor = ConcurrencySettings._get_event_loop_executor()
+    return executor.run_coro(coro)
 
 
 if __name__ == "__main__":
