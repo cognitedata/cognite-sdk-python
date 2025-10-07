0f8125eda36cec0bd623284e8e45eaf268eb4fbe
Håkon V. Treider <haakonvt@gmail.com>
2025-10-07 21:40:32 +0200

    make test util set_request_limit a fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,40 @@
 import platform
-from collections.abc import Iterator
+from collections.abc import Callable, Iterator
 
 import dotenv
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
 from cognite.client import global_config
+from cognite.client._api_client import APIClient
 
 dotenv.load_dotenv()
 
 global_config.disable_pypi_version_check = True
+
+
+_STANDARD_API_LIMIT_NAMES = [
+    "_CREATE_LIMIT",
+    "_LIST_LIMIT",
+    "_RETRIEVE_LIMIT",
+    "_UPDATE_LIMIT",
+    "_DELETE_LIMIT",
+]
+
+
+@pytest.fixture
+def set_request_limit(monkeypatch: pytest.MonkeyPatch) -> Callable[[APIClient, int], None]:
+    """
+    Pytest fixture that provides a factory function to temporarily set API limits
+    on a client instance for the duration of a single test.
+    """
+
+    def _setter(client: APIClient, limit: int) -> None:
+        for limit_name in _STANDARD_API_LIMIT_NAMES:
+            # We use raising=False to prevents an error if the attribute doesn't exist:
+            monkeypatch.setattr(client, limit_name, limit, raising=False)
+
+    return _setter
 
 
 @pytest.fixture
