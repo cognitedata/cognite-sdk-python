fef11eed32a0a85e0c0ad5cec1496acd18cc1053
Håkon V. Treider <haakonvt@gmail.com>
2025-10-07 13:41:39 +0200

    add Omitted class to be used as "not given" singleton

--- cognite/client/_constants.py
+++ cognite/client/_constants.py
@@ -1,16 +1,11 @@
 from __future__ import annotations
 
-try:
-    from pyodide.ffi import IN_BROWSER  # type: ignore [import-not-found]
-except ModuleNotFoundError:
-    IN_BROWSER = False
+from typing import Literal
 
-_RUNNING_IN_BROWSER = IN_BROWSER
-DEFAULT_LIMIT_READ = 25
-# Max JavaScript-safe integer 2^53 - 1
-MAX_VALID_INTERNAL_ID = 9007199254740991
-DATA_MODELING_DEFAULT_LIMIT_READ = 10
-DEFAULT_DATAPOINTS_CHUNK_SIZE = 100_000
+try:
+    from pyodide.ffi import IN_BROWSER as _RUNNING_IN_BROWSER  # type: ignore [import-not-found]
+except ModuleNotFoundError:
+    _RUNNING_IN_BROWSER = False
 
 try:
     import numpy as np  # noqa F401
@@ -19,3 +14,21 @@ try:
 
 except ImportError:  # pragma no cover
     NUMPY_IS_AVAILABLE = False
+
+
+class Omitted:
+    """Sentinel value for parameters that are not given."""
+
+    def __repr__(self) -> str:
+        return "<Omitted parameter>"
+
+    def __bool__(self) -> Literal[False]:
+        return False
+
+
+OMITTED = Omitted()
+DEFAULT_LIMIT_READ = 25
+# Max JavaScript-safe integer 2^53 - 1
+MAX_VALID_INTERNAL_ID = 9007199254740991
+DATA_MODELING_DEFAULT_LIMIT_READ = 10
+DEFAULT_DATAPOINTS_CHUNK_SIZE = 100_000
