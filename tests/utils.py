9f4f8281405b21d3cd54caa3a0a82065228c87c5
Håkon V. Treider <haakonvt@gmail.com>
2025-10-07 21:40:32 +0200

    make test util set_request_limit a fixture

--- tests/utils.py
+++ tests/utils.py
@@ -236,27 +236,6 @@ def jsgz_load(s: Any) -> Any:
     return _json.loads(gzip.decompress(s).decode())
 
 
-@contextmanager
-def set_request_limit(client: APIClient, limit: int) -> typing.Iterator[None]:
-    limits = [
-        "_CREATE_LIMIT",
-        "_LIST_LIMIT",
-        "_RETRIEVE_LIMIT",
-        "_UPDATE_LIMIT",
-        "_DELETE_LIMIT",
-    ]
-
-    tmp = {lim: 0 for lim in limits}
-    for limit_name in limits:
-        if hasattr(client, limit_name):
-            tmp[limit_name] = getattr(client, limit_name)
-            setattr(client, limit_name, limit)
-    yield
-    for limit_name, limit_val in tmp.items():
-        if hasattr(client, limit_name):
-            setattr(client, limit_name, limit_val)
-
-
 T = TypeVar("T")
 K = TypeVar("K")
 V = TypeVar("V")
