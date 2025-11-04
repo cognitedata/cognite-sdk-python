95efcb29217b24836504d54154546ac04eb7dbb8
Håkon V. Treider <haakonvt@gmail.com>
2025-11-04 11:53:03 +0100

    make async: utils/_session.py

--- cognite/client/utils/_session.py
+++ cognite/client/utils/_session.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
     from cognite.client import AsyncCogniteClient
 
 
-def create_session_and_return_nonce(
+async def create_session_and_return_nonce(
     client: AsyncCogniteClient, api_name: str, client_credentials: dict | ClientCredentials | None = None
 ) -> str:
     if client_credentials is None:
@@ -18,4 +18,6 @@ def create_session_and_return_nonce(
             raise CogniteAuthError(f"Client certificate credentials is not supported with the {api_name}")
     elif isinstance(client_credentials, dict):
         client_credentials = ClientCredentials(client_credentials["client_id"], client_credentials["client_secret"])
-    return client.iam.sessions.create(client_credentials).nonce
+
+    session = await client.iam.sessions.create(client_credentials)
+    return session.nonce
