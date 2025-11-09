35ebef7e1e028ae8a4d44dba81a7052e1c23eb44
Håkon V. Treider <haakonvt@gmail.com>
2025-11-09 12:33:38 +0100

    replace rsps with pytest-httpx (integration): test_api/test_simulators/utils.py

--- tests/tests_integration/test_api/test_simulators/utils.py
+++ tests/tests_integration/test_api/test_simulators/utils.py
@@ -1,9 +1,9 @@
-from requests import Response
+import httpx
 
-from cognite.client._cognite_client import CogniteClient
+from cognite.client import CogniteClient
 
 
-def update_logs(cognite_client: CogniteClient, log_id: int, payload: list[dict]) -> Response:
+def update_logs(cognite_client: CogniteClient, log_id: int, payload: list[dict]) -> httpx.Response:
     items = {"items": [{"id": log_id, "update": {"data": {"add": payload}}}]}
     return cognite_client.simulators._post(
         "/simulators/logs/update",
