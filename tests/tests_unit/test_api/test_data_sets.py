bd95a6487409c48e2d6a9edf02d09ff5635c886e
Morten Grønbech <morten.gronbech@cognite.com>
2026-01-05 09:03:56 +0100

    Fix error on Windows

--- tests/tests_unit/test_api/test_data_sets.py
+++ tests/tests_unit/test_api/test_data_sets.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime
+from datetime import datetime, timezone
 
 import pytest
 from responses import RequestsMock
@@ -52,7 +52,8 @@ class TestDataset:
         assert isinstance(res, DataSetList)
         assert mock_ds_response.calls[0].response.json()["items"] == res.dump(camel_case=True)
 
-    @pytest.mark.parametrize("min_time", [20, datetime.fromtimestamp(20 / 1000)])
+    # NOTE: Have to set timezeon in order to avoid OSError on Windows: https://stackoverflow.com/a/65564765/5753974
+    @pytest.mark.parametrize("min_time", [20, datetime.fromtimestamp(20 / 1000, timezone.utc)])
     def test_list_with_timestamp_range(
         self, cognite_client: CogniteClient, mock_ds_response: RequestsMock, min_time: int | datetime
     ):
