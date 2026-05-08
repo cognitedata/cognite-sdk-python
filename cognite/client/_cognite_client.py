cf4b9c6723eab5caa567a7f0c8625e90c7cdbf1b
Anders Øen Fylling <anders@nordic.email>
2026-05-08 14:14:41 +0200

    run pre-commit

--- cognite/client/_cognite_client.py
+++ cognite/client/_cognite_client.py
@@ -49,7 +49,6 @@ if _should_build_docs := os.getenv("BUILD_COGNITE_SDK_DOCS") == "true":
     from cognite.client._api.data_modeling.data_models import DataModelsAPI
     from cognite.client._api.data_modeling.graphql import DataModelingGraphQLAPI
     from cognite.client._api.data_modeling.instances import InstancesAPI
-    from cognite.client._api.data_modeling.records import RecordsAPI
     from cognite.client._api.data_modeling.space_statistics import SpaceStatisticsAPI
     from cognite.client._api.data_modeling.spaces import SpacesAPI
     from cognite.client._api.data_modeling.statistics import StatisticsAPI
