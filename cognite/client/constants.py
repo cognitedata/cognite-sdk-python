880001f8d903f731d76fcccae0ed79cef1d6d8fe
Morten Grønbech <morten.gronbech@cognite.com>
2026-05-22 11:21:07 +0200

    Fix constatns file

--- cognite/client/constants.py
+++ cognite/client/constants.py
@@ -2,19 +2,6 @@ from __future__ import annotations
 
 from typing import Literal
 
-try:
-    from pyodide.ffi import IN_BROWSER  # type: ignore [import-not-found]
-except ModuleNotFoundError:
-    IN_BROWSER = False
-
-try:
-    import numpy as np  # noqa F401
-
-    NUMPY_IS_AVAILABLE = True
-
-except ImportError:  # pragma no cover
-    NUMPY_IS_AVAILABLE = False
-
 
 class Omitted:
     """Sentinel value for parameters that are not given or should be treated as not given."""
@@ -28,14 +15,6 @@ class Omitted:
 
 OMITTED = Omitted()
 DEFAULT_LIMIT_READ = 25
-# Max JavaScript-safe integer 2^53 - 1
-MAX_VALID_INTERNAL_ID = 9007199254740991
+
 DATA_MODELING_DEFAULT_LIMIT_READ = 10
 DEFAULT_DATAPOINTS_CHUNK_SIZE = 100_000
-_RUNNING_IN_BROWSER = IN_BROWSER
-
-# Files API constants
-FILE_MIN_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MiB
-FILE_MAX_MULTIPART_SIZE = 4000 * 1024 * 1024  # 4000 MiB
-FILE_DEFAULT_MULTIPART_SIZE = 50 * 1024 * 1024  # 50 MiB
-FILE_MAX_MULTIPART_COUNT = 250
