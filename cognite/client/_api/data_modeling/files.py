27aa36a5523e6622814861d32cf90afb15985a94
Håkon V. Treider <haakonvt@gmail.com>
2026-05-26 10:56:03 +0200

    Add DM File API client interface

--- cognite/client/_api/data_modeling/files.py
+++ cognite/client/_api/data_modeling/files.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, NoReturn
 
 from cognite.client._api_client import APIClient
 
@@ -13,3 +13,36 @@ class DataModelingFilesAPI(APIClient):
     def __init__(self, config: ClientConfig, api_version: str | None, cognite_client: AsyncCogniteClient) -> None:
         super().__init__(config, api_version, cognite_client)
         self._files_api = cognite_client.files
+
+    async def retrieve_download_urls(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def download(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def download_to_path(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def download_bytes(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def upload(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def upload_bytes(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def upload_content(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def upload_content_bytes(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def __call__(self) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def retrieve(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
+
+    async def list(self, *args: Any, **kwargs: Any) -> NoReturn:
+        raise NotImplementedError("This method is not implemented yet!")
