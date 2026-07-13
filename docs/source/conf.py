f1e64a86fd22c0118f6e3f070eb38c88e390cf8f
Håkon V. Treider <haakonvt@gmail.com>
2026-07-13 15:10:35 +0200

    docs(conf): ignore self-referential readthedocs links in linkcheck

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -192,6 +192,10 @@ texinfo_documents = [
 
 python_maximum_signature_line_length = 80
 
+# Self-referential links to readthedocs fail in CI for PRs that add new sections
+# (the URL only exists after the PR is merged and docs are published):
+linkcheck_ignore = [r"https://cognite-sdk-python\.readthedocs-hosted\.com/.*"]
+
 # Patch Sphinx to hide @overload signatures in docs
 # Sphinx's autodoc uses ModuleAnalyzer.overloads via the parser
 # We patch the analyze method to clear overloads after parsing
