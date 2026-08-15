aadc3cecf9370de57253617d3867b16245fe1e19
Morten Grønbech <morten.gronbech@cognite.com>
2026-08-15 14:43:19 +0200

    Disable the pypi version check when mocking

--- cognite/client/testing.py
+++ cognite/client/testing.py
@@ -5,7 +5,7 @@ from contextlib import contextmanager
 from typing import Any
 from unittest.mock import MagicMock, create_autospec, patch
 
-from cognite.client import AsyncCogniteClient, ClientConfig, CogniteClient
+from cognite.client import AsyncCogniteClient, ClientConfig, CogniteClient, global_config
 from cognite.client._api.agents import AgentsAPI
 from cognite.client._api.ai import AIAPI
 from cognite.client._api.ai.tools import AIToolsAPI
@@ -211,7 +211,10 @@ class AsyncCogniteClientMock(MagicMock, metaclass=_SpecSetEnforcer):
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(spec=AsyncCogniteClient, *args, **kwargs)
+        original_value = global_config.disable_pypi_version_check
+        global_config.disable_pypi_version_check = True
         self.config = create_autospec(ClientConfig("mock", "mock", Token("mock"), base_url="mock"), spec_set=True)
+        global_config.disable_pypi_version_check = original_value
         # Developer note:
         # - Please add your mocked APIs in chronological order
         # - Use create_autospec with instance=True for better type safety and accurate mocking.
@@ -422,7 +425,10 @@ class CogniteClientMock(MagicMock, metaclass=_SpecSetEnforcer):
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(spec=CogniteClient, *args, **kwargs)
+        original_value = global_config.disable_pypi_version_check
+        global_config.disable_pypi_version_check = True
         self.config = create_autospec(ClientConfig("mock", "mock", Token("mock"), base_url="mock"), spec_set=True)
+        global_config.disable_pypi_version_check = original_value
         # Developer note:
         # - Please add your mocked APIs in chronological order
         # - Use create_autospec with instance=True for better type safety and accurate mocking.
