ff8cb43e373636bb7b06ac3f6e13a72ab0636e94
Håkon V. Treider <haakonvt@gmail.com>
2026-08-21 22:52:05 +0200

    add state_set to CogniteTimeSeries(-Apply) and add 'state' as valid time_series_type

--- cognite/client/data_classes/data_modeling/cdm/v1.py
+++ cognite/client/data_classes/data_modeling/cdm/v1.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Any, Literal
+from typing import Any, Literal, cast
 
 from cognite.client._constants import OMITTED, Omitted
 from cognite.client.data_classes.data_modeling import DirectRelationReference
@@ -3806,6 +3806,7 @@ class CogniteStateSet(_CogniteStateSetProperties, TypedNode):
 class _CogniteTimeSeriesProperties:
     is_step = PropertyOptions("isStep")
     time_series_type = PropertyOptions("type")
+    state_set = PropertyOptions("stateSet")
     source_id = PropertyOptions("sourceId")
     source_context = PropertyOptions("sourceContext")
     source_created_time = PropertyOptions("sourceCreatedTime")
@@ -3830,7 +3831,8 @@ class CogniteTimeSeriesApply(_CogniteTimeSeriesProperties, TypedNodeApply):
         space (str): The space where the node is located.
         external_id (str): The external id of the Cognite time series.
         is_step (bool): Specifies whether the time series is a step time series or not.
-        time_series_type (Literal['numeric', 'string']): Specifies the data type of the data points.
+        time_series_type (Literal['numeric', 'string', 'state']): Specifies the data type of the data points.
+        state_set (DirectRelationReference | tuple[str, str] | Omitted | None): The state set of the time series. It is only in effect when the 'time_series_type' is 'state'.
         name (str | Omitted | None): Name of the instance
         description (str | Omitted | None): Description of the instance
         tags (list[str] | Omitted | None): Text based labels for generic use, limited to 1000
@@ -3856,7 +3858,8 @@ class CogniteTimeSeriesApply(_CogniteTimeSeriesProperties, TypedNodeApply):
         external_id: str,
         *,
         is_step: bool,
-        time_series_type: Literal["numeric", "string"],
+        time_series_type: Literal["numeric", "string", "state"],
+        state_set: DirectRelationReference | tuple[str, str] | Omitted | None = OMITTED,
         name: str | Omitted | None = OMITTED,
         description: str | Omitted | None = OMITTED,
         tags: list[str] | Omitted | None = OMITTED,
@@ -3889,6 +3892,7 @@ class CogniteTimeSeriesApply(_CogniteTimeSeriesProperties, TypedNodeApply):
         self.source_updated_time = source_updated_time
         self.source_created_user = source_created_user
         self.source_updated_user = source_updated_user
+        self.state_set = DirectRelationReference.load(state_set) if state_set else state_set
         self.source_unit = source_unit
         self.unit = DirectRelationReference.load(unit) if unit else unit
         self.assets = [DirectRelationReference.load(a) for a in assets] if assets else assets
@@ -3909,7 +3913,8 @@ class CogniteTimeSeries(_CogniteTimeSeriesProperties, TypedNode):
         last_updated_time (int): The number of milliseconds since 00:00:00 Thursday, 1 January 1970, Coordinated Universal Time (UTC), minus leap seconds.
         created_time (int): The number of milliseconds since 00:00:00 Thursday, 1 January 1970, Coordinated Universal Time (UTC), minus leap seconds.
         is_step (bool): Specifies whether the time series is a step time series or not.
-        time_series_type (Literal['numeric', 'string']): Specifies the data type of the data points.
+        time_series_type (Literal['numeric', 'string', 'state']): Specifies the data type of the data points.
+        state_set (DirectRelationReference | None): The state set of the time series. It is only in effect when the 'time_series_type' is 'state'.
         name (str | None): Name of the instance
         description (str | None): Description of the instance
         tags (list[str] | None): Text based labels for generic use, limited to 1000
@@ -3938,7 +3943,8 @@ class CogniteTimeSeries(_CogniteTimeSeriesProperties, TypedNode):
         created_time: int,
         *,
         is_step: bool,
-        time_series_type: Literal["numeric", "string"],
+        time_series_type: Literal["numeric", "string", "state"],
+        state_set: DirectRelationReference | None = None,
         name: str | None = None,
         description: str | None = None,
         tags: list[str] | None = None,
@@ -3971,6 +3977,7 @@ class CogniteTimeSeries(_CogniteTimeSeriesProperties, TypedNode):
         self.source_updated_time = source_updated_time
         self.source_created_user = source_created_user
         self.source_updated_user = source_updated_user
+        self.state_set = DirectRelationReference.load(state_set) if state_set else state_set
         self.source_unit = source_unit
         self.unit = DirectRelationReference.load(unit) if unit else unit
         self.assets = [DirectRelationReference.load(a) for a in assets] if assets else assets
@@ -3981,7 +3988,7 @@ class CogniteTimeSeries(_CogniteTimeSeriesProperties, TypedNode):
             self.space,
             self.external_id,
             is_step=self.is_step,
-            time_series_type=self.time_series_type,
+            time_series_type=cast(Literal["numeric", "string", "state"], self.time_series_type),
             name=self.name,
             description=self.description,
             tags=self.tags,
@@ -3993,6 +4000,7 @@ class CogniteTimeSeries(_CogniteTimeSeriesProperties, TypedNode):
             source_updated_time=self.source_updated_time,
             source_created_user=self.source_created_user,
             source_updated_user=self.source_updated_user,
+            state_set=self.state_set,
             source_unit=self.source_unit,
             unit=self.unit,
             assets=self.assets,  # type: ignore[arg-type]
